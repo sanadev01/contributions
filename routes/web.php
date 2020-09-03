e5e2123c2854b7de45380f45323de702236913cf
sanadev01 <sanasheraz.dev@gmail.com>
2020-09-03 22:00:47 +0500

    hotfix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,28 +20,27 @@ Auth::routes();
 Route::namespace('Admin')
     ->as('admin.')
     ->group(function () {
+        Route::get('home', HomeController::class);
+        Route::resource('roles', RoleController::class);
+        Route::resource('roles.permissions', RolePermissionController::class);
 
-    Route::resource('roles', RoleController::class);
-    Route::resource('roles.permissions', RolePermissionController::class);
-
-    Route::resource('services', HandlingServiceController::class)->except('show');
-    Route::resource('addresses', AddressController::class);
-    Route::resource('shipping-services', ShippingServiceController::class);
+        Route::resource('services', HandlingServiceController::class)->except('show');
+        Route::resource('addresses', AddressController::class);
+        Route::resource('shipping-services', ShippingServiceController::class);
 
 
-    Route::prefix('rates')
-    ->namespace('Rates')
-    ->as('rates.')
-    ->group(function () {
-        Route::resource('profit-packages', ProfitPackageController::class); 
-        Route::resource('bps-leve', RateController::class)->only(['create', 'store', 'index']);
-    });
+        Route::prefix('rates')
+        ->namespace('Rates')
+        ->as('rates.')
+        ->group(function () {
+            Route::resource('profit-packages', ProfitPackageController::class); 
+            Route::resource('bps-leve', RateController::class)->only(['create', 'store', 'index']);
+        });
 
-    Route::resource('settings', SettingController::class)->only(['index', 'store']);
-    Route::resource('profile', ProfileController::class)->only(['index', 'store']);
+        Route::resource('settings', SettingController::class)->only(['index', 'store']);
+        Route::resource('profile', ProfileController::class)->only(['index', 'store']);
 
-    Route::resource('tickets', TicketController::class);
-    Route::post('tickets/{ticket}/close', [\App\Http\Controllers\Admin\TicketController::class, 'markClose'])->name('ticket.mark-closed');
-
+        Route::resource('tickets', TicketController::class);
+        Route::post('tickets/{ticket}/close', [\App\Http\Controllers\Admin\TicketController::class, 'markClose'])->name('ticket.mark-closed');
 
 });
