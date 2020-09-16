ca600ed9552af455be8dcaecd04646d91f25e684
sanadev01 <sanasheraz.dev@gmail.com>
2020-09-16 22:53:56 +0500

    added default string lenght

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        Schema::defaultStringLength(191);
+
         Blade::directive('admin', function () {
             return '<?php if ( auth()->user()->isAdmin() ) { ?>';
         });
