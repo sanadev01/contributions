9f944bbedae8b54dec6e3d4cc08907a73474e438
sanadev01 <sanasheraz.dev@gmail.com>
2020-11-27 01:06:20 +0500

    support ticket count in menu

---
+++ app/Http/Livewire/Components/SupportTicket.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Livewire\Components;
+use Illuminate\Support\Facades\Auth;
+use App\Models\TicketComment;
+use App\Models\Ticket;
+
+use Livewire\Component;
+
+class SupportTicket extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.support-ticket', [
+            'tickets' => $this->getQuery()
+        ]);
+    }
+
+
+   
+    public function getQuery()
+    {
+        if (Auth::user()->isUser()) {
+            $supportTickets = Ticket::query()
+                ->has('user')
+                ->whereHas('comments', function($q){
+                    $q->where('read', false)
+                    ->where('user_id', '!=', auth()->id());
+                })->count();  
+        }else{
+            $supportTickets = Ticket::query()
+                ->has('user')
+                ->whereHas('comments', function($q){
+                    $q->where('read', false)
+                    ->where('user_id', '!=', auth()->id());
+            })->count();  
+        }
+        return $supportTickets;
+
+    }
+}
