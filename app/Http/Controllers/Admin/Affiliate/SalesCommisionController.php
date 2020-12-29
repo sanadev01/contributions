27f300efe683e992cb80bae685881dc440ea312f
sanadev01 <sanasheraz.dev@gmail.com>
2020-12-29 13:32:14 +0500

    add policy permission in controller

--- app/Http/Controllers/Admin/Affiliate/SalesCommisionController.php
+++ app/Http/Controllers/Admin/Affiliate/SalesCommisionController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers\Admin\Affiliate;
 
 use App\Http\Controllers\Controller;
+use App\Models\AffiliateSale;
 use Illuminate\Http\Request;
 
 class SalesCommisionController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(AffiliateSale::class);
+    }
+
     public function index()
     {
         return view('admin.affiliate.sales-commission');
