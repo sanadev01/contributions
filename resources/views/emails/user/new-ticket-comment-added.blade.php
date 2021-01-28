a8cef03c5f127a3e7de6a80c3063d1bf88a64903
sanadev01 <sanasheraz.dev@gmail.com>
2021-01-28 19:44:05 +0500

    mail change template

--- resources/views/emails/user/new-ticket-comment-added.blade.php
+++ resources/views/emails/user/new-ticket-comment-added.blade.php
@@ -1,8 +1,9 @@
 @component('mail::message')
-@lang('email.new-ticket-comment-added.Hello')! {{ $ticketComment->ticket->name }}
+@lang('email.new-ticket-comment-added.Hello')! {{ $ticketComment->ticket->user->name }}
 <br>
 @lang('email.new-ticket-comment-added.Add comment') {{ $ticketComment->ticket->getHumanID() }}
 <br>
+
 {!! $ticketComment->text !!}
 
 @component('mail::button', ['url' => route('login')])
