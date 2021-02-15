4517a1204c6d74daa250603123841380271f3625
sanadev01 <sanasheraz.dev@gmail.com>
2021-02-15 20:31:13 +0500

    deploy script modified

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,10 +1,10 @@
 cd /var/repos/hd
-git fetch
 git reset --hard
+git fetch
 git checkout master
-git pull
+git pull origin master
+rsync -av /var/repos/hd/ /var/www/hd
 cd /var/www/hd
-rsync -av /var/repos/hd /var/www/hd
 composer install --no-interaction
 php artisan migrate --force
 php artisan cache:clear --no-interaction
