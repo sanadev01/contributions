26a1111edfac171be495a9e133be7e943e0d4944
sanadev01 <sanasheraz.dev@gmail.com>
2021-02-19 16:01:03 +0500

    change in scan label

--- resources/views/livewire/label/scan-label.blade.php
+++ resources/views/livewire/label/scan-label.blade.php
@@ -12,15 +12,16 @@
             <th>@lang('orders.print-label.Action')</th>
         </tr>
         @foreach ($packagesRows as $key => $package)
-            <tr>
+            <tr id="{{ $key }}">
                 <td>
-                    <input type="text" class="form-control" name="tracking[{{$key}}][tracking_code]"
+                    {{ $package['tracking_code'] }}
+                    {{-- <input type="text" class="form-control" name="tracking[{{$key}}][tracking_code]"
                     value="{{ $package['tracking_code'] }}" wire:keydown.enter="getTrackingCode($event.target.value, {{$key}})">
                     @error("tracking.$key.tracking_code")
                     <div class="text-danger">
                         {{ $message }}
                     </div>
-                    @enderror
+                    @enderror --}}
                 </td>
                 <td>
                     {{ $package['client'] }}
@@ -43,7 +44,7 @@
                 <td>
                     
                     @if( $package['client'] )
-                        <a href="{{ route('admin.label.scan.show',$order) }}" target="_blank" class="btn btn-success mr-2" title="@lang('orders.import-excel.Download')">
+                        <a href="{{route('admin.label.scan.show',$order)}}" target="_blank" class="btn btn-success mr-2" onclick="addClass({{$key}})" title="@lang('orders.import-excel.Download')">
                             <i class="feather icon-download"></i>@lang('orders.import-excel.Download')
                         </a>
                     @endif
@@ -54,13 +55,13 @@
             </tr>
         
         @endforeach
-        <tr>
+        {{-- <tr>
             <td colspan="7">
                 <button class="btn btn-primary" role="button" type="button" wire:click='addRow'>
                     @lang('orders.print-label.Add Row')
                 </button>
             </td>
-        </tr>
+        </tr> --}}
     </table>
 @include('layouts.livewire.loading')
 </div>
