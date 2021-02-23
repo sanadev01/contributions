362cace98f597afd2080104db03d2b719faa2b90
sanadev01 <sanasheraz.dev@gmail.com>
2021-02-23 12:11:25 +0500

    changes revert, will do in the another branch that will get from master

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -262,8 +262,8 @@ class Order extends Model
 
         $battriesExtra = $shippingService->contains_battery_charges * ( $this->items()->batteries()->count() );
         $pefumeExtra = $shippingService->contains_perfume_charges * ( $this->items()->perfumes()->count() );
-        // $dangrousGoodsCost = $this->user->perfume ? 0 : $battriesExtra + $this->user->battery ? 0 : $pefumeExtra;
-        $dangrousGoodsCost = (isset($this->user->perfume) && $this->user->perfume == 1 ? 0 : $battriesExtra) + (isset($this->user->battery) && $this->user->battery == 1 ? 0 : $pefumeExtra);
+
+        $dangrousGoodsCost = $this->user->perfume? 0 : $battriesExtra; + $this->user->battery ? 0 : $pefumeExtra;
 
         $consolidation = $this->isConsolidated() ?  setting('CONSOLIDATION_CHARGES',0,null,true) : 0;
 
