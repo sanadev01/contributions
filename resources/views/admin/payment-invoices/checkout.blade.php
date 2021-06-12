dc1e5f9d2e8793622cdb9a73ca42bd5a22cbfcb5
sanadev01 <sanasheraz.dev@gmail.com>
2021-06-12 18:08:59 +0500

    add deposit

--- resources/views/admin/payment-invoices/checkout.blade.php
+++ resources/views/admin/payment-invoices/checkout.blade.php
@@ -24,71 +24,109 @@
                                 <p class="h5 dim">@lang('invoice.Checkout Message Detail')</p>
                                 <hr>
                                 <div class="grid-wrapper w-100">
-                                    @foreach (auth()->user()->billingInformations as $billingInfo)
+                                
+                                
+                                    <div class="controls row mb-1 align-items-center">
+                                        <label class="col-md-3 text-md-right" for="balance">Pay By HD Account<span class="text-danger"></span></label>
+                                        <div class="col-md-6">
+                                            <div class="vs-checkbox-con vs-checkbox-primary" title="Pay By HD Account">
+                                                <input type="radio" name="pay" onclick="checkPay();" value="1" required class="col-md-1" id="balance">
+                                                <span class="vs-checkbox vs-checkbox-lg">
+                                                    <span class="vs-checkbox--check">
+                                                        <i class="vs-icon feather icon-check"></i>
+                                                    </span>
+                                                </span>
+                                                <span class="h3 mx-2 text-primary my-0 py-0"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="controls row mb-1 align-items-center">
+                                        <label class="col-md-3 text-md-right" for="card">Pay By Card<span class="text-danger"></span></label>
+                                        <div class="col-md-6">
+                                            <div class="vs-checkbox-con vs-checkbox-primary" title="Pay By Card">
+                                                <input type="radio" name="pay" onclick="checkPay();" value="0" required class="col-md-1" id="card">
+                                                <span class="vs-checkbox vs-checkbox-lg">
+                                                    <span class="vs-checkbox--check">
+                                                        <i class="vs-icon feather icon-check"></i>
+                                                    </span>
+                                                </span>
+                                                <span class="h3 mx-2 text-primary my-0 py-0"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="ifBalance" style="display:none">
+                                    Your Current Balance is : {{ getBalance() }}
+                                </div>
+                                <div  id="ifCard" style="display:none">
+                                    <div class="grid-wrapper w-100">
+                                        @foreach (auth()->user()->billingInformations as $billingInfo)
+                                            <div class="card-wrapper h-auto my-2 w-100">
+                                                <input class="c-card" type="radio" name="billingInfo" id="{{$billingInfo->id}}" {{ request('billingInfo') == $billingInfo->id ? 'checked': '' }} value="{{$billingInfo->id}}">
+                                                <div class="card-content">
+                                                    <div class="card-state-icon"></div>
+                                                    <label for="{{$billingInfo->id}}" class="w-100">
+                                                        <div class="h5 py-1 px-2">
+                                                            <strong class="border-bottom-dark mr-2">@lang('invoice.Card No'):</strong> <span class="text-info">**** **** **** {{ substr ($billingInfo->card_no, -4)}}</span>
+                                                        </div>
+                                                        <div class="h5 py-1 px-2">
+                                                            <strong class="border-bottom-dark mr-2">@lang('invoice.Full Name')</strong> <span class="text-info">{{ $billingInfo->first_name }} {{ $billingInfo->last_name }}</span>
+                                                        </div>
+                                                        <div class="h5 py-1 px-2">
+                                                            <strong class="border-bottom-dark mr-2">@lang('invoice.Phone')#</strong> <span class="text-info">{{ $billingInfo->phone }}</span>
+                                                        </div>
+                                                        <div class="h5 py-1 px-2">
+                                                            <strong class="border-bottom-dark mr-2">@lang('invoice.Address')</strong> <span class="text-info">{{ $billingInfo->address }}</span>
+                                                        </div>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        @endforeach
                                         <div class="card-wrapper h-auto my-2 w-100">
-                                            <input class="c-card" type="radio" name="billingInfo" id="{{$billingInfo->id}}" {{ request('billingInfo') == $billingInfo->id ? 'checked': '' }} value="{{$billingInfo->id}}">
+                                            <input class="c-card" type="radio" name="billingInfo" id="new" {{ request('billingInfo') == "" ? 'checked': '' }} value="">
                                             <div class="card-content">
                                                 <div class="card-state-icon"></div>
-                                                <label for="{{$billingInfo->id}}" class="w-100">
+                                                <label for="new" class="w-100">
                                                     <div class="h5 py-1 px-2">
-                                                        <strong class="border-bottom-dark mr-2">@lang('invoice.Card No'):</strong> <span class="text-info">**** **** **** {{ substr ($billingInfo->card_no, -4)}}</span>
-                                                    </div>
-                                                    <div class="h5 py-1 px-2">
-                                                        <strong class="border-bottom-dark mr-2">@lang('invoice.Full Name')</strong> <span class="text-info">{{ $billingInfo->first_name }} {{ $billingInfo->last_name }}</span>
-                                                    </div>
-                                                    <div class="h5 py-1 px-2">
-                                                        <strong class="border-bottom-dark mr-2">@lang('invoice.Phone')#</strong> <span class="text-info">{{ $billingInfo->phone }}</span>
-                                                    </div>
-                                                    <div class="h5 py-1 px-2">
-                                                        <strong class="border-bottom-dark mr-2">@lang('invoice.Address')</strong> <span class="text-info">{{ $billingInfo->address }}</span>
+                                                        <strong class="border-bottom-dark mr-2">@lang('invoice.Create New')</strong>
+                                                        <p class="mt-4">@lang('invoice.Select to Add new Address You can Save address for later use.')</p>
                                                     </div>
                                                 </label>
                                             </div>
                                         </div>
-                                    @endforeach
-                                    <div class="card-wrapper h-auto my-2 w-100">
-                                        <input class="c-card" type="radio" name="billingInfo" id="new" {{ request('billingInfo') == "" ? 'checked': '' }} value="">
-                                        <div class="card-content">
-                                            <div class="card-state-icon"></div>
-                                            <label for="new" class="w-100">
-                                                <div class="h5 py-1 px-2">
-                                                    <strong class="border-bottom-dark mr-2">@lang('invoice.Create New')</strong>
-                                                    <p class="mt-4">@lang('invoice.Select to Add new Address You can Save address for later use.')</p>
-                                                </div>
-                                            </label>
-                                        </div>
                                     </div>
-                                </div>
-                                <div class="clearfix mt-4"></div>
-                                <div class="billingInfo-wrapper position-relative border p-4" style="display: none;cursor: default;">
-                                    <x-authorize-card billingInformationId=""></x-authorize-card>
-                                    <div class="row mt-4 p-4">
-                                        <div class="col-md-12">
-                                            <fieldset>
-                                                <div class="vs-checkbox-con vs-checkbox-primary">
-                                                    <input type="checkbox" name="save-address" value="false">
-                                                    <span class="vs-checkbox vs-checkbox-lg">
-                                                        <span class="vs-checkbox--check">
-                                                            <i class="vs-icon feather icon-check"></i>
+                                    <div class="clearfix mt-4"></div>
+                                    <div class="billingInfo-wrapper position-relative border p-4" style="display: none;cursor: default;">
+                                        <x-authorize-card billingInformationId=""></x-authorize-card>
+                                        <div class="row mt-4 p-4">
+                                            <div class="col-md-12">
+                                                <fieldset>
+                                                    <div class="vs-checkbox-con vs-checkbox-primary">
+                                                        <input type="checkbox" name="save-address" value="false">
+                                                        <span class="vs-checkbox vs-checkbox-lg">
+                                                            <span class="vs-checkbox--check">
+                                                                <i class="vs-icon feather icon-check"></i>
+                                                            </span>
                                                         </span>
-                                                    </span>
-                                                    <span class="">@lang('invoice.Save Billing Information')</span>
-                                                </div>
-                                            </fieldset>
+                                                        <span class="">@lang('invoice.Save Billing Information')</span>
+                                                    </div>
+                                                </fieldset>
+                                            </div>
                                         </div>
                                     </div>
+                                    <hr>
                                 </div>
-                                <hr>
-                                <div class="row mt-4 p-4">
-                                    <div class="col-md-12 text-right">
-                                        <button class="btn btn-lg btn-success" type="submit">
-                                            <i class="feather icon-dollar-sign"></i> @lang('invoice.Pay')
-                                        </button>
-                                        <a href="{{ route('admin.payment-invoices.index') }}" class="btn btn-lg btn-warning">
-                                            <i class="feather icon-x"></i> @lang('invoice.Cancel')
-                                        </a>
+                                    <div class="row mt-4 p-4">
+                                        <div class="col-md-12 text-right">
+                                            <button class="btn btn-lg btn-success" type="submit">
+                                                <i class="feather icon-dollar-sign"></i> @lang('invoice.Pay')
+                                            </button>
+                                            <a href="{{ route('admin.payment-invoices.index') }}" class="btn btn-lg btn-warning">
+                                                <i class="feather icon-x"></i> @lang('invoice.Cancel')
+                                            </a>
+                                        </div>
                                     </div>
-                                </div>
+                                
                             </div>
                         </form>
                     </div>
@@ -118,5 +156,23 @@
         $(function(){
             $('input[name=billingInfo]').trigger('change');
         })
+    </script>
+
+    <script>
+        function checkPay() {
+            
+            if(document.getElementById('balance').checked){
+                document.getElementById('ifBalance').style.display = 'block';
+                document.getElementById('ifCard').style.display = 'none';
+                $('form').attr('novalidate','novalidate');
+            }
+
+            if (document.getElementById('card').checked) {
+                document.getElementById('ifCard').style.display = 'block';
+                document.getElementById('ifBalance').style.display = 'none';
+                $('.billingInfo-wrapper').fadeIn();
+                $('form').removeAttr('novalidate');
+            }
+        }
     </script>
 @endsection
