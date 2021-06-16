f17bc0793bc7a9c17f8c8cdff2d05e803463ba2c
sanadev01 <sanasheraz.dev@gmail.com>
2021-06-16 15:10:00 +0500

    add refund

--- app/Http/Controllers/Api/OrderStatusController.php
+++ app/Http/Controllers/Api/OrderStatusController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\Deposit;
 use Illuminate\Http\Request;
+use App\Models\PaymentInvoice;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class OrderStatusController extends Controller
 {
@@ -13,6 +16,33 @@ class OrderStatusController extends Controller
         $order = Order::find($request->order_id);
 
         if ( $order ){
+            if($request->status == Order::STATUS_REFUND && $order->status != Order::STATUS_REFUND && $order->status == Order::STATUS_PAYMENT_DONE){
+                $deposit = Deposit::create([
+                    'uuid' => PaymentInvoice::generateUUID('DP-'),
+                    'amount' => $order->gross_total,
+                    'user_id' => $order->user_id,
+                    'balance' => Deposit::getCurrentBalance($order->user) + $order->gross_total,
+                    'is_credit' => true,
+                ]);
+        
+                if ( $order ){
+                    $order->deposits()->sync($deposit->id);
+                }
+            }
+            
+            if($request->status >= Order::STATUS_PAYMENT_DONE && $order->status == Order::STATUS_REFUND ){
+                $deposit = Deposit::create([
+                    'uuid' => PaymentInvoice::generateUUID('DP-'),
+                    'amount' => $order->gross_total,
+                    'user_id' => $order->user_id,
+                    'balance' => Deposit::getCurrentBalance($order->user) - $order->gross_total,
+                    'is_credit' => false,
+                ]);
+        
+                if ( $order ){
+                    $order->deposits()->sync($deposit->id);
+                }
+            }
             $order->update([
                 'status' => $request->status,
                 'is_paid' => $request->status >= Order::STATUS_PAYMENT_DONE ? true: false
