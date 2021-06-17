40d2980fb38bff55f14046b514ea16724ca46aa6
sanadev01 <sanasheraz.dev@gmail.com>
2021-06-17 11:58:53 +0500

    label heights and font sizes

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\User;
+use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Picqer\Barcode\BarcodeGeneratorPNG;
 
 class HomeController extends Controller
 {
@@ -15,6 +17,33 @@ class HomeController extends Controller
      */
     public function __invoke()
     {
-        return view('home');   
+        
+       
+        $order = Order::where('id', 2525)->with('recipient', 'items')->first();
+        $chile_response = json_decode($order->chile_response);
+        $description = $this->itemsDescription($order->items);
+        $date = \Carbon\Carbon::parse($order->updated_at)->format('d/m/Y H:i');
+
+        // dd($order->toArray());
+        return view('labels.chile.index', [
+            'order' => $order,
+            'chile_response' => $chile_response,
+            'description' => $description,
+            'date' => $date,
+            'barcodeNew' => new BarcodeGeneratorPNG(),
+        ]);
+        // return view('home');   
+    }
+
+    public function itemsDescription($items)
+    {
+        foreach($items as $item)
+        {
+            $itemDescription[] = $item->description;
+        }
+
+        $description = implode(" ", $itemDescription);
+
+        return $description;
     }
 }
--- resources/views/labels/chile/index.blade.php
+++ resources/views/labels/chile/index.blade.php
@@ -8,7 +8,7 @@
         
         @page {
             /* width height */
-            size: 9.2cm 7.7cm; 
+            size: 9cm 7.3cm; 
             margin: 0px;
             padding: 0px;
         }
@@ -19,59 +19,62 @@
         
     </style>
 </head>
-<body style="margin-bottom: -28px !important;">
+<body style="margin-bottom: 0px !important;">
     <div style="border: 0.8px solid black; margin: 0.07cm; border-right: 0.8px solid black;">
-      <table style="width:100%; height:7.3cm; border-width: none;" cellspacing="0" border="1" cellpadding="0">
-        <tr>
+      <table style="width:100%; height:7cm; border-width: none;" cellspacing="0" border="1" cellpadding="0">
+        <tr style="height: 0.283cm; font-size: 9px !important;">
             <td rowspan="3">
-                <img src="{{ asset('images/chile-logo.png') }}" alt="" style="width: 2cm; height: 0.5cm;">
+                <img src="{{ asset('images/chile-logo.png') }}" alt="" style="height: 0.85cm;">
             </td>
             <td>
-                <small style="font-size: 7px; text-align: center;">A.R.</small>
+                <small style="text-align: center;">A.R.</small>
             </td>
-            <td style="text-align: center;"><small style="font-size: 7px;">RDOC</small></td>
-            <td style="text-align: center;"><small style="font-size: 7px;">A.TEL</small></td>
-            <td style="text-align: center;"><small style="font-size: 7px;">P.D</small></td>
-            <td style="text-align: center;"><small style="font-size: 7px;">REEM</small></td>
-            <td style="text-align: center;"><small style="font-size: 7px;">PRG</small></td>
-            <td style="text-align: center;"><small style="font-size: 7px;">SAB</small></td>
-            <td style="text-align: center;"><small style="font-size: 7px;">FIVPS</small></td>
+            <td style="text-align: center;"><small>RDOC</small></td>
+            <td style="text-align: center;"><small>A.TEL</small></td>
+            <td style="text-align: center;"><small>P.D</small></td>
+            <td style="text-align: center;"><small>REEM</small></td>
+            <td style="text-align: center;"><small>PRG</small></td>
+            <td style="text-align: center;"><small>SAB</small></td>
+            <td style="text-align: center;"><small>FIVPS</small></td>
         </tr>
-        <tr style="height: 0.1cm;">
+        <tr style="height: 0.283cm; font-size: 9px !important;">
             <td colspan="8" style="font-size: 7px;">DE: <small style="font-size: 7px;">{{$order->sender_first_name}}  {{$order->sender_last_name}}</small></td>
         </tr>
-        <tr style="height: 0.1cm;">
-            <td colspan="9" style="border-right: none;">
+        <tr style="height: 0.283cm; font-size: 9px !important;">
+            <td colspan="8" style="border-right: none;">
                 <small style="font-size: 7px;">RUT :</small>
                 <small style="font-size: 7px;">CTA :</small>
                 <small style="margin-left: 2rem; font-size: 7px">Tel : {{$order->sender_phone}}</small>
             </td>
         </tr>
-        <tr style="height: 0px !important;">
-            <td rowspan="2" style="text-align: center; height: 2px !important;">
+        <tr style="height: 0.33cm !important; font-size: 9px !important;">
+            <td rowspan="2" style="text-align: center;">
                 <small style="font-size: 7px;">{{$date}}</small><br />
                 {{$chile_response->AbreviaturaServicio}}
             </td>
             <td colspan="8" style="height: 0.5px !important;"><small style="font-size: 7px;">Referencia : {{$order->customer_reference}}</small></td>
         </tr>
-        <tr style="height: 0px !important;">
-            <td colspan="4" style="height: 0.5px !important;">
-                <small style="font-size: 7px;">Description Del Production :</small><br>
+        <tr style="height: 0.33cm !important;">
+            <td colspan="6" style="height: 0.5px !important;">
+                <small style="font-size: 6px;">Description Del Production :</small><br>
                 <small style="font-size: 7px;">{{$description}}</small>
             </td>
-            <td colspan="5" style="border-right: none; height: 0.5px !important;"><small style="font-size: 7px;">Valor Declarado USD: {{$order->order_value}}</small></td>
-        </tr>
-        <tr style="height: 2.2cm;">
-            <td colspan="9">
-                <img src="data:image/png;base64,{{ base64_encode($barcodeNew->getBarcode($order->corrios_tracking_code, $barcodeNew::TYPE_CODE_128, 1,94, [0,0,0]))}}" alt="barcode" style="margin-left: 1.3cm; height: 1.5cm; width: 6.5cm; margin-right: 1.4cm; margin-top: 0.5cm; margin-bottom: 0.5cm;"   />
+            <td colspan="2" style="border-right: none;">
+                <small style="font-size: 5px; font-weight: bold !important;">Valor Declarado USD:</small>
+                <small style="font-size: 6px !important;">{{$order->order_value}}</small>
             </td>
         </tr>
-        <tr>
+        <tr style="height: 3cm;">
+            <td colspan="9">
+                <img src="data:image/png;base64,{{ base64_encode($barcodeNew->getBarcode($order->corrios_tracking_code, $barcodeNew::TYPE_CODE_128, 1,94, [0,0,0]))}}" alt="barcode" style="margin-left: 1.3cm; height: 2.1cm; width: 6.5cm; margin-right: 1.4cm; margin-top: 0.5cm; margin-bottom: 0.5cm;"   />
+            </td>
+        </tr>
+        <tr style="height: 0.5cm !important;">
             <td colspan="2"><small style="font-size: 7px;">Encaminamiento</small><br /><small style="font-size: 7px;">{{$chile_response->CodigoEncaminamiento}}</small></td>
             <td colspan="5"><small style="font-size: 7px;">N Envio :</small><br /> <small style="font-size: 7px; font-weight: bold;">{{$chile_response->NumeroEnvio}}</small></td>
             <td colspan="2" style="text-align: center; font-size: 7px;"><small style="font-size: 7px;">Bulto(s)</small><br>001</td>
         </tr>
-        <tr style="height: 1cm;">
+        <tr style="height: 1cm !important;">
             <td colspan="9">
                 <table style="font-size: 7px; width:100%">
                     <tr>
@@ -122,7 +125,7 @@
                 </table>
             </td>
         </tr>
-        <tr style="height: 0.5cm">
+        <tr style="height: 0.2cm !important">
             <td colspan="9" style="padding: 0px !important;">
                 <table cellspacing="0" cellpadding="0" style="margin: 0px !important;">
                     <tr>
@@ -142,27 +145,25 @@
                 </table>
             </td>
         </tr>
-        <tr>
+        <tr style="height: 0.6cm !important;">
             <td colspan="9" style="padding: 0px !important;">
                 <table cellspacing="0" cellpadding="0" style="margin: 0px !important;">
                     <tr>
-                        <td style="border-right: 1px solid; width: 58px; height: 18px; text-align: center;">
+                        <td style="border-right: 1px solid; width: 58px; text-align: center;">
                             <small style="font-size: 7px;"></small>    
                         </td>
-                        <td style="border-right: 1px solid; width: 150px; height: 18px; text-align: center;">
+                        <td style="border-right: 1px solid; width: 150px; text-align: center;">
                             <small style="font-size: 7px;">{{$chile_response->NombreDelegacionDestino}}</small>
                         </td>
-                        <td style="border-right: 1px solid;  width: 70px; height: 18px; text-align: center;">
+                        <td style="border-right: 1px solid;  width: 70px; text-align: center;">
                             <small style="font-size: 7px;"></small>
                         </td>
-                        <td style="border-right: 1px solid; width: 60px; height: 18px; text-align: center;">
+                        <td style="border-right: 1px solid; width: 60px; text-align: center;">
                             <small style="font-size: 7px;"></small>
                         </td>
                     </tr>
                 </table>
             </td>
-        </tr>
-            
         </tr>
       </table>
     </div> 
