d3d5c7948edf3b8fceacc826b266bd7ccb5779d1
sanadev01 <sanasheraz.dev@gmail.com>
2021-06-19 10:43:46 +0500

    update

--- resources/views/admin/warehouse/searchPackage/index.blade.php
+++ resources/views/admin/warehouse/searchPackage/index.blade.php
@@ -16,7 +16,7 @@
                     </div>
                     <div class="card-content card-body">
                         <div class="mt-1">
-                            
+                            <livewire:warehouse.search-package>
                         </div>
                     </div>
                 </div>
