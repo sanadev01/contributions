d51299bd1e61fbcbb1b71b13b036babd8e09929f
sanadev01 <sanasheraz.dev@gmail.com>
2021-07-26 16:43:27 +0500

    dimensions in single column

--- app/Services/Excel/Export/OrderExport.php
+++ app/Services/Excel/Export/OrderExport.php
@@ -44,24 +44,22 @@ class OrderExport extends AbstractExportService
             $this->setCellValue('H'.$row, $this->checkValue(number_format($order->dangrous_goods,2)));
             $this->setCellValue('I'.$row, $order->getWeight('kg'));
             $this->setCellValue('J'.$row, $order->getWeight('lbs'));
-            $this->setCellValue('K'.$row, $order->length);
-            $this->setCellValue('L'.$row, $order->width);
-            $this->setCellValue('M'.$row, $order->height);
+            $this->setCellValue('K'.$row, $order->length. ' X '. $order->width.' X '.$order->height);
 
             if($order->status == Order::STATUS_ORDER){
-                $this->setCellValue('N'.$row, 'ORDER');
+                $this->setCellValue('L'.$row, 'ORDER');
             }
             if($order->status == Order::STATUS_PAYMENT_PENDING){
-                $this->setCellValue('N'.$row, 'PAYMENT_PENDING');
+                $this->setCellValue('L'.$row, 'PAYMENT_PENDING');
             }
             if($order->status == Order::STATUS_PAYMENT_DONE){
-                $this->setCellValue('N'.$row, 'PAYMENT_DONE');
+                $this->setCellValue('L'.$row, 'PAYMENT_DONE');
             }
             if($order->status == Order::STATUS_SHIPPED){
-                $this->setCellValue('N'.$row, 'SHIPPED');
+                $this->setCellValue('L'.$row, 'SHIPPED');
             }
             
-            $this->setCellValue('O'.$row, $order->order_date);
+            $this->setCellValue('M'.$row, $order->order_date);
             
             $row++;
         }
@@ -71,7 +69,7 @@ class OrderExport extends AbstractExportService
         $this->setCellValue('I'.$row, "=SUM(I1:I{$row})");
         $this->setCellValue('J'.$row, "=SUM(J1:J{$row})");
         $this->mergeCells("A{$row}:G{$row}");
-        $this->setBackgroundColor("A{$row}:N{$row}", 'adfb84');
+        $this->setBackgroundColor("A{$row}:L{$row}", 'adfb84');
         $this->setAlignment('A'.$row, Alignment::VERTICAL_CENTER);
         $this->setCellValue('A'.$row, 'Total Order: '.$this->orders->count());
 
@@ -113,22 +111,17 @@ class OrderExport extends AbstractExportService
         $this->setCellValue('J1', 'Weight(Lbs)');
         
         $this->setColumnWidth('K', 20);
-        $this->setCellValue('K1', 'Length');
+        $this->sheet->getStyle('K')->getAlignment()->setHorizontal('center');
+        $this->setCellValue('K1', 'Dimesnsions');
 
         $this->setColumnWidth('L', 20);
-        $this->setCellValue('L1', 'Width');
+        $this->setCellValue('L1', 'Status');
 
         $this->setColumnWidth('M', 20);
-        $this->setCellValue('M1', 'Height');
+        $this->setCellValue('M1', 'Date');
 
-        $this->setColumnWidth('N', 20);
-        $this->setCellValue('N1', 'Status');
-
-        $this->setColumnWidth('O', 20);
-        $this->setCellValue('O1', 'Date');
-
-        $this->setBackgroundColor('A1:O1', '2b5cab');
-        $this->setColor('A1:O1', 'FFFFFF');
+        $this->setBackgroundColor('A1:M1', '2b5cab');
+        $this->setColor('A1:M1', 'FFFFFF');
 
         $this->currentRow++;
     }
