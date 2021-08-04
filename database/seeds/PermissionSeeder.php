e73c9b8230beca22de8b0b5822caf0265596f18e
sanadev01 <sanasheraz.dev@gmail.com>
2021-08-04 16:59:47 +0500

    remove conflict

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -414,15 +414,15 @@ class PermissionSeeder extends Seeder
                 'description' => 'User can Duplicate PreAlert'
             ],
             [
-                'slug' => 'update_label',
-                'group' => 'Order',
-                'description' => 'User can update label'
-            ],
-            [
                 'slug' => 'commission-report',
                 'group' => 'Reports',
                 'description' => 'User can see Commission Reports'
             ],
+            [
+                'slug' => 'update_label',
+                'group' => 'Order',
+                'description' => 'User can update label'
+            ]
             
         ];
     }
