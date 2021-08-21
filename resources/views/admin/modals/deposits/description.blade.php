4c7cb894c8e7d2d7279a7d7279c3cb04bdabb685
sanadev01 <sanasheraz.dev@gmail.com>
2021-08-21 10:55:14 +0500

    edit description

--- resources/views/admin/modals/deposits/description.blade.php
+++ resources/views/admin/modals/deposits/description.blade.php
@@ -2,7 +2,12 @@
     <h5 class="modal-title">Description</h5>
 </div>
 <div class="modal-body">
-    <p>{{ $description }}</p>
+    <form action="{{ route('admin.deposit.description.update',$deposit) }}" method="POST">
+        @csrf
+        <textarea class="form-control" rows="5" name="description">{{ $deposit->description }}</textarea>
+        {{-- <p>{{ $deposit->description }}</p> --}}
+        <button class="btn btn-primary mt-2">Update</button>
+    </form>
 </div>
 <div class="modal-footer">
     <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
