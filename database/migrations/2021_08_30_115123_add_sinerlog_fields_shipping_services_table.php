ee1bc80012bde1105dbb448316899a55871bac36
sanadev01 <sanasheraz.dev@gmail.com>
2021-08-30 09:09:14 -0300

    Sinerlog integration
    Upload the correct migration files

--- database/migrations/2021_08_30_115123_add_sinerlog_fields_shipping_services_table.php
+++ database/migrations/2021_08_30_115123_add_sinerlog_fields_shipping_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFieldsToShippingServicesTable extends Migration
+class AddSinerlogFieldsShippingServicesTable extends Migration
 {
     /**
      * Run the migrations.
