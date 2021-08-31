5e1bfe5d4ee670d626cddb9eee6b8c189e904551
sanadev01 <sanasheraz.dev@gmail.com>
2021-08-31 17:52:02 +0500

    change in tracking design

--- resources/views/livewire/order-tracking/trackings.blade.php
+++ resources/views/livewire/order-tracking/trackings.blade.php
@@ -7,28 +7,30 @@
             <button class="btn btn-primary btn-lg" wire:click="trackOrder">Search</button>
         </div>
     </div>
-    @if ($tracking)
-    {{ dd($tracking) }}
+   
+    {{-- 
+        @if($tracking->order->status >= App\Models\Order::STATUS_PAYMENT_DONE ) active @endif
+        @if($tracking->order->status >= App\Models\Order::STATUS_SHIPPED ) active @endif --}}
+        @if ($tracking)
+        
         <hr>
         <div class="row">
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
                         <h4 class="mb-0">
-                            Tracking Package
+                            Find Your Package
                         </h4>
-                        <div>
-                            <a href="{{ route('tracking.index') }}" class="btn btn-primary"> <i class="fa fa-search"></i> Tracking Package </a>
-                        </div>
+                        
                     </div>
                     
-                    <div class="card">
+                    {{-- <div class="card"> --}}
                         <div class="row col-12 d-flex justify-content-between px-3 top">
                             <div class="col-3 d-flex">
-                                <h6>HD ORDER WHR#: <span class="text-primary font-weight-bold">{{ $tracking->order->warehouse_number }}</span></h6>
+                                <h6>HD WHR#: <span class="text-primary font-weight-bold">{{ optional($tracking->order)->warehouse_number }}</span></h6>
                             </div>
                             <div class="col-3 d-flex">
-                                <h6>Tracking Number: <span class="text-primary font-weight-bold">{{ $tracking->order->corrios_tracking_code }}</span></h6>
+                                <h6>Tracking Number: <span class="text-primary font-weight-bold">{{ optional($tracking->order)->corrios_tracking_code }}</span></h6>
                             </div>
                             <div class="col-3 d-flex">
                                 <h6>Dispatch Number: <span class="text-primary font-weight-bold">{{ optional(optional($tracking->order->containers)[0])->dispatch_number }}</span></h6>
@@ -38,45 +40,64 @@
                             </div>
                             
                         </div>
+                        <hr>
                         <div class="row d-flex justify-content-center">
                             <div class="col-12">
                                 <ul id="progressbar" class="text-center">
-                                    <li class="@if($tracking->order->status >= App\Models\Order::STATUS_ORDER ) active @endif step0"></li>
-                                    <li class="@if($tracking->order->status >= App\Models\Order::STATUS_PAYMENT_DONE ) active @endif step0"></li>
-                                    <li class="@if($tracking->order->status >= App\Models\Order::STATUS_SHIPPED ) active @endif step0"></li>
-                                    {{-- <li class="@if($tracking->order->status >= App\Models\Order::STATUS_ORDER ) active @endif step0"></li>
-                                    <li class="@if($tracking->order->status >= App\Models\Order::STATUS_ORDER ) active @endif step0"></li> --}}
+                                    @if ($tracking->type == 'HD')
+                                        <li class="@if($tracking->status_code >=  70) active @endif step0"></li>
+                                        <li class="@if($tracking->status_code >=  73) active @endif step0"></li>
+                                        <li class="@if($tracking->status_code >=  75) active @endif step0"></li>
+                                        <li class="@if($tracking->status_code >=  80) active @endif step0"></li>
+                                        <li class="step0"></li>
+                                        <li class="step0"></li>
+                                    @else
+                                        <li class="active step0"></li>
+                                        <li class="active step0"></li>
+                                        <li class="active step0"></li>
+                                        <li class="active step0"></li>
+                                        <li class="@if($tracking->status_code =  01) active @endif step0"></li>
+                                        <li class="@if($tracking->status_code =  01) active @endif step0"></li>
+                                    @endif
+                                    
                                 </ul>
                             </div>
                         </div>
                         <div class="row justify-content-between top">
-                            <div class="row d-flex icon-content"> <img class="icon" src="{{ asset('images/order.png') }}">
+                            <div class="row d-flex icon-content"> <img class="icon" src="{{ asset('images/tracking/to-hd.png') }}">
                                 <div class="d-flex flex-column">
-                                    <p class="font-weight-bold">Order<br>Processed</p>
+                                    <p class="font-weight-bold">Freight  come <br> from customer </p>
                                 </div>
                             </div>
-                            {{-- <div class="row d-flex icon-content"> <img class="icon" src="https://i.imgur.com/GiWFtVu.png">
+                            <div class="row d-flex icon-content"> <img class="icon" src="{{ asset('images/tracking/hd-whr.png') }}">
                                 <div class="d-flex flex-column">
-                                    <p class="font-weight-bold">Order<br>Designing</p>
+                                    <p class="font-weight-bold">Warehouse <br> Receive</p>
                                 </div>
                             </div>
-                            <div class="row d-flex icon-content"> <img class="icon" src="https://i.imgur.com/u1AzR7w.png">
+                            <div class="row d-flex icon-content"> <img class="icon" src="{{ asset('images/tracking/container.png') }}">
                                 <div class="d-flex flex-column">
-                                    <p class="font-weight-bold">Order<br>Shipped</p>
-                                </div>
-                            </div> --}}
-                            <div class="row d-flex icon-content"> <img class="icon" src="{{ asset('images/onway.png') }}">
-                                <div class="d-flex flex-column">
-                                    <p class="font-weight-bold">Order<br>En Route</p>
+                                    <p class="font-weight-bold">Parcels Scan in <br> the Container </p>
                                 </div>
                             </div>
-                            <div class="row d-flex icon-content"> <img class="icon" src="{{ asset('images/shipped.png') }}">
+                            
+                            <div class="row d-flex icon-content"> <img class="icon" src="{{ asset('images/tracking/to-air.png') }}">
                                 <div class="d-flex flex-column">
-                                    <p class="font-weight-bold">Order<br>Shipped</p>
+                                    <p class="font-weight-bold">Container <br> goto Airport </p>
+                                </div>
+                            </div>
+                            
+                            <div class="row d-flex icon-content"> <img class="icon" src="{{ asset('images/tracking/awb.png') }}">
+                                <div class="d-flex flex-column">
+                                    <p class="font-weight-bold">Airplane <br> Departure MIA</p>
+                                </div>
+                            </div>
+                            <div class="row d-flex icon-content"> <img class="icon" src="{{ asset('images/tracking/correios.png') }}">
+                                <div class="d-flex flex-column">
+                                    <p class="font-weight-bold">Arrival in <br> Destination</p>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    {{-- </div> --}}
                 </div>
             </div>
         </div>
--- resources/views/ordertracking/index.blade.php
+++ resources/views/ordertracking/index.blade.php
@@ -1,5 +1,122 @@
 @extends('layouts.app')
+@section('css')
+<style>
 
+
+.card {
+    z-index: 0; 
+}
+
+.top {
+    padding-top: 40px;
+    padding-left: 8% !important;
+    padding-right: 7% !important
+}
+
+#progressbar {
+    margin-bottom: 30px;
+    overflow: hidden;
+    color: #455A64;
+    padding-left: 0px;
+    margin-top: 30px
+}
+
+#progressbar li {
+    list-style-type: none;
+    font-size: 13px;
+    width: 16%;
+    float: left;
+    position: relative;
+    font-weight: 400
+}
+
+#progressbar .step0:before {
+    font-family: FontAwesome;
+    content: "\f10c";
+    color: #fff
+}
+
+#progressbar li:before {
+    width: 40px;
+    height: 40px;
+    line-height: 45px;
+    display: block;
+    font-size: 20px;
+    background: #C5CAE9;
+    border-radius: 50%;
+    margin: auto;
+    padding: 0px
+}
+
+#progressbar li:after {
+    content: '';
+    width: 100%;
+    height: 12px;
+    background: #C5CAE9;
+    position: absolute;
+    left: 0;
+    top: 16px;
+    z-index: -1
+}
+
+#progressbar li:last-child:after {
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+    position: absolute;
+    left: -50%
+}
+
+#progressbar li:nth-child(2):after,
+#progressbar li:nth-child(3):after,
+#progressbar li:nth-child(4):after,
+#progressbar li:nth-child(5):after {
+    left: -50%
+}
+
+#progressbar li:first-child:after {
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    position: absolute;
+    left: 50%
+}
+
+#progressbar li:last-child:after {
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px
+}
+
+#progressbar li:first-child:after {
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px
+}
+
+#progressbar li.active:before,
+#progressbar li.active:after {
+    background: #651FFF
+}
+
+#progressbar li.active:before {
+    font-family: FontAwesome;
+    content: "\f00c"
+}
+
+.icon {
+    width: 60px;
+    height: 60px;
+    margin-right: 15px
+}
+
+.icon-content {
+    padding-bottom: 20px
+}
+
+@media screen and (max-width: 992px) {
+    .icon-content {
+        width: 50%
+    }
+}
+</style>
+@endsection
 @section('content')
     <div class="">
         <div class="row">
