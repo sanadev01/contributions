b7a390d20b6dc257ce800763d2a7b07b8aa60b3a
sanadev01 <sanasheraz.dev@gmail.com>
2021-09-07 15:26:56 +0500

    usps rates with and without profit

--- resources/views/uspscalculator/show.blade.php
+++ resources/views/uspscalculator/show.blade.php
@@ -55,6 +55,60 @@
                             </div>
                         </div>
                     </div>
+                    @auth
+                        @if (auth()->user()->hasRole('admin'))
+                        <div class="card p-2">
+                            <div class="card-header pb-0">
+                                <h2 class="mb-2 text-center w-100">
+                                    Rate Calculated For USPS (without Profit)
+                                </h2>
+                            </div>
+
+                            <div class="col-md-12">
+                                <x-flash-message></x-flash-message>
+                            </div>
+
+                            <div class="card-body">
+                                @if ($usps_rates != null)
+                                    <div class="text-center">
+                                        @foreach ($usps_rates as $usps_rate) 
+                                            <div class="card-body"><div class="row justify-content-center mb-2 full-height align-items-center"><div class="col-10"><div class="row justify-content-center"><div class="pb-1 pt-1 border-bottom-light col-md-5 bg-primary text-white">
+                                                Service Name
+                                            </div> <div class="border col-5 py-1">
+                                                {{$usps_rate['name']}}
+                                            </div></div> <div class="row justify-content-center"><div class="pb-1 pt-1 border-bottom-light col-md-5 bg-primary text-white">
+                                                Weight
+                                            </div> <div class="border col-5 py-1">
+                                                @if($order->measurement_unit == 'kg/cm')
+                                                    {{$chargableWeight}} Kg ( {{$weightInOtherUnit}} lbs)
+                                                @else
+                                                    {{$chargableWeight}} lbs ( {{$weightInOtherUnit}} kg)
+                                                @endif
+                                            </div></div> <div class="row justify-content-center"><div class="pb-1 pt-1 border-bottom-light col-md-5 bg-primary text-white">
+                                                Cost
+                                            </div> <div class="border col-5 py-1 text-danger h2">
+
+                                                {{$usps_rate['rate']}} USD
+                                            
+                                                <br>
+                                            
+                                            </div></div></div></div></div>
+                                            <hr>
+                                        @endforeach
+                                    </div>
+                                @endif
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-12 d-flex justify-content-center">
+                                    <a href="{{route('usps-calculator.index')}}" class="btn btn-primary btn-lg">
+                                            Go Back
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                    @endauth
                 </div>
             </div>
         </div>
