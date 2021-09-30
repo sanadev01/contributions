0ed5dba6d3df270e57f405505ef6e0c495a6fbdd
sanadev01 <sanasheraz.dev@gmail.com>
2021-09-30 16:31:34 +0500

    fix clear search issue

--- app/Http/Livewire/Label/ScanLabel.php
+++ app/Http/Livewire/Label/ScanLabel.php
@@ -25,7 +25,9 @@ class ScanLabel extends Component
     public $totalWeight = 0;
     public $totalPieces = 0;
 
-    protected $listeners = ['user:updated' => 'getUser'];
+    protected $listeners = ['user:updated' => 'getUser',
+                             'clear-search' => 'removeUser'   
+                            ];
 
     public function mount()
     {
@@ -195,6 +197,11 @@ class ScanLabel extends Component
     public function getUser($userId)
     {
         $this->user_id = $userId;
+    }
+
+    public function removeUser()
+    {
+        $this->user_id = null;
     }
 
     public function calculateTotalWeight()
