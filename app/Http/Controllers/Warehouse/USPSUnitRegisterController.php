fd21c770ef414996f4f7e01a5d37c69070df8b6f
sanadev01 <sanasheraz.dev@gmail.com>
2021-10-12 17:57:42 +0500

    change tracking description text for order shipped

--- app/Http/Controllers/Warehouse/USPSUnitRegisterController.php
+++ app/Http/Controllers/Warehouse/USPSUnitRegisterController.php
@@ -55,7 +55,7 @@ class USPSUnitRegisterController extends Controller
                 'order_id' => $order->id,
                 'status_code' => Order::STATUS_SHIPPED,
                 'type' => 'HD',
-                'description' => 'Homedelivery sent parcel to airport with CN35',
+                'description' => 'Homedelivery sent parcel to airport',
                 'country' => 'US',
                 'city' => 'Miami'
             ]);
--- app/Repositories/Warehouse/DeliveryBillRepository.php
+++ app/Repositories/Warehouse/DeliveryBillRepository.php
@@ -141,7 +141,7 @@ class DeliveryBillRepository extends AbstractRepository
             'order_id' => $order_id,
             'status_code' => Order::STATUS_SHIPPED,
             'type' => 'HD',
-            'description' => 'Homedelivery sent parcel to airport with CN35',
+            'description' => 'Homedelivery sent parcel to airport',
             'country' => 'US',
             'city' => 'Miami'
         ]);
