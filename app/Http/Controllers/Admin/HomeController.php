c7b3010061486aaf50e81b25771cb4b759d6e5b4
sanadev01 <sanasheraz.dev@gmail.com>
2022-01-07 17:41:09 +0500

    revert seeder commands

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Artisan;
-
 use Illuminate\Support\Facades\Session;
-use App\Repositories\DashboardRepository;
 
+use App\Repositories\DashboardRepository;
 class HomeController extends Controller
 {
     /**
@@ -21,21 +19,11 @@ class HomeController extends Controller
     {
         $orders = $dashboard->getDashboardStats();
         
-        $this->test();
         return view('home',compact('orders'));   
     }
     
     public function test()
     {
-        
-        try {
-            Artisan::call('db:seed --class=RegionSeeder');
-            Artisan::call('db:seed --class=CommuneSeeder');
-
-            return true;
-        } catch (\Exception $ex) {
-            dd($ex->getMessage());
-        }
-        
+        return true;
     }
 }
