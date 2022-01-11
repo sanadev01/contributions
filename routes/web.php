9a97faf93f106baf76ea9d837aa80c4239b9ecbf
sanadev01 <sanasheraz.dev@gmail.com>
2022-01-11 14:21:46 +0500

    fix regions seeder for prodution

--- routes/web.php
+++ routes/web.php
@@ -247,6 +247,10 @@ Route::get('test-profit/{id}',function($id){
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index')->middleware('auth');
 
 Route::get('/tests', function() {
+
+    \Artisan::call('db:seed --class=RegionSeeder');
+    \Artisan::call('db:seed --class=CommuneSeeder');
+
     $regions = \App\Models\Region::get();
     $communes = \App\Models\Commune::get();
 
