3689795ff3031bae5ec830c874b3fa7887ee198e
sanadev01 <sanasheraz.dev@gmail.com>
2022-03-18 01:01:53 +0500

    add amazon

--- resources/views/components/shared-menu.blade.php
+++ resources/views/components/shared-menu.blade.php
@@ -5,6 +5,12 @@
     </a>
 </li>
 <li class="nav-item">
+    <a class="nav-link" target="__blank" href="https://amazon.aleem.dev/user-generate-Api/ecqypVh9sTO5cTuuQbhgGwK5TaihYEf1SMVgSYBQhtivv9BNeov1Ppoldy67un8U4AmQmyJsTQl5YGGfCjmxHfP0LDyc2c9IGZqB">
+        <i class="fa fa-list-alt" style="color: #90f3bc;"></i>
+        <span data-i18n="Apps"> Amazon Fulfilment </span>
+    </a>
+</li>
+<li class="nav-item">
     <a class="nav-link" target="__blank" href="https://documenter.getpostman.com/view/16057364/TzeXmSxT">
         <i class="fa fa-list-alt" style="color: #28c76f;"></i>
         <span data-i18n="Apps"> @lang('menu.API Documents') </span>
