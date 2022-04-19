16cbf844b384589130b76e44c2fa8dcf8154c686
sanadev01 <sanasheraz.dev@gmail.com>
2022-04-19 22:44:12 +0500

    change amazon api

--- resources/views/components/shared-menu.blade.php
+++ resources/views/components/shared-menu.blade.php
@@ -9,7 +9,7 @@
     @if($user->amazon_api_enabled && $user->api_enabled && $user->api_token)
         <li class="nav-item" style="background: #f79400;color: #fff;font-weight: 700;font-size: 16px;">
             <a class="nav-link" target="__blank"
-             href="http://hd-v2.test/test-label/?@if($user->amazon_api_key){{ 'token='.$user->amazon_api_key }}@else{{'first_name='.$user->name.'&last_name='.$user->last_name.'&email='.$user->email.'&phone='.$user->phone.'&api_token='.$user->api_token.'&city='.$user->city.'&street_no='.$user->street_no.'&address='.$user->address.'&address2='.$user->address2.'&zipcode='.$user->zipcode.'&state_id='.$user->state_id.'&country_id='.$user->country_id}}@endif">
+             href="https://app.labelsup.com/api-token-login?@if($user->amazon_api_key){{ 'token='.$user->amazon_api_key }}@else{{'first_name='.$user->name.'&last_name='.$user->last_name.'&email='.$user->email.'&phone='.$user->phone.'&api_token='.$user->api_token.'&city='.$user->city.'&street_no='.$user->street_no.'&address='.$user->address.'&address2='.$user->address2.'&zipcode='.$user->zipcode.'&state_id='.$user->state_id.'&country_id='.$user->country_id}}@endif">
              <img src="{{ asset('images/icon/amazon.svg') }}" alt="amazon api" width="20px">
                 <span data-i18n="Apps"style="color: #fff;"> Amazon Fulfilment</span>
             </a>
