fcdff1f5a245f85243e755defc1b1c259b0f5c80
sanadev01 <sanasheraz.dev@gmail.com>
2022-04-20 17:48:40 +0500

    amazon api to production

--- resources/views/components/shared-menu.blade.php
+++ resources/views/components/shared-menu.blade.php
@@ -1,9 +1,21 @@
 <li class="nav-item">
     <a class="nav-link" href="{{ route('admin.profile.index') }}">
-        <img src="{{ asset('images/icon/profile.svg') }}" alt="Orders">
+        <img src="{{ asset('images/icon/profile.svg') }}" alt="Profile">
         <span data-i18n="Apps"> @lang('menu.profile') </span>
     </a>
 </li>
+@user
+    @php $user = Auth()->user(); @endphp
+    @if($user->amazon_api_enabled && $user->api_enabled && $user->api_token)
+        <li class="nav-item" style="background: #f79400;color: #fff;font-weight: 700;font-size: 16px;">
+            <a class="nav-link" target="__blank"
+             href="https://app.labelsup.com/api-token-login?@if($user->amazon_api_key){{ 'token='.$user->amazon_api_key }}@else{{'first_name='.$user->name.'&last_name='.$user->last_name.'&email='.$user->email.'&phone='.$user->phone.'&api_token='.$user->api_token.'&city='.$user->city.'&street_no='.$user->street_no.'&address='.$user->address.'&address2='.$user->address2.'&zipcode='.$user->zipcode.'&state_id='.$user->state_id.'&country_id='.$user->country_id}}@endif">
+             <img src="{{ asset('images/icon/amazon.svg') }}" alt="amazon api" width="20px">
+                <span data-i18n="Apps"style="color: #fff;"> Amazon Fulfilment</span>
+            </a>
+        </li>
+    @endif
+@enduser
 <li class="nav-item">
     <a class="nav-link" target="__blank" href="https://documenter.getpostman.com/view/16057364/TzeXmSxT">
         <i class="fa fa-list-alt" style="color: #28c76f;"></i>
