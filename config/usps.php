10db71a5223201e3463354a26e4e24daf3374c7f
sanadev01 <sanasheraz.dev@gmail.com>
2022-04-27 22:38:53 +0500

    adds usps production credentials

--- config/usps.php
+++ config/usps.php
@@ -2,14 +2,14 @@
 
 return [
     'production' => [
-        'createLabelUrl' => env('USPS_URL', 'https://api.myibservices.com/v1/labels'),
-        'deleteLabelUrl' => env('USPS_DELETE_LABEL_URL', 'https://api.myibservices.com/v1/labels/'),
-        'createManifestUrl' => env('USPS_CREATE_MANIFEST_URL', 'https://api.myibservices.com/v1/manifests.json'),
-        'getPriceUrl' => env('USPS_GET_PRICE_URL', 'https://api.myibservices.com/v1/price.json'),
-        'trackingUrl' => env('USPS_TRACKING_URL', 'https://api.myibservices.com/v1/track/'),
+        'createLabelUrl' => 'https://api.myibservices.com/v1/labels',
+        'deleteLabelUrl' => 'https://api.myibservices.com/v1/labels/',
+        'createManifestUrl' => 'https://api.myibservices.com/v1/manifests.json',
+        'getPriceUrl' => 'https://api.myibservices.com/v1/price.json',
+        'trackingUrl' => 'https://api.myibservices.com/v1/track/',
         'addressValidationUrl' => 'https://api.myibservices.com/v1/address/validate',
-        'email' => env('USPS_Email', 'marcio.freitas@hercoinc.com'),
-        'password' => env('USPS_Password', 'Marcio@1504'),
+        'email' => 'marcio.freitas@hercoinc.com',
+        'password' => '150495Ca$',
     ],
     'testing' => [
         'createLabelUrl' => 'https://api-sandbox.myibservices.com/v1/labels',
