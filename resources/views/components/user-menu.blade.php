85f64a0eb07e7e47fa18f90522d4518791d048fd
sanadev01 <sanasheraz.dev@gmail.com>
2022-06-16 23:12:33 +0500

    sidebar content fonts

--- resources/views/components/user-menu.blade.php
+++ resources/views/components/user-menu.blade.php
@@ -140,7 +140,7 @@
 
             <li class="nav-item has-sub sidebar-group">
                 <a href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg"  height="15px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dollar-sign"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+                    <svg viewBox="0 0 24 24" height="15" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg>
                     <span class="menu-title" data-i18n="Dashboard">@lang('menu.Rates')</span>
                 </a>
                 <ul class="menu-content">
@@ -213,7 +213,7 @@
                 <li class="nav-item {{ $isActive(['admin.payment-invoices.index','admin.payment-invoices.edit']) }}">
                     <a href="{{ route('admin.payment-invoices.index') }}">
                         <svg xmlns="http://www.w3.org/2000/svg"  height="15px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dollar-sign"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
-                        <span class="menu-title">@lang('menu.payment-invoice')</span>
+                        <span class="menu-title" id="invoice">@lang('menu.payment-invoice')</span>
                     </a>
                 </li>
             @endcan
