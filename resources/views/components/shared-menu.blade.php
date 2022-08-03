29c6ac85e484fc65740e119b49d49d6b1e454214
sanadev01 <sanasheraz.dev@gmail.com>
2022-08-03 17:31:49 +0500

    Box Control icon update

--- resources/views/components/shared-menu.blade.php
+++ resources/views/components/shared-menu.blade.php
@@ -15,8 +15,12 @@
 @if(!auth()->user()->hideBoxControl())
 <li class="nav-item">
     <a class="nav-link" target="__blank" href="https://app.ideainfo.com.br/index.php?app=boxcontrol">
-        <i class="fa fa-bold" style="font-size: 10px"></i>
-        <span data-i18n="Apps"> Box Control </span>
+        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em"
+            preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="none"
+            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+            stroke-width="2" d="M6 4h8a4 4 0 0 1 4 4a4 4 0 0 1-4 4H6zm0 8h9a4 4 0 0 1 4 4a4 4 0 0 1-4 4H6z"/>
+        </svg>
+        <span class="menu-title"> Box Control </span>
     </a>
 </li>
 @endif
