b65e5ebf9ffff642d3c76bbf75333253becd64bc
sanadev01 <sanasheraz.dev@gmail.com>
2022-08-11 15:35:11 +0500

    update delivery bill hot fix

--- resources/views/admin/warehouse/deliverybills/index.blade.php
+++ resources/views/admin/warehouse/deliverybills/index.blade.php
@@ -80,11 +80,13 @@
                                         </td>
                                         <td>
                                             {{ $deliveryBill->name }}
-                                            {{-- @if(optional(optional(optional(optional(optional($deliveryBill)->Containers)->first())->orders->first())->shippingService)->isAnjunService())
+                                            @if ($deliveryBill->Containers->count() > 0)
+                                                @if($deliveryBill->Containers->first()->orders->first()->shippingService->isAnjunService())
                                                 <span class="badge badge-success">A</span>
-                                            @else
+                                                @else
                                                 <span class="badge badge-primary">H</span>
-                                            @endif --}}
+                                                @endif
+                                            @endif
                                         </td>
                                         <td>
                                             {{ $deliveryBill->request_id }}
