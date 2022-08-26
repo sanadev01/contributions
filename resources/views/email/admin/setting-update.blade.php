e0f4cc14eded9f2fca3d8bdd5a437a18c64ee575
sanadev01 <sanasheraz.dev@gmail.com>
2022-08-27 01:00:45 +0500

    update mail setting report

--- resources/views/email/admin/setting-update.blade.php
+++ resources/views/email/admin/setting-update.blade.php
@@ -2,33 +2,39 @@
 Hello, <br>
 Login User details<br>
 IP address: {{ $request->ip() }}<br>
-Admin UserName: {{ auth()->user()->name }} {{ auth()->user()->last_name }} Pobox No.: {{ auth()->user()->pobox_number }} setting has been updated on 
+<strong>Admin UserName:</strong> {{ auth()->user()->name }} {{ auth()->user()->last_name }} <strong>Pobox No.:</strong> {{ auth()->user()->pobox_number }} setting has been updated on 
 <strong>Date:</strong> {{ $user->updated_at->format('Y-m-d') }} 
 <strong>Time:</strong> {{ $user->updated_at->format('g:i:s a') }}<br>
 User Setting's Details<br>
-Username:{{ $user->name }} {{ $user->last_name }} Pobox No.: {{ $user->pobox_number }}<br>
+<strong>Username:</strong> {{ $user->name }} {{ $user->last_name }} <strong>Pobox No.:</strong> {{ $user->pobox_number }}<br>
 Which has following Diffrence <br>
 
 @component('mail::table')
 | Setting Name  | Old Values    | New Values	|
 | ------------- |:-------------:| --------:		|
-| Battery		| {{ $oldData['battery'] }}   	| {{$request->battery  ? 'Active':'Inactive'}}			|      	
-| Perfume		| {{ $oldData['perfume'] }} | {{$request->perfume  ? 'Active':'Inactive'}}			|     
-| Insurance		| {{ $oldData['insurance'] }} | {{$request->insurance  ? 'Active':'Inactive'}}			|      	  
-| USPS			| {{ $oldData['usps'] }} | {{$request->usps  ? 'Active':'Inactive'}}			|     
-| UPS			| {{ $oldData['ups'] }} | {{$request->ups  ? 'Active':'Inactive'}}			|
-| Sinerlog		| {{ $oldData['sinerlog'] }} | {{$request->sinerlog  ? 'Active':'Inactive'}}			|     
-| Fedex			| {{ $oldData['fedex'] }} | {{$request->fedex  ? 'Active':'Inactive'}}			|
-| tax			| {{ $oldData['tax'] }} | {{$request->tax  ? 'Active':'Inactive'}}			|     
-| Vol Discount	| {{ $oldData['volumetric_discount'] }} | {{$request->volumetric_discount ? 'Active':'Inactive'}}			|
-| UPS Profit	| {{ $oldData['ups_profit'] }} | {{$request->ups_profit}}			|     
-| USPS Profit	| {{ $oldData['usps_profit'] }} | {{$request->usps_profit}}			|     
-| Discount %	| {{ $oldData['discount_percentage'] }} | {{$request->discount_percentage}}			|
-| Fedex Profit	| {{ $oldData['fedex_profit'] }} | {{$request->fedex_profit}}			|     
-| Weight		| {{ $oldData['weight'] }} | {{$request->weight}}			|
-| Length		| {{ $oldData['length'] }} | {{$request->length}}			|     
-| Width			| {{ $oldData['width'] }} | {{$request->width}}			|
-| Height		| {{ $oldData['height'] }} | {{$request->height}}			|
+| Battery		| {{ $userData['battery'] }}   	| {{$request->battery  ? 'Active':'Inactive'}}			|      	
+| Perfume		| {{ $userData['perfume'] }} | {{$request->perfume  ? 'Active':'Inactive'}}			|     
+| Insurance		| {{ $userData['insurance'] }} | {{$request->insurance  ? 'Active':'Inactive'}}			|      	  
+| USPS			| {{ $userData['usps'] }} | {{$request->usps  ? 'Active':'Inactive'}}			|     
+| UPS			| {{ $userData['ups'] }} | {{$request->ups  ? 'Active':'Inactive'}}			|
+| Sinerlog		| {{ $userData['sinerlog'] }} | {{$request->sinerlog  ? 'Active':'Inactive'}}			|     
+| Fedex			| {{ $userData['fedex'] }} | {{$request->fedex  ? 'Active':'Inactive'}}			|
+| tax			| {{ $userData['tax'] }} | {{$request->tax  ? 'Active':'Inactive'}}			|     
+| Vol Discount	| {{ $userData['volumetric_discount'] }} | {{$request->volumetric_discount ? 'Active':'Inactive'}}			|
+| UPS Profit	| {{ $userData['ups_profit'] }} | {{$request->ups_profit}}			|     
+| USPS Profit	| {{ $userData['usps_profit'] }} | {{$request->usps_profit}}			|     
+| Discount %	| {{ $userData['discount_percentage'] }} | {{$request->discount_percentage}}			|
+| Fedex Profit	| {{ $userData['fedex_profit'] }} | {{$request->fedex_profit}}			|     
+| Weight		| {{ $userData['weight'] }} | {{$request->weight}}			|
+| Length		| {{ $userData['length'] }} | {{$request->length}}			|     
+| Width			| {{ $userData['width'] }} | {{$request->width}}			|
+| Height		| {{ $userData['height'] }} | {{$request->height}}			|
 @endcomponent
 
+@component('mail::button', ['url' => route('login') ])
+Dashboard
+@endcomponent
+
+@lang('email.contactUs') <br>
+@lang('email.Home DeliveryBR Team')
 @endcomponent
