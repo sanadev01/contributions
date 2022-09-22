bfd1168154010803e1a09edb1cb498456abd0f77
sanadev01 <sanasheraz.dev@gmail.com>
2022-09-23 00:50:55 +0500

    PostNL Rates Update

--- app/Services/Excel/ImportCharges/ImportPostNLRates.php
+++ app/Services/Excel/ImportCharges/ImportPostNLRates.php
@@ -134,6 +134,172 @@ class ImportPostNLRates extends AbstractImportService
         $this->countryCodes['CX'] = $this->workSheet->getCell('CX1')->getValue();
         $this->countryCodes['CY'] = $this->workSheet->getCell('CY1')->getValue();
         $this->countryCodes['CZ'] = $this->workSheet->getCell('CZ1')->getValue();
+        $this->countryCodes['DA'] = $this->workSheet->getCell('DA1')->getValue();
+        $this->countryCodes['DB'] = $this->workSheet->getCell('DB1')->getValue();
+        $this->countryCodes['DC'] = $this->workSheet->getCell('DC1')->getValue();
+        $this->countryCodes['DD'] = $this->workSheet->getCell('DD1')->getValue();
+        $this->countryCodes['DE'] = $this->workSheet->getCell('DE1')->getValue();
+        $this->countryCodes['DF'] = $this->workSheet->getCell('DF1')->getValue();
+        $this->countryCodes['DG'] = $this->workSheet->getCell('DG1')->getValue();
+        $this->countryCodes['DH'] = $this->workSheet->getCell('DH1')->getValue();
+        $this->countryCodes['DI'] = $this->workSheet->getCell('DI1')->getValue();
+        $this->countryCodes['DJ'] = $this->workSheet->getCell('DJ1')->getValue();
+        $this->countryCodes['DK'] = $this->workSheet->getCell('DK1')->getValue();
+        $this->countryCodes['DL'] = $this->workSheet->getCell('DL1')->getValue();
+        $this->countryCodes['DM'] = $this->workSheet->getCell('DM1')->getValue();
+        $this->countryCodes['DN'] = $this->workSheet->getCell('DN1')->getValue();
+        $this->countryCodes['DO'] = $this->workSheet->getCell('DO1')->getValue();
+        $this->countryCodes['DP'] = $this->workSheet->getCell('DP1')->getValue();
+        $this->countryCodes['DQ'] = $this->workSheet->getCell('DQ1')->getValue();
+        $this->countryCodes['DR'] = $this->workSheet->getCell('DR1')->getValue();
+        $this->countryCodes['DS'] = $this->workSheet->getCell('DS1')->getValue();
+        $this->countryCodes['DT'] = $this->workSheet->getCell('DT1')->getValue();
+        $this->countryCodes['DU'] = $this->workSheet->getCell('DU1')->getValue();
+        $this->countryCodes['DV'] = $this->workSheet->getCell('DV1')->getValue();
+        $this->countryCodes['DW'] = $this->workSheet->getCell('DW1')->getValue();
+        $this->countryCodes['DX'] = $this->workSheet->getCell('DX1')->getValue();
+        $this->countryCodes['DY'] = $this->workSheet->getCell('DY1')->getValue();
+        $this->countryCodes['DZ'] = $this->workSheet->getCell('DZ1')->getValue();
+        $this->countryCodes['EA'] = $this->workSheet->getCell('EA1')->getValue();
+        $this->countryCodes['EB'] = $this->workSheet->getCell('EB1')->getValue();
+        $this->countryCodes['EC'] = $this->workSheet->getCell('EC1')->getValue();
+        $this->countryCodes['ED'] = $this->workSheet->getCell('ED1')->getValue();
+        $this->countryCodes['EE'] = $this->workSheet->getCell('EE1')->getValue();
+        $this->countryCodes['EF'] = $this->workSheet->getCell('EF1')->getValue();
+        $this->countryCodes['EG'] = $this->workSheet->getCell('EG1')->getValue();
+        $this->countryCodes['EH'] = $this->workSheet->getCell('EH1')->getValue();
+        $this->countryCodes['EI'] = $this->workSheet->getCell('EI1')->getValue();
+        $this->countryCodes['EJ'] = $this->workSheet->getCell('EJ1')->getValue();
+        $this->countryCodes['EK'] = $this->workSheet->getCell('EK1')->getValue();
+        $this->countryCodes['EL'] = $this->workSheet->getCell('EL1')->getValue();
+        $this->countryCodes['EM'] = $this->workSheet->getCell('EM1')->getValue();
+        $this->countryCodes['EN'] = $this->workSheet->getCell('EN1')->getValue();
+        $this->countryCodes['EO'] = $this->workSheet->getCell('EO1')->getValue();
+        $this->countryCodes['EP'] = $this->workSheet->getCell('EP1')->getValue();
+        $this->countryCodes['EQ'] = $this->workSheet->getCell('EQ1')->getValue();
+        $this->countryCodes['ER'] = $this->workSheet->getCell('ER1')->getValue();
+        $this->countryCodes['ES'] = $this->workSheet->getCell('ES1')->getValue();
+        $this->countryCodes['ET'] = $this->workSheet->getCell('ET1')->getValue();
+        $this->countryCodes['EU'] = $this->workSheet->getCell('EU1')->getValue();
+        $this->countryCodes['EV'] = $this->workSheet->getCell('EV1')->getValue();
+        $this->countryCodes['EW'] = $this->workSheet->getCell('EW1')->getValue();
+        $this->countryCodes['EX'] = $this->workSheet->getCell('EX1')->getValue();
+        $this->countryCodes['EY'] = $this->workSheet->getCell('EY1')->getValue();
+        $this->countryCodes['EZ'] = $this->workSheet->getCell('EZ1')->getValue();
+        $this->countryCodes['FA'] = $this->workSheet->getCell('FA1')->getValue();
+        $this->countryCodes['FB'] = $this->workSheet->getCell('FB1')->getValue();
+        $this->countryCodes['FC'] = $this->workSheet->getCell('FC1')->getValue();
+        $this->countryCodes['FD'] = $this->workSheet->getCell('FD1')->getValue();
+        $this->countryCodes['FE'] = $this->workSheet->getCell('FE1')->getValue();
+        $this->countryCodes['FF'] = $this->workSheet->getCell('FF1')->getValue();
+        $this->countryCodes['FG'] = $this->workSheet->getCell('FG1')->getValue();
+        $this->countryCodes['FH'] = $this->workSheet->getCell('FH1')->getValue();
+        $this->countryCodes['FI'] = $this->workSheet->getCell('FI1')->getValue();
+        $this->countryCodes['FJ'] = $this->workSheet->getCell('FJ1')->getValue();
+        $this->countryCodes['FK'] = $this->workSheet->getCell('FK1')->getValue();
+        $this->countryCodes['FL'] = $this->workSheet->getCell('FL1')->getValue();
+        $this->countryCodes['FM'] = $this->workSheet->getCell('FM1')->getValue();
+        $this->countryCodes['FN'] = $this->workSheet->getCell('FN1')->getValue();
+        $this->countryCodes['FO'] = $this->workSheet->getCell('FO1')->getValue();
+        $this->countryCodes['FP'] = $this->workSheet->getCell('FP1')->getValue();
+        $this->countryCodes['FQ'] = $this->workSheet->getCell('FQ1')->getValue();
+        $this->countryCodes['FR'] = $this->workSheet->getCell('FR1')->getValue();
+        $this->countryCodes['FS'] = $this->workSheet->getCell('FS1')->getValue();
+        $this->countryCodes['FT'] = $this->workSheet->getCell('FT1')->getValue();
+        $this->countryCodes['FU'] = $this->workSheet->getCell('FU1')->getValue();
+        $this->countryCodes['FV'] = $this->workSheet->getCell('FV1')->getValue();
+        $this->countryCodes['FW'] = $this->workSheet->getCell('FW1')->getValue();
+        $this->countryCodes['FX'] = $this->workSheet->getCell('FX1')->getValue();
+        $this->countryCodes['FY'] = $this->workSheet->getCell('FY1')->getValue();
+        $this->countryCodes['FZ'] = $this->workSheet->getCell('FZ1')->getValue();
+        $this->countryCodes['GA'] = $this->workSheet->getCell('GA1')->getValue();
+        $this->countryCodes['GB'] = $this->workSheet->getCell('GB1')->getValue();
+        $this->countryCodes['GC'] = $this->workSheet->getCell('GC1')->getValue();
+        $this->countryCodes['GD'] = $this->workSheet->getCell('GD1')->getValue();
+        $this->countryCodes['GE'] = $this->workSheet->getCell('GE1')->getValue();
+        $this->countryCodes['GF'] = $this->workSheet->getCell('GF1')->getValue();
+        $this->countryCodes['GG'] = $this->workSheet->getCell('GG1')->getValue();
+        $this->countryCodes['GH'] = $this->workSheet->getCell('GH1')->getValue();
+        $this->countryCodes['GI'] = $this->workSheet->getCell('GI1')->getValue();
+        $this->countryCodes['GJ'] = $this->workSheet->getCell('GJ1')->getValue();
+        $this->countryCodes['GK'] = $this->workSheet->getCell('GK1')->getValue();
+        $this->countryCodes['GL'] = $this->workSheet->getCell('GL1')->getValue();
+        $this->countryCodes['GM'] = $this->workSheet->getCell('GM1')->getValue();
+        $this->countryCodes['GN'] = $this->workSheet->getCell('GN1')->getValue();
+        $this->countryCodes['GO'] = $this->workSheet->getCell('GO1')->getValue();
+        $this->countryCodes['GP'] = $this->workSheet->getCell('GP1')->getValue();
+        $this->countryCodes['GQ'] = $this->workSheet->getCell('GQ1')->getValue();
+        $this->countryCodes['GR'] = $this->workSheet->getCell('GR1')->getValue();
+        $this->countryCodes['GS'] = $this->workSheet->getCell('GS1')->getValue();
+        $this->countryCodes['GT'] = $this->workSheet->getCell('GT1')->getValue();
+        $this->countryCodes['GU'] = $this->workSheet->getCell('GU1')->getValue();
+        $this->countryCodes['GV'] = $this->workSheet->getCell('GV1')->getValue();
+        $this->countryCodes['GW'] = $this->workSheet->getCell('GW1')->getValue();
+        $this->countryCodes['GX'] = $this->workSheet->getCell('GX1')->getValue();
+        $this->countryCodes['GY'] = $this->workSheet->getCell('GY1')->getValue();
+        $this->countryCodes['GZ'] = $this->workSheet->getCell('GZ1')->getValue();
+        $this->countryCodes['HA'] = $this->workSheet->getCell('HA1')->getValue();
+        $this->countryCodes['HB'] = $this->workSheet->getCell('HB1')->getValue();
+        $this->countryCodes['HC'] = $this->workSheet->getCell('HC1')->getValue();
+        $this->countryCodes['HD'] = $this->workSheet->getCell('HD1')->getValue();
+        $this->countryCodes['HE'] = $this->workSheet->getCell('HE1')->getValue();
+        $this->countryCodes['HF'] = $this->workSheet->getCell('HF1')->getValue();
+        $this->countryCodes['HG'] = $this->workSheet->getCell('HG1')->getValue();
+        $this->countryCodes['HH'] = $this->workSheet->getCell('HH1')->getValue();
+        $this->countryCodes['HI'] = $this->workSheet->getCell('HI1')->getValue();
+        $this->countryCodes['HJ'] = $this->workSheet->getCell('HJ1')->getValue();
+        $this->countryCodes['HK'] = $this->workSheet->getCell('HK1')->getValue();
+        $this->countryCodes['HL'] = $this->workSheet->getCell('HL1')->getValue();
+        $this->countryCodes['HM'] = $this->workSheet->getCell('HM1')->getValue();
+        $this->countryCodes['HN'] = $this->workSheet->getCell('HN1')->getValue();
+        $this->countryCodes['HO'] = $this->workSheet->getCell('HO1')->getValue();
+        $this->countryCodes['HP'] = $this->workSheet->getCell('HP1')->getValue();
+        $this->countryCodes['HQ'] = $this->workSheet->getCell('HQ1')->getValue();
+        $this->countryCodes['HR'] = $this->workSheet->getCell('HR1')->getValue();
+        $this->countryCodes['HS'] = $this->workSheet->getCell('HS1')->getValue();
+        $this->countryCodes['HT'] = $this->workSheet->getCell('HT1')->getValue();
+        $this->countryCodes['HU'] = $this->workSheet->getCell('HU1')->getValue();
+        $this->countryCodes['HV'] = $this->workSheet->getCell('HV1')->getValue();
+        $this->countryCodes['HW'] = $this->workSheet->getCell('HW1')->getValue();
+        $this->countryCodes['HX'] = $this->workSheet->getCell('HX1')->getValue();
+        $this->countryCodes['HY'] = $this->workSheet->getCell('HY1')->getValue();
+        $this->countryCodes['HZ'] = $this->workSheet->getCell('HZ1')->getValue();
+        $this->countryCodes['IA'] = $this->workSheet->getCell('IA1')->getValue();
+        $this->countryCodes['IB'] = $this->workSheet->getCell('IB1')->getValue();
+        $this->countryCodes['IC'] = $this->workSheet->getCell('IC1')->getValue();
+        $this->countryCodes['ID'] = $this->workSheet->getCell('ID1')->getValue();
+        $this->countryCodes['IE'] = $this->workSheet->getCell('IE1')->getValue();
+        $this->countryCodes['IF'] = $this->workSheet->getCell('IF1')->getValue();
+        $this->countryCodes['IG'] = $this->workSheet->getCell('IG1')->getValue();
+        $this->countryCodes['IH'] = $this->workSheet->getCell('IH1')->getValue();
+        $this->countryCodes['II'] = $this->workSheet->getCell('II1')->getValue();
+        $this->countryCodes['IJ'] = $this->workSheet->getCell('IJ1')->getValue();
+        $this->countryCodes['IK'] = $this->workSheet->getCell('IK1')->getValue();
+        $this->countryCodes['IL'] = $this->workSheet->getCell('IL1')->getValue();
+        $this->countryCodes['IM'] = $this->workSheet->getCell('IM1')->getValue();
+        $this->countryCodes['IN'] = $this->workSheet->getCell('IN1')->getValue();
+        $this->countryCodes['IO'] = $this->workSheet->getCell('IO1')->getValue();
+        $this->countryCodes['IP'] = $this->workSheet->getCell('IP1')->getValue();
+        $this->countryCodes['IQ'] = $this->workSheet->getCell('IQ1')->getValue();
+        $this->countryCodes['IR'] = $this->workSheet->getCell('IR1')->getValue();
+        $this->countryCodes['IS'] = $this->workSheet->getCell('IS1')->getValue();
+        $this->countryCodes['IT'] = $this->workSheet->getCell('IT1')->getValue();
+        $this->countryCodes['IU'] = $this->workSheet->getCell('IU1')->getValue();
+        $this->countryCodes['IV'] = $this->workSheet->getCell('IV1')->getValue();
+        $this->countryCodes['IW'] = $this->workSheet->getCell('IW1')->getValue();
+        $this->countryCodes['IX'] = $this->workSheet->getCell('IX1')->getValue();
+        $this->countryCodes['IY'] = $this->workSheet->getCell('IY1')->getValue();
+        $this->countryCodes['IZ'] = $this->workSheet->getCell('IZ1')->getValue();
+
+
+
+
+
+
+
+
+
+
         
     }
 
@@ -141,7 +307,7 @@ class ImportPostNLRates extends AbstractImportService
     {
         $limit = 49;
         foreach ($this->countryCodes as $cell => $countryCodes) {
-           $countryId = Country::where('name', $countryCodes)->value('id');
+           $countryId = Country::where('name', ucwords(strtolower($countryCodes)))->value('id');
            $rates = [];
             if ($countryId) {
                 foreach (range(2, $limit) as $row)
