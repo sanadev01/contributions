bcee213ea28f5474b8657ee591d7c7588666128b
sanadev01 <sanasheraz.dev@gmail.com>
2022-10-08 01:00:11 +0500

    Domestic API Lables

--- app/Http/Controllers/Api/PublicApi/DomesticLabelController.php
+++ app/Http/Controllers/Api/PublicApi/DomesticLabelController.php
@@ -21,34 +21,54 @@ class DomesticLabelController extends Controller
     {
         //dd($request->toArray());
         $validated = $request->validate([
-            'orders' => 'required',
-            'sender' => 'required',
-            'receiver' => 'required',
+            'tracking_code' => 'required',
+            //'sender' => 'required',
+            'recipient' => 'required',
+            'service' => 'required',
+            'total_price' => 'required',
+            //'consolidate_dimensions' => 'required',
         ]);
 
-        //$orderIds = json_decode(json_encode($validated['orders']), true);
-        $orderIds = json_decode($validated['orders'], true);
-        //dd($orderIds);
+        $trackIds = json_decode(json_encode($validated['tracking_code']), true);
+        //dd($trackIds);
+        $orderIds = [];
+        if (count($trackIds) >= 1) {
+            foreach ($trackIds as $key => $item) {
+                $trackCode = Order::where('corrios_tracking_code', $item)->value('id');
+               array_push($orderIds, $trackCode);
+            }
+        }
+        //dd(json_encode($orderIds));
         if (!$orderIds) {
             return apiResponse(false,"Please provide order IDs.");
         }
 
-        //$orders = $consolidatedDomesticLabelRepository->getInternationalOrders($orderIds);
+        $orders = $consolidatedDomesticLabelRepository->getInternationalOrders($orderIds);
         //dd($orders);
-        // if ($orders->isEmpty()) {
-        //     return apiResponse(false,"Selected orders already have domestic label");
-        // }
+        if ($orders->isEmpty()) {
+            return apiResponse(false,"Selected orders already have domestic label");
+        }
 
         $error = $consolidatedDomesticLabelRepository->getErrors();
 
         if(!$error){
-            $order = Order::find($orderIds);
-            //$totalWeight = $consolidatedDomesticLabelRepository->getTotalWeight($orders);
-            $rates = $domesticLabelRepository->getShippingServices($order);
+
+            $totalWeight = $consolidatedDomesticLabelRepository->getTotalWeight($orders);
+            $domesticLabelRepository->handle();
+
+            $order = Order::find($orderIds[0]);
+            if(request()->service == "UPS") { request()->merge(['service' => 3]); }
+            elseif(request()->service == "FedEx") { request()->merge(['service' => 4]); }
+            elseif(request()->service == "USPS") { request()->merge(['service' => 3440]); }
+            else { request()->merge(['service' => 3441]); }
+            //dd($order);
+            $shippingServices = $domesticLabelRepository->getShippingServices($order);
+            $label = $domesticLabelRepository->getDomesticLabel($order);
+            //$rates = $domesticLabelRepository->getRatesForDomesticServices($shippingServices);
 
             return apiResponse(true,"Total Weight of Orders",[
-                //'weight' => $totalWeight,
-                'rates' => $rates
+                'weight' => $totalWeight,
+                'label' => $label
             ]);
 
         }
