40e40006bd3f9ac15e33f1e7440c2d7a30212ceb
sanadev01 <sanasheraz.dev@gmail.com>
2022-11-01 18:27:49 +0500

    Tax Table Migrations Set

--- database/migrations/2022_11_01_132345_alter_add_payment_to_taxes_table.php
+++ database/migrations/2022_11_01_132345_alter_add_payment_to_taxes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTax1BrazilToTaxesTable extends Migration
+class AlterAddPaymentToTaxesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class AddTax1BrazilToTaxesTable extends Migration
     public function up()
     {
         Schema::table('taxes', function (Blueprint $table) {
-            $table->double('tax_1_br')->after('tax_1')->nullable();
-            $table->double('tax_2_br')->after('tax_2')->nullable();
+            $table->double('tax_payment')->change();
         });
     }
 
@@ -27,8 +26,7 @@ class AddTax1BrazilToTaxesTable extends Migration
     public function down()
     {
         Schema::table('taxes', function (Blueprint $table) {
-            $table->dropColumn('tax_1_br');
-            $table->dropColumn('tax_2_br');
+            //
         });
     }
 }
