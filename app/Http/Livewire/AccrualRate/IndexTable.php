75ed2ee18e06a51452378ecd4db3555059f5fbd9
sanadev01 <sanasheraz.dev@gmail.com>
2022-11-03 00:15:54 +0500

    GePS manifest by Service Fix

--- app/Http/Livewire/AccrualRate/IndexTable.php
+++ app/Http/Livewire/AccrualRate/IndexTable.php
@@ -46,7 +46,10 @@ class IndexTable extends Component
             'name' => 'Anjun Express',
             'value' => Package::SERVICE_CLASS_AJ_EXPRESS,
         ];
-        
+        $this->services[7] = [
+            'name' => 'GePS',
+            'value' => Package::SERVICE_CLASS_GePS,
+        ];
         return $this->services;
     }
 }
