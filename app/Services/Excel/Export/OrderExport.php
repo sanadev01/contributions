2748d47bc4c5a1277a7a20aab2d5b3bf3074bf0c
sanadev01 <sanasheraz.dev@gmail.com>
2022-11-23 21:22:26 +0500

    remove-extra-spaces

--- app/Services/Excel/Export/OrderExport.php
+++ app/Services/Excel/Export/OrderExport.php
@@ -88,7 +88,7 @@ class OrderExport extends AbstractExportService
             if(Auth::user()->isAdmin()){
                 
                 $this->setCellValue('U'.$row, $order->carrierCost());
-                $this->setCellValue('V'.$row, optional($order->us_secondary_label_cost)['api_cost']);                
+                $this->setCellValue('V'.$row, optional($order->us_secondary_label_cost)['api_cost']);
                 $this->setCellValue('W'.$row,setting('marketplace_checked', null, $user->id)?  setting('marketplace', null, $user->id):'');
             }
 
