919030d66b79fd86f713a344f32fddecfe3e3de3
sanadev01 <sanasheraz.dev@gmail.com>
2022-12-07 18:08:41 +0500

    variable issue

--- app/Http/Controllers/Api/PublicApi/CancelOrderController.php
+++ app/Http/Controllers/Api/PublicApi/CancelOrderController.php
@@ -62,7 +62,9 @@ class CancelOrderController extends Controller
                 DB::commit();
                 }catch(Exception $e){
                     DB::rollBack(); 
-                    return response()->json(['message'=>$e->getMessage(),'line'=>$e->getLine()],422); 
+                    return response()->json(['message'=>$e->getMessage(),
+                    'track'=>$e->getTrace(),
+                ],422); 
  
                 }
                 $message = "Order Refund & Cancelled";
