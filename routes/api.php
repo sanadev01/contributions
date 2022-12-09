3dd0c46ce27ec69876b50e7315a1dd3765f19cab
sanadev01 <sanasheraz.dev@gmail.com>
2022-12-09 19:28:02 +0500

    GePS Cancel Parcel Label from API

--- routes/api.php
+++ routes/api.php
@@ -86,7 +86,6 @@ Route::prefix('v1')->group(function(){
         Route::get('country/{country}/states', StateController::class);
         Route::get('shipping-services/{country_code?}', ServicesController::class);
         Route::get('shcodes/{search?}', ShCodeController::class);
-
     });
 
 
