016838321fe4c5aa74acec131cb0949a640df6cb
sanadev01 <sanasheraz.dev@gmail.com>
2022-12-22 18:08:46 +0500

    livewire

--- app/Http/Livewire/SwedenPostContainer/Package.php
+++ app/Http/Livewire/SwedenPostContainer/Package.php
@@ -8,7 +8,7 @@ use App\Models\OrderTracking;
 use App\Models\Warehouse\Container;
 use App\Http\Controllers\Warehouse\SwedenPostContainerPackageController;
 
-class Packages extends Component
+class Package extends Component
 {
     public $container;
     public $orders = [];
@@ -37,7 +37,7 @@ class Packages extends Component
         $this->totalPackages();
         $this->totalWeight();
 
-        return view('livewire.swedenpost-container.packages');
+        return view('livewire.sweden-post-container.package');
     }
 
     public function getPackages($id)
