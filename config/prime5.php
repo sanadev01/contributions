f0c6041e9e1e555f152bd9b59ebfb23512936c30
sanadev01 <sanasheraz.dev@gmail.com>
2022-12-26 19:48:45 +0500

    update manifest and cn23 label

--- config/prime5.php
+++ config/prime5.php
@@ -11,7 +11,5 @@ return [
         'token' => 'testa0wXdbpML6JGQ7NRP3O',
         'host' => 'qa.etowertech.com',
         'baseUrl' => 'http://qa.etowertech.com/',
-        // 'host' => 'cn.etowertech.com',
-        // 'baseUrl' => 'https://cn.etowertech.com/',
     ]
 ];
