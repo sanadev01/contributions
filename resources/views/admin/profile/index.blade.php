89d29db169f7b64d23f13481420ecefea0e4cc86
sanadev01 <sanasheraz.dev@gmail.com>
2022-12-30 18:44:25 +0500

    update email notify auto charge

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -251,6 +251,21 @@
                                       </div>
                                   </div>
                              </div>
+                             <div class="controls row mb-1 align-items-center">
+                                <div class="offset-3">
+                                    <div class="input-group ml-3">
+                                        <div class="vs-checkbox-con vs-checkbox-primary" title="Auto charge">
+                                            <input type="checkbox" name="auto_charge" id="auto_charge" @if(setting('auto_charge', null, auth()->user()->id)) checked @endif>
+                                            <span class="vs-checkbox vs-checkbox-lg">
+                                                <span class="vs-checkbox--check">
+                                                    <i class="vs-icon feather icon-check"></i>
+                                                </span>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <label class="col-md-6 font-medium-1 font-weight-bold" for="auto_charge">@lang('profile.payment permission')<span class="text-danger"></span></label>
+                            </div>
                             <div class="row mt-1">
                                 <div class="col-12 d-flex flex-sm-row flex-column justify-content-end mt-1">
                                     <button type="submit"
