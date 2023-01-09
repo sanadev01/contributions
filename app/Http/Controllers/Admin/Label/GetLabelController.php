d33aedd45003bea26dbeb0de79ef256da2910a13
sanadev01 <sanasheraz.dev@gmail.com>
2023-01-09 21:25:03 +0500

    update label

--- app/Http/Controllers/Admin/Label/GetLabelController.php
+++ app/Http/Controllers/Admin/Label/GetLabelController.php
@@ -12,24 +12,27 @@ class GetLabelController extends Controller
     {
   
         try{
-        $order = Order::find(decrypt($id));
-        
-        if ( $order->sinerlog_url_label != '' ) {
-            return redirect($order->sinerlog_url_label);
-        } else {
-            if ( !file_exists(storage_path("app/labels/{$order->corrios_tracking_code}.pdf")) ){
-                return apiResponse(false,"Lable Expired or not generated yet please update lable");
+            $order = Order::find(decrypt($id));
+            
+            if($order->corrios_tracking_code){  
+                if ( $order->sinerlog_url_label != '' ) {
+                    return redirect($order->sinerlog_url_label);
+                } else {
+                    if ( !file_exists(storage_path("app/labels/{$order->corrios_tracking_code}.pdf")) ){
+                        return apiResponse(false,"Lable Expired or not generated yet please update lable");
+                    }
+                }
+                return response()->download(storage_path("app/labels/{$order->corrios_tracking_code}.pdf"),"{$order->corrios_tracking_code} - {$order->warehouse_number}.pdf",[],'inline');
             }
-        }
-        return response()->download(storage_path("app/labels/{$order->corrios_tracking_code}.pdf"),"{$order->corrios_tracking_code} - {$order->warehouse_number}.pdf",[],'inline');
-   
+            return apiResponse(false,'Something Went wrong please Contact Homedelivery support');
+
         }catch(Exception $e){
-                return response()->json(
-                    [
-                        'success' => false,
-                        'message' => $e->getMessage(),
-                    ],
-                422);
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => $e->getMessage(),
+                ],
+            422);
         }
  }
 }
