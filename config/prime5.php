54c34885c273e7e8f3cb69bd39c6690bf5d27118
sanadev01 <sanasheraz.dev@gmail.com>
2023-01-23 21:33:50 +0500

    Direct Link Multiple Tracking Updated

--- config/prime5.php
+++ config/prime5.php
@@ -9,9 +9,7 @@ return [
             'userName' => 'HFFBagscanAPI',
             'password' => 'f65004ab2af0dc0f0cc063ee2b45e804d003d9d9a0cad02ce8933f4e0217415e',
             'baseURL'  => 'https://api-server-test.directlink.pncloud.se/'
-        ],
-        'trackUrl' => 'https://api.directlink.com/responseStatus?itemNumbers=',
-        'trackApiKey' => '8fcbd7946d1d0886f5e6bce32d54b199f14113fe70eed818316c69b22024ada7',
+        ]
     ],
     'production' => [
         'secret' => 'Q-P1jmxEOJLvQ-sEGIvunQ',
@@ -22,8 +20,8 @@ return [
             'userName' => 'HFFBagscanAPI',
             'password' => '13182a58e385abf1b02395288284902403ae89ccbf6fe0197388b99c83d083e4',
             'baseURL'  => 'https://api.directlink.com/'
+            ]
         ],
-        'trackUrl' => 'https://api.directlink.com/responseStatus?itemNumbers=',
-        'trackApiKey' => '8fcbd7946d1d0886f5e6bce32d54b199f14113fe70eed818316c69b22024ada7',
-    ]
-];
+    'trackUrl' => 'https://api.directlink.com/responseStatus?itemNumbers=',
+    'trackApiKey' => '8fcbd7946d1d0886f5e6bce32d54b199f14113fe70eed818316c69b22024ada7',
+    ];
