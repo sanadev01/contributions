a7b5f0cb461607997be43bed73f7164c81255b4c
sanadev01 <sanasheraz.dev@gmail.com>
2023-01-26 16:55:09 +0500

    KPI Report Updated

--- resources/views/admin/reports/kpi-report.blade.php
+++ resources/views/admin/reports/kpi-report.blade.php
@@ -22,16 +22,16 @@
                                     </select>
                                 </div>
                                 <div class="col-11 text-right">
-                                    <form action="{{ route('admin.reports.kpi-report.create') }}" method="GET" target="_blank">
+                                    <form action="{{ route('admin.reports.kpi-report.index') }}" method="GET">
                                         @csrf
                                         <div class="row">
-                                            <div class="offset-2 col-md-3">
+                                            <div class="offset-4 col-md-3">
                                                 <div class="row">
                                                     <div class="col-md-4 mt-2">
                                                         <label>Start Date</label>
                                                     </div>
                                                     <div class="col-md-8 pl-0 pr-0">
-                                                        <input type="date" name="start_date" class="form-control">
+                                                        <input type="date" name="start_date" class="form-control" id="startDate" placeholder="mm/dd/yyyy">
                                                     </div>
                                                 </div>
                                             </div>
@@ -41,7 +41,7 @@
                                                         <label>End Date</label>
                                                     </div>
                                                     <div class="col-md-8 pl-0">
-                                                        <input type="date" name="end_date" class="form-control">
+                                                        <input type="date" name="end_date" class="form-control" id="endDate" placeholder="mm/dd/yyyy">
                                                     </div>
                                                 </div>
                                             </div>
@@ -50,13 +50,17 @@
                                                     @lang('user.Search')
                                                 </button>
                                             </div>
-                                            <div class="offset-2 col-md-1">
-                                                <button class="btn btn-success" title="@lang('orders.import-excel.Download')">
-                                                    <i class="fa fa-arrow-down"></i>
-                                                </button>
-                                            </div>
+                                            </form>
+                                            <form action="{{ route('admin.reports.kpi-report.create') }}" method="GET" target="_blank" id="reportForm">
+                                                <input type="hidden" name="_token" value="{{ csrf_token() }}" id="token"/>
+                                                <div class="col-md-1 justify-content-end">
+                                                    <button class="btn btn-success" title="@lang('orders.import-excel.Download')">
+                                                        <i class="fa fa-arrow-down"></i>
+                                                    </button>
+                                                </div>
+                                            </form>
                                         </div>
-                                    </form>
+                                    
                                 </div>
                             </div>
                             <table class="table mb-0 table-responsive-md">
@@ -77,61 +81,17 @@
                                 <tbody>
                                     @foreach($trackings['return']['objeto'] as $data)
                                         @if(isset($data['evento']))
-                                            @php
-                                                $delivered = "No";
-                                                $returned = "No";
-                                                $taxed = "No";
-                                                $diffDates = "0";        
-                                                for($t=count($data['evento'])-1;$t>=0;$t--) {   ##start the looping beginning from the first event
-                                                    
-                                                    switch($data['evento'][$t]['descricao']) {
-
-                                                        case "Objeto entregue ao destinatário":     ## if the package was delivered mark as delivered
-                                                            $delivered = "Yes";
-
-                                                            if($taxed == "")       ## if the package was delivered and the status of taxed is blank our package wasn't taxed
-                                                                $taxed = "No";
-
-                                                        break;
-
-                                                        case "Devolução autorizada pela Receita Federal":   ## if the package was declined by inspection
-                                                        case "A entrada do objeto no Brasil não foi autorizada pelos órgãos fiscalizadores":
-                                                            $returned = "Yes";
-                                                        break;
-
-                                                        case "Aguardando pagamento":        ## if we have the event of "waiting for payment" or "payment approved" is because the package was taxed
-                                                        case "Pagamento confirmado":
-                                                            $taxed = "Yes";
-                                                        break;
-
-                                                        case "Fiscalização aduaneira finalizada":  ## if we have the status of "inspection finished" is because the package wasn't taxed YET
-                                                            if($taxed == "")
-                                                                $taxed = "No";
-                                                        break;
-
-                                                    }
-                                                    
-                                                }
-                                                $eventsQtd = count($data['evento'])-1; ## number of events for the package
-                                                $dateFirstEvent = \DateTime::createFromFormat('d/m/Y', $data['evento'][$eventsQtd]['data']); ##date of first event
-                                                $dateLastEvent = \DateTime::createFromFormat('d/m/Y', $data['evento'][0]['data']); ##date of last event
-                                                $interval = $dateFirstEvent->diff($dateLastEvent);  ##days between first and last event
-                                                $diffDates = $interval->format('%R%a days');
-                                            @endphp
                                         <tr>
                                             @if(optional($data) && isset($data['numero']))
                                                 <td>{{ $data['numero'] }}</td>
                                                 <td><span>{{ $data['categoria'] }}</span></td>
                                                 <td>{{ $data['evento'][count($data['evento'])-1]['data'] }}</td>
                                                 <td>{{ $data['evento'][0]['data'] }}</td>
-                                                <td>
-                                                    
-                                                    {{ $diffDates }}
-                                                </td>
+                                                <td>{{ sortTrackingEvents($data)['diffDates'] }} </td>
                                                 <td>{{ $data['evento'][0]['descricao'] }}</td>
-                                                <td>{{ $taxed }}</td>
-                                                <td>{{ $delivered }}</td>
-                                                <td>{{ $returned }}</td>
+                                                <td>{{ sortTrackingEvents($data)['taxed'] }}</td>
+                                                <td>{{ sortTrackingEvents($data)['delivered'] }}</td>
+                                                <td>{{ sortTrackingEvents($data)['returned'] }}</td>
                                             @else
                                             <td colspan='9'>No Trackings Found</td>
                                             @endif
@@ -148,6 +108,16 @@
             </div>
         </div>
     </section>
+    <script>
+        document.querySelector('#reportForm').addEventListener('submit', (e) => {
+            e.preventDefault();
+            var formData = new FormData(e.target);
+            var startDate = $("#startDate").val();
+            var endDate = $("#endDate").val();
+            var token = $("#token").val();
+            window.location.href = "kpi-report/create?start_date="+startDate+"&end_date="+endDate
+        });
+    </script>
 @endsection
 @section('modal')
 <x-modal />
