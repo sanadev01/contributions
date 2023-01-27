f053d990c126e9135528acb6cac7de375a52b642
sanadev01 <sanasheraz.dev@gmail.com>
2023-01-27 21:56:24 +0500

    KPI Report Fixes

--- app/Services/Excel/Export/KPIReport.php
+++ app/Services/Excel/Export/KPIReport.php
@@ -36,7 +36,7 @@ class KPIReport extends AbstractExportService
             if(optional($data) && isset($data->evento)) {
                 $this->setCellValue('A'.$row, $data->numero);
                 $this->setCellValue('B'.$row, $data->categoria);
-                $this->setCellValue('C'.$row, optional(optional($data->evento)[optional(count($data->evento))-1])->data);
+                $this->setCellValue('C'.$row, optional(optional(optional($data)->evento)[count($data->evento)-1])->data);
                 $this->setCellValue('D'.$row, optional(optional($data->evento)[0])->data);
                 $this->setCellValue('E'.$row, sortTrackingEvents($data, $report)['diffDates']);
                 $this->setCellValue('F'.$row, optional(optional($data->evento)[0])->descricao);
