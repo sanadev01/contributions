65281cd6ea41f3175c66579cd32f7a364ce7fbd2
sanadev01 <sanasheraz.dev@gmail.com>
2023-01-30 13:44:05 +0000

    Revert "update time limit (pull request #2388)"

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -386,8 +386,6 @@ class OrderRepository
     
     public function getOdersForExport($request)
     {
-        set_time_limit(800);
-        
         $orders = Order::where('status','>=',Order::STATUS_ORDER)
         ->has('user');
 
