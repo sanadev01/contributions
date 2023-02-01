ff3362f021970f98ebf1087c682c9972e8e350c1
sanadev01 <sanasheraz.dev@gmail.com>
2023-02-02 00:14:00 +0500

    zipcode validation

--- app/Http/Controllers/Api/PublicApi/ParcelController.php
+++ app/Http/Controllers/Api/PublicApi/ParcelController.php
@@ -68,19 +68,19 @@ class ParcelController extends Controller
 
             }
             $errors = [];
-            $unwanted_array = array('Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+            $replaceCharacter = array('Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
                             'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
                             'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
                             'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
                             'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
 
-            if(strtr($response['uf'],$unwanted_array) != strtr($state->code,$unwanted_array)){ 
+            if(strtr($response['uf'],$replaceCharacter) != strtr($state->code,$replaceCharacter)){ 
                 array_push($errors ,[ 'recipient.state_id' => 'According to your Zipcode the valid state is '.$response['uf']]);
             }
-            if(strtr($response['city'],$unwanted_array) != strtr($request->recipient['address'],$unwanted_array)){ 
+            if(strtr($response['city'],$replaceCharacter) != strtr($request->recipient['address'],$replaceCharacter)){ 
                 array_push($errors ,[ 'recipient.city' => 'According to your Zipcode the valid city is '.$response['city']]);
             }
-            if(strtr($response['street'],$unwanted_array)!= strtr($request->recipient['address'],$unwanted_array)){ 
+            if(strtr($response['street'],$replaceCharacter)!= strtr($request->recipient['address'],$replaceCharacter)){ 
                 array_push($errors ,[ 'recipient.address' => 'According to your Zipcode the valid address is '.$response['street']]);
             }
             if(count($errors)){
