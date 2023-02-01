2bd19ed574c8cdd46ba9aa32fd70034a65979c6f
sanadev01 <sanasheraz.dev@gmail.com>
2023-02-01 21:13:03 +0500

    Order Report Updated

--- app/Http/Livewire/Order/ExportOrder.php
+++ app/Http/Livewire/Order/ExportOrder.php
@@ -4,14 +4,39 @@ namespace App\Http\Livewire\Order;
 
 use App\Models\Reports;
 use Livewire\Component;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\Storage;
 
 class ExportOrder extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
+    public $pageSize = 50;
+
     public function render()
     {
-        $id = Reports::orderBy('id', 'desc')->value('id');
-        $report = Reports::find($id);
-        
-        return view('livewire.order.export-order', compact('report'));
+        $reports = Reports::all();
+        return view('livewire.order.export-order', compact('reports'));
+    }
+
+    public function download(Reports $report)
+    {
+        if($report->path) {
+            return response()->download($report->path);
+        }
+    }
+
+    public function delete(Reports $report)
+    {
+        if($report->path) {
+            Storage::delete(basename($report->path));
+        }
+        $report->delete();
+    }
+
+    public function updating()
+    {
+        $this->resetPage();
     }
 }
--- resources/views/livewire/order/export-order.blade.php
+++ resources/views/livewire/order/export-order.blade.php
@@ -1,7 +1,20 @@
-<div @if(!$report->is_complete) wire:poll.30000ms @endif>
+<div @if(!$reports->first()->is_complete) wire:poll.30000ms @endif>
     <div class="card-header">
-        <h4 class="mb-0">{{ $report->name }} Report</h4>
+        <h4 class="mb-0">Orders Export Report</h4>
     </div><br>
+    <div class="row mb-2 no-print">
+        <div class="col-1">
+            <select class="form-control" wire:model="pageSize">
+                <option value="1">1</option>
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+                <option value="300">300</option>
+            </select>
+        </div>
+    </div>
     <table class="table mb-0 table-responsive-md">
         <thead>
             <tr>
@@ -12,21 +25,27 @@
             </tr>
         </thead>
         <tbody>
-            @if($report)
-                <tr>
-                    <td>{{ $report->name }}</td>
-                    <td>{{ date('d-M-Y', strtotime($report->start_date)) }}</td>
-                    <td>{{ date('d-M-Y', strtotime($report->end_date)) }}</td>
-                    @if($report->is_complete == '0')
-                        <td><button class="btn btn-warning btn-sm disabled">Processing..</button></td>
-                    @else
-                        <td><a href="{{ $report->path }}" download><button class="btn btn-success btn-sm">Download</button></a></td>
-                    @endif
-                </tr>
+            @if($reports)
+                @foreach($reports as $report)
+                    <tr>
+                        <td>{{ $report->name }}</td>
+                        <td>{{ date('d-M-Y', strtotime($report->start_date)) }}</td>
+                        <td>{{ date('d-M-Y', strtotime($report->end_date)) }}</td>
+                        <td>
+                            @if($report->is_complete == '0')
+                                <button class="btn btn-warning btn-sm disabled">Processing..</button>
+                            @else
+                                <button class="btn btn-success btn-sm pr-3" wire:click.prevent="download({{ $report->id }})">Download</button>
+                            @endif
+                            <button type="" class="btn btn-danger btn-sm" wire:click.prevent="delete({{ $report->id }})">Delete</button>
+                        </td>
+                    </tr>
+                @endforeach
             @else
                 <tr><td colspan='4'>No Reports Found</td></tr>
             @endif
         </tbody>
     </table>
+    
     @include('layouts.livewire.loading')
 </div>
