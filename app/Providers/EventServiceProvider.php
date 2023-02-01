3927024b3be6959184b7569cfcc9d39f22283c1c
sanadev01 <sanasheraz.dev@gmail.com>
2023-02-02 00:44:52 +0500

    update report order

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,9 +4,7 @@ namespace App\Providers;
 
 use App\Models\Order;
 use App\Events\OrderPaid;
-use App\Events\OrderReport;
 use App\Models\AffiliateSale;
-use App\Listeners\ExportOrder;
 use App\Observers\OrderObserver;
 use Illuminate\Support\Facades\Event;
 use App\Listeners\CalculateCommission;
