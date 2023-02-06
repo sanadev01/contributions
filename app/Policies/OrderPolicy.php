1385c07e0275b68e9223e1a8d57ee7d8bf03ecb5
sanadev01 <sanasheraz.dev@gmail.com>
2023-02-07 01:30:13 +0500

    update permission

--- app/Policies/OrderPolicy.php
+++ app/Policies/OrderPolicy.php
@@ -161,7 +161,7 @@ class OrderPolicy
 
     public function updateOrder(User $user,Order $order)
     {
-        return $user->hasPermission('edit_order') && $order->user_id == $user->id && !$order->isPaid() || in_array($user->id, [996,13]);
+        return $user->hasPermission('edit_order') && $order->user_id == $user->id && !$order->isPaid();
     }
     
     public function copyOrder(User $user,Order $order)
