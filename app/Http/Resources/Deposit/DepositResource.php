38629deb7c44864eb01b2816ba18ac64f7673586
sanadev01 <sanasheraz.dev@gmail.com>
2023-02-16 22:45:57 +0500

    deposit resource update

--- app/Http/Resources/Deposit/DepositResource.php
+++ app/Http/Resources/Deposit/DepositResource.php
@@ -10,9 +10,10 @@ class DepositResource extends JsonResource{
     { 
        return [
             'type' => $this->type,
-            'pobox_number' => $this->user->pobox_number,
+            'id' => $this->uuid,
             'amount' => $this->amount,
             'description' => $this->description,
+            'detail' => $this->last_four_digits,
             'created_at' => Carbon::parse($this->created_at)->format('Y-M-d'),
             'orders' => $this->orders?OrderResource::collection($this->orders()->withTrashed()->get()):[],
            
