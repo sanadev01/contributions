4fd98e18feee421c555ed00e3d22bf450e1f50f2
sanadev01 <sanasheraz.dev@gmail.com>
2023-02-16 23:28:18 +0500

    api deposits

--- app/Http/Resources/Deposit/DepositResource.php
+++ app/Http/Resources/Deposit/DepositResource.php
@@ -3,19 +3,21 @@ namespace App\Http\Resources\Deposit;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\Deposit\OrderResource;
+use App\Models\Order;
 use Carbon\Carbon;
 
 class DepositResource extends JsonResource{
     public function toArray($request)
     { 
+        $order = count($this->orders)?$this->orders()->withTrashed()->first():Order::where('id',$this->order_id)->first();
        return [ 
         'type' => $this->type,
         'id' => $this->uuid,
+        'warehouse_number' => optional($order)->warehouse_number,
         'amount' => $this->amount,
         'description' => $this->description,
         'detail' => $this->last_four_digits,
-        'created_at' => Carbon::parse($this->created_at)->format('Y-M-d'),
-        'orders' => $this->orders?OrderResource::collection($this->orders()->withTrashed()->get()):[],
+        'created_at' => Carbon::parse($this->created_at)->format('Y-M-d'), 
        ];
     }
 
