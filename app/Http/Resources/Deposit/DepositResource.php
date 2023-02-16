74d8f286c3b26d509ab0d81fcc7351801379b1d5
sanadev01 <sanasheraz.dev@gmail.com>
2023-02-16 23:37:14 +0500

    api deposit

--- app/Http/Resources/Deposit/DepositResource.php
+++ app/Http/Resources/Deposit/DepositResource.php
@@ -1,1 +1,24 @@
-git
+<?php 
+namespace App\Http\Resources\Deposit;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Deposit\OrderResource;
+use App\Models\Order;
+use Carbon\Carbon;
+
+class DepositResource extends JsonResource{
+    public function toArray($request)
+    { 
+        $order = count($this->orders)?$this->orders()->withTrashed()->first():Order::where('id',$this->order_id)->first();
+       return [ 
+        'type' => $this->type,
+        'id' => $this->uuid,
+        'warehouse_number' => optional($order)->warehouse_number,
+        'amount' => $this->amount,
+        'description' => $this->description,
+        'detail' => $this->last_four_digits,
+        'created_at' => Carbon::parse($this->created_at)->format('Y-M-d'), 
+       ];
+    }
+
+}
