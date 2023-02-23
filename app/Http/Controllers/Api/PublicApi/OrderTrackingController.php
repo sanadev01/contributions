d525a1eabaef6fa06522ec90ac3435b1139cb922
sanadev01 <sanasheraz.dev@gmail.com>
2023-02-23 23:53:36 +0500

    api-response

--- app/Http/Controllers/Api/PublicApi/OrderTrackingController.php
+++ app/Http/Controllers/Api/PublicApi/OrderTrackingController.php
@@ -23,7 +23,7 @@ class OrderTrackingController extends Controller
     
                     $this->trackings = $this->trackings->toArray();
                     
-                    return apiResponse(true,'Order found', ['trackings'=> $this->trackings, 'apiResponse' => null ]);
+                    return apiResponse(true,'Order found', ['hdTrackings'=> $this->trackings, 'apiTrakings' => null ]);
                 }
                 if($response['service'] == 'USPS')
                 {
@@ -31,22 +31,22 @@ class OrderTrackingController extends Controller
     
                     $this->trackings = $this->trackings->toArray();
                     
-                    return apiResponse(true,'Order found',['trackings'=> $this->trackings, 'apiResponse' => null ]);
+                    return apiResponse(true,'Order found',['hdTrackings'=> $this->trackings, 'apiTrakings' => null ]);
                 }
                 if($response['service'] == 'Correios_Brazil')
                 {
                     $this->trackings = $response['trackings']->toArray();
                     $apiTracking = $response['api_trackings']->toArray(); 
-                    return apiResponse(true,'Order found',['trackings'=> $this->trackings, 'apiResponse' => $apiTracking]); 
+                    return apiResponse(true,'Order found',['hdTrackings'=> $this->trackings, 'apiTrakings' => $apiTracking]); 
                 }
                 
                 $this->trackings = $response['trackings']->toArray();
     
-                return apiResponse(true,'Order found',['trackings'=> $this->trackings , 'apiResponse' => null]);
+                return apiResponse(true,'Order found',['hdTrackings'=> $this->trackings , 'apiTrakings' => null]);
             }
         }
 
-        return apiResponse(false,'Order not found', ['trackings'=> $this->trackings, 'apiResponse' => null ]);
+        return apiResponse(false,'Order not found', ['hdTrackings'=> $this->trackings, 'apiTrakings' => null ]);
         
     }
 
