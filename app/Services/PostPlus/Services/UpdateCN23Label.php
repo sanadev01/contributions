3dbfe1d893988ccea3dad153732ad9528fb40cde
sanadev01 <sanasheraz.dev@gmail.com>
2023-03-01 22:51:13 +0500

    Update Label CN23 Post Plus

--- app/Services/PostPlus/Services/UpdateCN23Label.php
+++ app/Services/PostPlus/Services/UpdateCN23Label.php
@@ -66,52 +66,67 @@ class UpdateCN23Label
             // }    
         }else {
             // initiate FPDI
-            $this->pdfi = new PDFRotate('L', 'mm', array(147, 212.9));
-            // $this->pdfi->setPrintHeader(false);
-            $this->pdfi->AddPage();
-            $this->pdfi->SetMargins(0, 0, 0);
-            // set the source file 
-            $this->pdfi->setSourceFile($this->pdf_file);
-            // import page 1
-            $tplId = $this->pdfi->importPage(1);
-            // use the imported page and place it at point 0,0
-            $this->pdfi->useTemplate($tplId, 0, 0);
-            $this->pdfi->SetFillColor(255, 255, 255);
-            $this->pdfi->SetFont("Arial", "", 7);
-            //FOR SENDER NAME & ADDRESS
-            $this->pdfi->SetFont("Arial", "B", 6);
-            $this->pdfi->RotatedText(60.7, 26.2, 'Sender:', 0);
-            $this->pdfi->SetFont("Arial", "B", 6);
-            $this->pdfi->RotatedText(60.7, 28, $this->order->getSenderFullName(), 0);
-            $this->pdfi->SetFont("Arial", "", 6);
-            $this->pdfi->RotatedText(60.7, 30, "2200 NW 129TH AVE", 0);
-            $this->pdfi->SetFont("Arial", "", 6);
-            $this->pdfi->RotatedText(60.7, 32, "United States 33182 Miami FL", 0);
-            //FOR SHIPPING
-            $this->pdfi->SetFont("Arial", "B", 6);
-            $this->pdfi->RotatedText(12.5, 89.5, 'Shipping:', 00);
-            //FOR SHIPPING COST
-            $this->pdfi->SetFont("Arial", "B", 6);
-            $userDeclaredFreight = $this->order->user_declared_freight <= 0.01 ? 0 : $this->order->user_declared_freight;
-            $this->pdfi->RotatedText(135.8, 89.5, number_format($userDeclaredFreight, 2, '.', ','), 0);
-            //FOR TOTAL ORDER VALUE
-            $this->pdfi->SetFillColor(255, 255, 255);
-            $this->pdfi->Rect(135.8, 92.5, 6, 3, "F");
-            $this->pdfi->SetFont("Arial", "B", 6);
-            $this->pdfi->RotatedText(136, 95.2, number_format($userDeclaredFreight + $this->order->order_value, 2, '.', ','), 0);
-            //FOR CPF#
-            if ($this->order->recipient->tax_id) {
-                $this->pdfi->SetFont("Arial", "B", 7);
-                $this->pdfi->RotatedText(186, 39.7, 'CPF: ' . $this->order->recipient->tax_id, 0);
+            if(count($this->order->items) > 5) {
+                $this->pdfi = new PDFRotate('P', 'mm', array(212.9, 275));
+                $this->textEMSLabel(118.7, 212.5, 135.8, 212.5, 136, 215, 12.5, 234.3, 133.1, 213, 118.6, 211.1, 118.7, 215);
+                
+            }else {
+                $this->pdfi = new PDFRotate('L', 'mm', array(147, 212.9));
+                $this->textEMSLabel(118.7, 92.5, 135.8, 92.5, 136, 95.2, 12.5, 114.7, 133.8, 92.9, 117.8, 91.2, 118.7, 95.3);
             }
-            //FOR REFERENCE NO
-            if ($this->order->customer_reference || $order->tracking_id) {
-                $this->pdfi->SetFont("Arial", "B", 7);
-                $this->pdfi->RotatedText(12.5, 114.7, 'Ref#: ' . ($this->order->customer_reference ? $this->order->customer_reference : $this->order->tracking_id).' HD-'.$this->order->id, 0);
-            } 
+            
         }
         //OUTPUT
         $this->pdfi->Output($this->pdf_file, 'F');
         return true;
+    }
+    
+    public function textEMSLabel($sw, $sh, $cw, $ch, $fw, $fh, $rw, $rh, $bw, $bh, $sbw, $sbh, $tvw, $tvh) {
+
+        // $this->pdfi->setPrintHeader(false);
+        $this->pdfi->AddPage();
+        $this->pdfi->SetMargins(0, 0, 0);
+        // set the source file 
+        $this->pdfi->setSourceFile($this->pdf_file);
+        // import page 1
+        $tplId = $this->pdfi->importPage(1);
+        // use the imported page and place it at point 0,0
+        $this->pdfi->useTemplate($tplId, 0, 0);
+        $this->pdfi->SetFillColor(255, 255, 255);
+        $this->pdfi->SetFont("Arial", "", 7);
+        //FOR SENDER NAME & ADDRESS
+        $this->pdfi->SetFont("Arial", "B", 6);
+        $this->pdfi->RotatedText(60.7, 26.2, 'Sender:', 0);
+        $this->pdfi->SetFont("Arial", "B", 6);
+        $this->pdfi->RotatedText(60.7, 28, $this->order->getSenderFullName(), 0);
+        $this->pdfi->SetFont("Arial", "", 6);
+        $this->pdfi->RotatedText(60.7, 30, "2200 NW 129TH AVE", 0);
+        $this->pdfi->SetFont("Arial", "", 6);
+        $this->pdfi->RotatedText(60.7, 32, "United States 33182 Miami FL", 0);
+        //FOR CPF#
+        if ($this->order->recipient->tax_id) {
+            $this->pdfi->SetFont("Arial", "B", 7);
+            $this->pdfi->RotatedText(186, 39.7, 'CPF: ' . $this->order->recipient->tax_id, 0);
+        }
+        //FOR SHIPPING
+        $this->pdfi->SetFillColor(255, 255, 255);
+        $this->pdfi->Rect($sbw, $sbh, 9.3, 2.5, "F");
+        $this->pdfi->SetFont("Arial", "", 5);
+        $this->pdfi->RotatedText($sw, $sh, 'Shipping', 0);
+        $this->pdfi->RotatedText($tvw, $tvh, 'Total value', 0);
+        //FOR SHIPPING COST
+        $this->pdfi->SetFont("Arial", "B", 5);
+        $userDeclaredFreight = $this->order->user_declared_freight <= 0.01 ? 0 : $this->order->user_declared_freight;
+        $this->pdfi->RotatedText($cw, $ch, number_format($userDeclaredFreight, 2, '.', ','), 0);
+        //FOR TOTAL ORDER VALUE
+        $this->pdfi->SetFillColor(255, 255, 255);
+        $this->pdfi->Rect($bw, $bh, 8, 2.5, "F");
+        $this->pdfi->SetFont("Arial", "B", 6);
+        $this->pdfi->RotatedText($fw, $fh, number_format($userDeclaredFreight + $this->order->order_value, 2, '.', ','), 0);
+        //FOR REFERENCE NO
+        if ($this->order->customer_reference || $order->tracking_id) {
+            $this->pdfi->SetFont("Arial", "B", 7);
+            $this->pdfi->RotatedText($rw, $rh, 'Ref#: ' . ($this->order->customer_reference ? $this->order->customer_reference : $this->order->tracking_id).' HD-'.$this->order->id, 0);
+        } 
     }
 }
