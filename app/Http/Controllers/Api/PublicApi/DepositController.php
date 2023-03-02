a9d1be2edcefd43a69eee17ea59b818f62264338
sanadev01 <sanasheraz.dev@gmail.com>
2023-03-03 00:25:01 +0500

    deposit

--- app/Http/Controllers/Api/PublicApi/DepositController.php
+++ app/Http/Controllers/Api/PublicApi/DepositController.php
@@ -14,9 +14,9 @@ class DepositController extends Controller
     {
         $startDate = $request->start_date ??date('Y-m-01'); 
         $endDate  = $request->end_date  ??date('Y-m-d'); 
-        $deposits = Deposit::with(['orders.tax','user'])->when( $request->tracking_code,function($query)  use($request){
-            return $query->whereHas('orders',function($query) use($request){
-              return $query->where('corrios_tracking_code',$request->tracking_code);
+        $deposits = Deposit::with(['orders.tax','user'])->when( $request->tracking_code,function($query,$tracking_code){
+            return $query->whereHas('orders',function($query) use($tracking_code){
+              return $query->where('corrios_tracking_code',$tracking_code);
         });
         })->where('user_id',Auth::id())->filter($startDate,$endDate)->get();
 
