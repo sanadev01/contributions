386a5eddaa83316938188ea401b6db800bc1fcc8
sanadev01 <sanasheraz.dev@gmail.com>
2023-04-13 22:07:29 +0500

    Trash Order Report Get

--- app/Http/Controllers/Admin/Order/OrderExportController.php
+++ app/Http/Controllers/Admin/Order/OrderExportController.php
@@ -16,10 +16,11 @@ class OrderExportController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $todayDate = Carbon::now()->format('Y-m-d');
         $startDate = $request->start_date;
         $endDate = $request->end_date;
-        $startDate = $startDate ? $startDate : Carbon::now()->format('Y-m-d');
-        $endDate = $endDate ? $endDate : Carbon::now()->format('Y-m-d');
+        $startDate = $startDate ? $startDate : $todayDate;
+        $endDate = $endDate ? $endDate : $todayDate;
 
         $report = Reports::create([
             'user_id' => Auth::id(),
