0e50feda8e7ec2301a2b4ff5c7c8afa125c779ed
sanadev01 <sanasheraz.dev@gmail.com>
2023-04-17 20:47:10 +0500

    update versio

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Milon\Barcode\BarcodeServiceProvider::class,
+        // Milon\Barcode\BarcodeServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -234,8 +234,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
-        'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class
+        // 'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
+        // 'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class
     ],
 
 ];
