c5f9ad1d5ba26a0930265c11e833c04208384a24
sanadev01 <sanasheraz.dev@gmail.com>
2023-04-18 16:32:20 +0500

    warehous

--- app/Http/functions.php
+++ app/Http/functions.php
@@ -219,36 +219,36 @@ function orderProductsValue($products)
            return  $count + ($product['value'])*($product['quantity']);
     });
 }
-function orignalWarehouseNumber($whrNo)
-{  
-    if(count(explode("-", $whrNo))>1){
-        $whrNo = explode("-", $whrNo)[1];
-    }
-    $whrNo = trim($whrNo);
-    $whrLen = strlen($whrNo)-4; 
+// function orignalWarehouseNumber($whrNo)
+// {  
+//     if(count(explode("-", $whrNo))>1){
+//         $whrNo = explode("-", $whrNo)[1];
+//     }
+//     $whrNo = trim($whrNo);
+//     $whrLen = strlen($whrNo)-4; 
 
-    switch(true){
-        case($whrLen < 1):
-        {
-            $orignalWhrNo = -1;
-            break; 
-        }
-        case ($whrLen <= 3):{
-            $orignalWhrNo = substr($whrNo,0,$whrLen);
-            break;
-        } 
-        case ($whrLen <= 6):{
-             $orignalWhrNo = substr($whrNo,0,3).substr($whrNo,5,$whrLen-3);
-            break;
-        }
-        case ($whrLen <= 9 ):{
-            $orignalWhrNo = substr($whrNo,0,3).substr($whrNo,5,3).substr($whrNo,10,3);;
-            break;
-        }
-        case ($whrLen >=10 ):{
-            $orignalWhrNo = substr($whrNo,0,3).substr($whrNo,5,3). substr($whrNo,8,3) .substr($whrNo,13);
-            break;
-        }
-    }
-    return (int) $orignalWhrNo;
-}
+//     switch(true){
+//         case($whrLen < 1):
+//         {
+//             $orignalWhrNo = -1;
+//             break; 
+//         }
+//         case ($whrLen <= 3):{
+//             $orignalWhrNo = substr($whrNo,0,$whrLen);
+//             break;
+//         } 
+//         case ($whrLen <= 6):{
+//              $orignalWhrNo = substr($whrNo,0,3).substr($whrNo,5,$whrLen-3);
+//             break;
+//         }
+//         case ($whrLen <= 9 ):{
+//             $orignalWhrNo = substr($whrNo,0,3).substr($whrNo,5,3).substr($whrNo,10,3);;
+//             break;
+//         }
+//         case ($whrLen >=10 ):{
+//             $orignalWhrNo = substr($whrNo,0,3).substr($whrNo,5,3). substr($whrNo,8,3) .substr($whrNo,13);
+//             break;
+//         }
+//     }
+//     return (int) $orignalWhrNo;
+// }
