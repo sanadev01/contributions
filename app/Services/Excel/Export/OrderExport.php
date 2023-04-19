278e01cd2495613ed25275b59b07f50214473819
sanadev01 <sanasheraz.dev@gmail.com>
2023-04-19 22:14:59 +0500

    Order Export Filetes

--- app/Services/Excel/Export/OrderExport.php
+++ app/Services/Excel/Export/OrderExport.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Services\Excel\Export;
-use App\Models\Order;
 use App\Models\User;
+use App\Models\Order;
+use App\Models\ShippingService;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
