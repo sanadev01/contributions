0cf083a8b444074c9aeb65f9ce36d2d3c35c6468
sanadev01 <sanasheraz.dev@gmail.com>
2023-05-03 22:12:21 +0500

    Colombia APi Label URLs Updated

--- config/ColombiaService.php
+++ config/ColombiaService.php
@@ -2,11 +2,11 @@
 
 return [
     'testing' => [
-        'shippingUrl' => 'http://appcer.4-72.com.co/WcfServiceSPOKE/ServiceSPOKE.svc/PostShipping',
+        'shippingUrl' => 'http://appcer.4-72.com.co/WcfServiceSPOKE/ServiceSPOKE.svc/PostShippingWithoutFile',
         'containerRegisterUrl' => 'http://190.144.185.12:8290/servicio/pruebasEntrega/v1.0.0/pruebaEntrega',
     ],
     'production' => [
-        'shippingUrl' => 'http://appcer.4-72.com.co/WcfServiceSPOKE/ServiceSPOKE.svc/PostShipping',
+        'shippingUrl' => 'http://svc1.sipost.co/WcfServiceSPOKE/ServiceSPOKE.svc/PostShippingWithoutFile',
         'containerRegisterUrl' => 'http://190.144.185.12:8290/servicio/pruebasEntrega/v1.0.0/pruebaEntrega',
     ],
     'credentials' => [
