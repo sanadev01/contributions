896084d602db57726cbe63d06e458ac303d1cd65
sanadev01 <sanasheraz.dev@gmail.com>
2023-05-11 19:20:33 +0500

    tracking

--- app/Http/Controllers/Admin/TrackingController.php
+++ app/Http/Controllers/Admin/TrackingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\OrderTracking;
 
 class TrackingController extends Controller
 {
@@ -15,7 +16,7 @@ class TrackingController extends Controller
      */
     public function index()
     {
-        $this->authorize('view',App\Models\OrderTracking::class);        
+        $this->authorize('view',OrderTracking::class);        
         return view('ordertracking.index');
     }
 
