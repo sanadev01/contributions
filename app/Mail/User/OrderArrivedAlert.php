de1b06e4c0e7e370d094dfbfc84c1e476aea31ff
sanadev01 <sanasheraz.dev@gmail.com>
2023-05-23 18:45:01 +0500

    order

---
+++ app/Mail/User/OrderArrivedAlert.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail\User;
+
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderArrivedAlert extends Mailable
+{
+    use Queueable, SerializesModels;
+ 
+    public $order;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param User $user
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.user.order-arrived-alert');
+    }
+}
