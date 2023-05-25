72ea770d17c383092b4f99d1b09d770c0e87e215
sanadev01 <sanasheraz.dev@gmail.com>
2023-05-25 18:56:13 +0500

    arrived order

--- app/Http/Controllers/Api/PublicApi/ArrivedOrderController.php
+++ app/Http/Controllers/Api/PublicApi/ArrivedOrderController.php
@@ -14,9 +14,13 @@ class ArrivedOrderController extends Controller
     public function __invoke(Request $request)
     {
         $orders = Order::where('user_id', Auth::id())->where('arrived_date', '!=', null)
-            ->when($request->from || $request->to, function ($query) use ($request) {
-                $query->whereBetween('arrived_date', [$request->from . ' 00:00:00', $request->to . ' 23:59:59']);
-            });
+        ->when($request->from && $request->to, function ($query) use ($request) {
+            $query->whereBetween('arrived_date', [$request->from . ' 00:00:00', $request->to . ' 23:59:59']);
+        })->when($request->to, function ($query) use ($request) {
+            $query->whereDate('arrived_date',  '<=',$request->to . ' 23:59:59');
+        })->when($request->from , function ($query) use ($request) { 
+            $query->whereDate('arrived_date',  '>=',$request->from . ' 00:00:00');
+        });
         $data['total'] = $orders->count();
         $itemsPerPage = request('item_per_page') ?? "all";
         $data['orders'] = OrderResource::collection($orders
