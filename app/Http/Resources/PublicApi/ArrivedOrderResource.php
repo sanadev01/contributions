25875019e56fd5b51de1b58c4a056f444dbf0462
sanadev01 <sanasheraz.dev@gmail.com>
2023-05-25 19:51:54 +0500

    arrived

--- app/Http/Resources/PublicApi/ArrivedOrderResource.php
+++ app/Http/Resources/PublicApi/ArrivedOrderResource.php
@@ -17,16 +17,12 @@ class ArrivedOrderResource extends JsonResource
         return [
             
             "corrios_tracking_code"=> $this->corrios_tracking_code ,
-            "pobox_number"=> $this->user->pobox_number ,
-            "driver_tracking_name"=> optional(optional($this->driverTracking)->user)->name ,
             "merchant"=> $this->merchant ,
             "Dimensions"=> $this->length ."X".$this->length ."X". $this->height,
-            "kg_weight"=> number_format($this->getWeight('kg'),2),
-            "id"=> $this->id ,
-            "tracking_id"=> $this->tracking_id ,
-            "recipient_first_name"=> $this->recipient->first_name ,
-            "order_date"=> $this->order_date ,
-            "driver_tracking_created_at"=> optional(optional($this->driverTracking)->created_at)->format('m-d-y') ,
+            "weight"=> number_format($this->getWeight('kg'),2),
+            "recipient"=> $this->recipient->first_name ,
+            "order_date"=> $this->order_date->format('Y-m-d'),
+            "picked_at"=> optional(optional($this->driverTracking)->created_at)->format('Y-m-d') ,
             "arrived_date"=> $this->arrived_date ,
             'status'=>$this->status < 80?"Scanned in the warehouse":'Shipped'  , 
         ];
