6604c605aa04f7dcdff7dc41617cac7c1825ecba
sanadev01 <sanasheraz.dev@gmail.com>
2023-05-29 21:45:27 +0500

    testing route removed

--- routes/web.php
+++ routes/web.php
@@ -288,10 +288,5 @@ Route::get('session-refresh/{slug?}', function($slug = null){
     session()->forget('anjun_token');
     Cache::forget('anjun_token');
     return 'Anjun Token refresh';
-}); 
-Route::get('order-arrived', function(){
-    \Artisan::call('email:order-arrived');
-
-    return 'sended';
 });
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index')->middleware('auth');
