e77879cbe827af1ad7638b2378d9a28a56f3bd5c
sanadev01 <sanasheraz.dev@gmail.com>
2023-05-29 20:15:29 +0500

    update sender address

--- routes/web.php
+++ routes/web.php
@@ -261,10 +261,10 @@ Route::get('order/{order}/us-label/get', function (App\Models\Order $order) {
     return response()->download(storage_path("app/labels/{$order->us_api_tracking_code}.pdf"),"{$order->us_api_tracking_code} - {$order->warehouse_number}.pdf",[],'inline');
 })->name('order.us-label.download');
 
-Route::get('test-label',function(){
+Route::get('test-label/{id}',function($id){
 
     $labelPrinter = new CN23LabelMaker();
-    $order = Order::find(91240);
+    $order = Order::find($id);
     // $order->status = 70;
     // $order->save();
     // dd($order);
