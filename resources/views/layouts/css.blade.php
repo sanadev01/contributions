6d426b9e501fed59305b8ced3103e9953da53fd2
sanadev01 <sanasheraz.dev@gmail.com>
2023-06-13 20:00:48 +0500

    Dynamic CSS and JS Load

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -20,8 +20,8 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/bootstrap-extended.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/colors.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/components.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/dark-layout.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/semi-dark-layout.css') }}">
+<!-- <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/dark-layout.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/themes/semi-dark-layout.css') }}"> -->
 
 {{-- Toggleable Css --}}
 <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/core/menu/menu-types/vertical-menu.css') }}">
