2524111cf39cb202d82e717ed230ff11be59882a
sanadev01 <sanasheraz.dev@gmail.com>
2023-07-10 22:45:42 +0500

    Tracking Code Endpoint

---
+++ app/Http/Controllers/Api/PublicApi/TrackingController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\publicApi;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\OrderTrackingRepository;
+
+class TrackingController extends Controller
+{
+    public $trackings;
+
+    public function __invoke($search = null)
+    {
+        $orderTrackingRepository = new OrderTrackingRepository($search);
+        $this->trackings = $orderTrackingRepository->getMarketPlaceTrackings();
+
+            if(!is_null($this->trackings))
+            {
+                return apiResponse(true,'Trackings Found',['hdTrackings'=> $this->trackings]); 
+            }
+
+        return apiResponse(false,'Tracking not found', ['hdTrackings'=> $this->trackings]);
+        
+    }
+}
