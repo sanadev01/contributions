f9fe55794f97bf5d493d266562914e47e1149297
sanadev01 <sanasheraz.dev@gmail.com>
2023-07-10 16:36:19 +0500

    charts added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-@section('css') 
+@section('css')
     <script src="{{ asset('app-assets/vendors/js/charts/echarts/echarts.min.js') }}"></script>
     <style>
         .order-card {
@@ -7,24 +7,24 @@
         }
 
         .bg-c-blue {
-            background: linear-gradient(45deg, #4099ff, #73b4ff);
+            background: linear-gradient(45deg, #A5DB92, #007C05);
         }
 
         .bg-c-green {
-            background: linear-gradient(45deg, #2ed8b6, #59e0c5);
+            background: linear-gradient(45deg, #52B6FE, #6154FE);
         }
 
         .bg-c-yellow {
-            background: linear-gradient(45deg, #FFB64D, #ffcb80);
+            background: linear-gradient(45deg, #FF2C6B, #FF94A1);
         }
 
         .bg-c-pink {
-            background: linear-gradient(45deg, #FF5370, #ff869a);
+            background: linear-gradient(45deg, #FF974B, #FF94A1);
         }
 
 
         .card_block {
-            border-radius: 5px;
+            border-radius: 15px;
             -webkit-box-shadow: 0 1px 2.94px 0.06px rgba(4, 26, 55, 0.16);
             box-shadow: 0 1px 2.94px 0.06px rgba(4, 26, 55, 0.16);
             border: none;
@@ -51,14 +51,15 @@
     </style>
 @endsection
 
-@section('page') 
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb light-color"> 
-                <li class="breadcrumb-item"><a href="#">  <img src="{{ asset('images/icon/dashboard.svg') }}" alt="dashboard"></a></li>
-                {{-- <li class="breadcrumb-item"><a href="#">Library</a></li> --}}
-                <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
-            </ol>
-        </nav> 
+@section('page')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb light-color">
+            <li class="breadcrumb-item"><a href="#"> <img src="{{ asset('images/icon/dashboard.svg') }}"
+                        alt="dashboard"></a></li>
+            {{-- <li class="breadcrumb-item"><a href="#">Library</a></li> --}}
+            <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+        </ol>
+    </nav>
     {{-- contact us --}}
     <div class='row no-gutters d-flex align-items-center'>
         <div class="col-4">
@@ -69,14 +70,14 @@
                 </dl>
             </div>
         </div>
-        <div class="col-8 light-green-color p-2"> 
-                <div class="row mt-0 ">
-                    <div class="col-12 pb-xl-2 pb-1 h-25">
-                        <a href="{{ url('tickets') }}" target="_blank"> <img class="banner rounded-4"
-                                src="{{ asset('images/kpi-banner.png') }}" width="100%" height="auto" alt="contact us">
-                        </a>
-                    </div>
-                </div>  
+        <div class="col-8 light-green-color p-2">
+            <div class="row mt-0 ">
+                <div class="col-12 pb-xl-2 pb-1 h-25">
+                    <a href="{{ url('tickets') }}" target="_blank"> <img class="banner rounded-4"
+                            src="{{ asset('images/kpi-banner.png') }}" width="100%" height="auto" alt="contact us">
+                    </a>
+                </div>
+            </div>
         </div>
 
     </div>
@@ -128,7 +129,91 @@
         <div class="card  border-radius-15 mt-3">
             <livewire:dashboard.stats-filter />
         </div>
+        <div class="row mt-3">
+            <div class="card col-7 border-radius-15 p-5">
+                <canvas id="bar"></canvas>
+
+            </div>
+            <div class="card col-5 border-radius-15 p-5">
+                <p>Total Orders</p>
+                <div class="row">
+                    <div class="col-6">
+                        <h6>Total Monthly Order</h6> 
+                        <h2> {{ $orders['currentmonthTotal'] }} </h2>
+                    </div>
+                    <div class="col-6"> 
+                        <h6>Total Year Order</h6> 
+                        <h2> {{ $orders['currentYearTotal'] }} </h2>
+                    </div>
+                </div>
+                <canvas id="doughnut"></canvas> 
+            </div>
+        </div>
+
+        </div>
 
     </section>
     <!-- Dashboard Analytics end -->
+@endsection
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script>
+        const bar = document.getElementById('bar');
+
+        new Chart(bar, {
+            type: 'bar',
+            data: {
+                labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+                datasets: [{
+                    label: 'Sales Analytics',
+                    data: [12, 19, 12, 19, 3, 5, 2, 3, 3, 5, 2, 3],
+                    borderWidth: 1
+                }, {
+                    label: 'Total Orders',
+                    data: [12, 1, 3, 19, 3, 5, 2, 3, 3, 5, 2, 3],
+                    borderWidth: 1
+                }, ]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+
+
+        const doughnut = document.getElementById('doughnut');
+
+        new Chart(doughnut, {
+            type: 'doughnut',
+            data: {
+                labels: [
+                    'Red',
+                    'Blue',
+                    'Yellow'
+                ],
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)'
+                    ],
+                    hoverOffset: 4
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
 @endsection
--- resources/views/livewire/dashboard/stats-filter.blade.php
+++ resources/views/livewire/dashboard/stats-filter.blade.php
@@ -14,60 +14,68 @@
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-            <div class="card_block bg-c-green order-card">
-                <div class="card-block">
+            <div class="card_block bg-c-green order-card  border-radius-15">
+                <div class="card-block   border-radius-15">
                     <div class="row"> 
                         <div class="col-9"> 
-                            <h6 class="white min-h-25">@lang('dashboard.Today Orders')</h6> 
+                            <h6 class="white height-30">@lang('dashboard.Today Orders')</h6> 
                         </div>
-                        <div class="col-3"><i class="fa fa-cart-plus f-left white"></i></div>
+                        <div class="col-3 d-flex justify-content-end">
+                            <i class="fa fa-cart-plus f-left white"></i>
+                        </div>
                     </div>
                     <h2>
                         <span class="white">{{ $orders['currentDayTotal'] }}</span>
                     </h2>
-                    <h6 class="m-b-0 white">@lang('dashboard.Completed Orders')
+                    <h6 class="white">@lang('dashboard.Completed Orders')
                         <span class="f-right">{{ $orders['currentDayConfirm'] }}</span>
                     </h6>
                 </div>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-            <div class="card_block bg-c-yellow order-card">
-                <div class="card-block">
+            <div class="card_block bg-c-yellow order-card border-radius-15">
+                <div class="card-block   border-radius-15">
                     <div class="row"> 
                         <div class="col-9"> 
-                            <h6 class="white min-h-25">@lang('dashboard.Total Month Order', ['month' => $orders['monthName']])</h6>
+                            <h6 class="white height-30">@lang('dashboard.Total Month Order', ['month' => $orders['monthName']])</h6>
                         </div>
-                        <i class="fas fa-calendar-week f-left white"></i>
+                        <div class="col-3 d-flex justify-content-end">
+                            <i class="fas fa-calendar-week f-left white"></i>
+                        </div>
                     </div>
                     <h2><span class="white">{{ $orders['currentmonthTotal'] }}</span></h2>
-                    <h6 class="m-b-0 white">@lang('dashboard.Completed Orders')
+                    <h6 class="white">@lang('dashboard.Completed Orders')
                         <span class="f-right white">{{ $orders['currentmonthConfirm'] }}</span>
                     </h6>
                 </div>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-            <div class="card_block bg-c-pink order-card">
-                <div class="card-block">
+            <div class="card_block bg-c-pink order-card  border-radius-15">
+                <div class="card-block   border-radius-15">
                     <div class="row">
-                        <h6 class="col-9  white">@lang('dashboard.Current Year')</h6>
-                        <i class="fa fa-credit-card f-left white"></i> 
+                        <h6 class="col-9 white height-30">@lang('dashboard.Current Year')</h6>
+                        <div class="col-3 d-flex justify-content-end">
+                            <i class="fa fa-credit-card f-left white"></i> 
+                        </div>
                     </div>
                     <h2><span class="white"> {{ $orders['currentYearTotal'] }} </span></h2>  
-                    <h6 class="m-b-0 white">@lang('dashboard.Completed Orders')<span  class="f-right white">{{ $orders['currentYearConfirm'] }}</span></h6>
+                    <h6 class="white">@lang('dashboard.Completed Orders')<span  class="f-right white">{{ $orders['currentYearConfirm'] }}</span></h6>
                 </div>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
-            <div class="card_block bg-c-blue order-card">
-                <div class="card-block">
+            <div class="card_block bg-c-blue order-card  border-radius-15">
+                <div class="card-block   border-radius-15">
                     <div class="row">
-                         <h6 class="col-9  white">@lang('dashboard.Total Orders')</h6>
-                         <i class="col-3 fa fa-cart-plus f-left white"></i>
+                         <h6 class="col-9  white height-30">@lang('dashboard.Total Orders')</h6>
+                         <div class="col-3 d-flex justify-content-end">
+                                 <i class=" fa fa-cart-plus f-left white"></i>
+                         </div>
                     </div>
                     <h2><span class="white">{{ $orders['totalOrders'] }}</span></h2>
-                    <h6 class="m-b-0 white">@lang('dashboard.Completed Orders')<span class="f-right white">{{ $orders['totalCompleteOrders'] }}</span></h6>
+                    <h6 class="white">@lang('dashboard.Completed Orders')<span class="f-right white">{{ $orders['totalCompleteOrders'] }}</span></h6>
                 </div>
             </div>
         </div>
