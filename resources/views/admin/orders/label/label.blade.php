49372b0113fc377b26f70b27661e020011b91f30
sanadev01 <sanasheraz.dev@gmail.com>
2023-08-04 01:01:02 +0500

    total express

--- resources/views/admin/orders/label/label.blade.php
+++ resources/views/admin/orders/label/label.blade.php
@@ -2,7 +2,9 @@
     <div class="col-md-12 text-right">
         <button class="btn btn-primary" onclick="reloadLabel({{$order->id}},'#row_{{$order->id}}')">Reload</button>
         @if (!$error)
-            <button onclick="window.open('@if($order->shippingService->is_total_express) {{ $order->totalExpressLabelUrl() }} @else{{ route('order.label.download',[encrypt($order->id),'time'=>md5(microtime())]) }}@endif','','top:0,left:0,width:600px;height:700px;')" class="btn btn-primary">Download</button>
+            <button onclick="window.open('@if($order->shippingService->isColombiaService()) {{ $order->colombiaLabelUrl() }}
+            @elseif($order->shippingService->is_total_express) {{ $order->totalExpressLabelUrl() }}
+            @else{{ route('order.label.download',[encrypt($order->id),'time'=>md5(microtime())]) }}@endif','','top:0,left:0,width:600px;height:700px;')" class="btn btn-primary">Download</button>
             <!-- <button class="btn btn-primary" onclick="updateLabel({{$order->id}},'#row_{{$order->id}}')">Update</button> -->
             @can('canPrintLableUpdate', $order)
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#confirm">Update</button>
@@ -11,6 +13,7 @@
         @endif
     </div>
 </div>
+
 @if (!$buttonsOnly)
     <div class="label mt-2">
         @if (!$error)
@@ -18,22 +21,18 @@
                 Sinerlog modification
                 Shows the sinerlog label
              -->
-            @if ($order->sinerlog_tran_id != '')
-                <iframe src="@isset($renderLabel) {{$renderLabel}} @endisset" style="width:100%; height:700px;" frameborder="0">
+             @if ($order->sinerlog_tran_id != '')
+            <iframe src="" style="width:100%; height:700px;" frameborder="0">
                 
-                </iframe>
+            </iframe>
             @else
-                <iframe src="https://docs.google.com/gview?url={{ route('order.label.download', encrypt($order->id)) }}&embedded=true&time{{md5(microtime())}}" style="width:100%; height:700px;" frameborder="0">
-                    <iframe src="{{ route('order.label.download', encrypt($order->id)) }}" style="width:100%; height:700px;" frameborder="0"></iframe>
-                </iframe>
+            <iframe src="https://docs.google.com/gview?url={{ route('order.label.download', encrypt($order->id)) }}&embedded=true&time{{md5(microtime())}}" style="width:100%; height:700px;" frameborder="0">
+                <iframe src="{{ route('order.label.download', encrypt($order->id)) }}" style="width:100%; height:700px;" frameborder="0"></iframe>
+            </iframe>
             @endif
         @else
             <div class="alert alert-danger">
-                <ul> 
-                    @foreach(explode('<br>',$error) as $e)
-                    <li>{{ $e}}</li>
-                    @endforeach
-                </ul>
+                {{ $error }}
             </div>
         @endif
     </div>
