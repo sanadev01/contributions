ae5b1687bc8f59609396341548bc861ae1f679a3
sanadev01 <sanasheraz.dev@gmail.com>
2023-08-15 17:15:38 +0500

    Balace Issue

--- resources/views/livewire/deposit/table-component.blade.php
+++ resources/views/livewire/deposit/table-component.blade.php
@@ -118,9 +118,9 @@
                 </td>
                 <td>
                     @if($deposit->order_id)
-                    <a data-toggle="modal" href="javascript:void(0)" data-target="#hd-modal" data-url="{{ route('admin.modals.order.invoice',$deposit->order_id) }}" class="w-100" title="Show Order Details">
-                        {{ optional($deposit->order)->warehouse_number??"$deposit->order_id  Order Deleted "}}
-                            </a>
+                        <a data-toggle="modal" href="javascript:void(0)" data-target="#hd-modal" data-url="{{ route('admin.modals.order.invoice',$deposit->order_id) }}" class="w-100" title="Show Order Details">
+                            {{ $deposit->getOrder($deposit->order_id)->warehouse_number }}
+                        </a>
                     @else
                          {{  "$deposit->order_id  Order Deleted "}}
                     @endif
