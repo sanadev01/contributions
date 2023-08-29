7a50cdb556ba3fa5564b77c5464ed38b938791eb
sanadev01 <sanasheraz.dev@gmail.com>
2023-08-30 00:25:21 +0500

    Total Carrier APi

--- app/Services/TotalExpress/Services/TotalExpressMasterbox.php
+++ app/Services/TotalExpress/Services/TotalExpressMasterbox.php
@@ -171,10 +171,7 @@ class TotalExpressMasterBox
             return $this->consultCloseManifest($response->data->request_id, $deliveryBill);
         }
         else{ 
-            return [ 
-                'type'=>'alert-danger',
-                'message'=> ''.new HandleError($apiRequest)
-            ]; 
+            return $this->responseUnprocessable($response->messages[0]);
         }
     }
 
@@ -192,7 +189,7 @@ class TotalExpressMasterBox
             return $this->responseSuccessful($response, $response->messages[0]);
         }
         else{ 
-            return $this->responseUnprocessable($response->messages[0][0]);
+            return $this->responseUnprocessable($response);
         }
     }
 
