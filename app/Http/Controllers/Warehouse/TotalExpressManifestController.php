9cf9d267d2c880acde57da466d8e6a70cac254bf
sanadev01 <sanasheraz.dev@gmail.com>
2023-08-30 22:33:54 +0500

    Total Carrier Api in Dev

--- app/Http/Controllers/Warehouse/TotalExpressManifestController.php
+++ app/Http/Controllers/Warehouse/TotalExpressManifestController.php
@@ -13,18 +13,35 @@ use App\Services\TotalExpress\Services\TotalExpressMasterBox;
 
 class TotalExpressManifestController extends Controller
 {
-    public function addFlightDetails(Request $request)
-    {
+    public function createFlight(Request $request) {
         $deliveryBill = DeliveryBill::find($request->id);
+        $container = $deliveryBill->containers->first();
         
         if ($deliveryBill->containers->isEmpty()) {
             session()->flash('alert-danger',  'please add a container to this delivery bill');
             return back()->withInput();
         }
-        
-        $container = $deliveryBill->containers->first();
 
         $apiRequest = (new TotalExpressMasterBox($container))->createFlight($deliveryBill, $request);
+        $response = $apiRequest->getData();
+        if ($response->isSuccess){
+            session()->flash('alert-success', $response->message);
+            return back();
+              
+        } else {
+            session()->flash('alert-danger',$response->message);
+            return back();
+        }
+    }
+    
+    public function addFlightDetails(Request $request)
+    {
+        $deliveryBill = DeliveryBill::find($request->id);
+               
+        $container = $deliveryBill->containers->first();
+
+        $apiRequest = (new TotalExpressMasterBox($container))->updateFlightInformation($deliveryBill->cnd38_code, $request);
+
         session()->flash($apiRequest['type'],$apiRequest['message']);
         return back();
  
@@ -35,6 +52,22 @@ class TotalExpressManifestController extends Controller
         $deliveryBill = DeliveryBill::find($request->id);
         $container = $deliveryBill->containers->first();
         $apiRequest = (new TotalExpressMasterBox($container))->closemanifest($deliveryBill);
+        $response = $apiRequest->getData();
+        if ($response->isSuccess){
+            session()->flash('alert-success', $response->message);
+            return back();
+              
+        } else {
+            session()->flash('alert-danger',$response->message);
+            return back();
+        }
+    }
+
+    public function closeFlight(Request $request) {
+        
+        $deliveryBill = DeliveryBill::find($request->id);
+        $container = $deliveryBill->containers->first();
+        $apiRequest = (new TotalExpressMasterBox($container))->consultCloseManifest($deliveryBill->request_id);
         $response = $apiRequest->getData();
         if ($response->isSuccess){
             session()->flash('alert-success', $response->message);
