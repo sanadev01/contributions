278ebfaebe9f92251f6b6c2731a9d8d4ccc10d4f
sanadev01 <sanasheraz.dev@gmail.com>
2023-09-01 23:55:44 +0500

    tracking

--- app/Http/Controllers/Api/PublicApi/TrackingController.php
+++ app/Http/Controllers/Api/PublicApi/TrackingController.php
@@ -14,7 +14,7 @@ class TrackingController extends Controller
     public function __invoke(Request $request)
     {
         $orderTrackingRepository = new OrderTrackingRepository($search = null);
-        $this->trackings = $orderTrackingRepository->getOrderTrackings($request);
+        $this->trackings = $orderTrackingRepository->getTrackings($request);
             if(count($this->trackings))
             {
                 return apiResponse(true,'Trackings Found',['hdTrackings'=> $this->trackings]); 
