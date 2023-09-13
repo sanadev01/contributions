91ff7dd04cb95e04f764bcb86207cf0ee9918a54
sanadev01 <sanasheraz.dev@gmail.com>
2023-09-13 19:15:27 +0500

    directlink

--- app/Models/Warehouse/DeliveryBill.php
+++ app/Models/Warehouse/DeliveryBill.php
@@ -74,7 +74,7 @@ class DeliveryBill extends Model
 
     public function isSwedenPost()
     {
-        if($this->containers->first()->services_subclass_code == ShippingService::Prime5 || $this->containers->first()->services_subclass_code == ShippingService::Prime5RIO){
+        if($this->containers->first()->is_directlink_coutry ||$this->containers->first()->services_subclass_code == ShippingService::Prime5 || $this->containers->first()->services_subclass_code == ShippingService::Prime5RIO){
             return true;
         }
     }
