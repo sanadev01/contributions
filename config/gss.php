b570bf07149e360b08f1bcc3ba00ef22308f4c21
sanadev01 <sanasheraz.dev@gmail.com>
2023-09-20 00:25:51 +0500

    GSS Update Production Creds

--- config/gss.php
+++ config/gss.php
@@ -8,10 +8,10 @@ return [
         'baseUrl' => 'https://gss.usps.com/usps-cpas/Testgssapiv2/api',
     ],
     'production' => [
-        'userId' => 'HFTSdemo',
-        'password' => 'FLOR23test',
-        'locationId' => 'TESTSITEHERCOFRGTUSM',
-        'workStationId' => 'HERCOFRGTUSM',
-        'baseUrl' => 'https://gss.usps.com/usps-cpas/Testgssapiv2/api',
+        'userId' => 'HERFuser',
+        'password' => 'MFLA#831gss',
+        'locationId' => 'HERCOFRGTUSM',
+        'workStationId' => 'MIAMIFLOHERCOFRGTUSM',
+        'baseUrl' => 'https://gss.usps.com/usps-cpas/gssapiv2',
         ]
     ];
