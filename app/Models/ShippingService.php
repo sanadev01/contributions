a9252aff8d63018d1948a0a99f9b007473361639
sanadev01 <sanasheraz.dev@gmail.com>
2023-09-20 18:03:53 +0500

    GDE Fix

--- app/Models/ShippingService.php
+++ app/Models/ShippingService.php
@@ -363,27 +363,29 @@ class ShippingService extends Model
 
     public function getGDERate($order){
         $zone = getUSAZone($order->recipient->state->code);
-        $region = Region::where('country_id', $order->recipient->country_id)->where('code', $zone)->first();
-        $weight = ceil(WeightCalculator::kgToGrams($order->weight));
-        if ( $weight<100 ){
-            $weight = 100;
-        }
-        $serviceRates = $region->rates()->first();
-        if($serviceRates){
-            $rate = collect($serviceRates->data)->where('weight','<=',$weight)->sortByDesc('weight')->take(1)->first();
-            if(($rate)['leve'] && setting('gde', null, User::ROLE_ADMIN) && setting('gde', null, $order->user_id)){
-                $type = 'gde_fc_profit';
-                if($this->service_sub_class == self::GDE_PRIORITY_MAIL){
-                    $type = 'gde_pm_profit';
-                }
-                $userProfit = setting($type, null, $order->user_id);
-                $adminProfit = setting($type, null, User::ROLE_ADMIN);
-                $profit = $userProfit ? $userProfit : $adminProfit; 
-                $rate = ($profit / 100) * $rate['leve'] + $rate['leve'];
-                return number_format($rate,2);
+        if($zone) {
+            $region = Region::where('country_id', $order->recipient->country_id)->where('code', $zone)->first();
+            $weight = ceil(WeightCalculator::kgToGrams($order->weight));
+            if ( $weight<100 ){
+                $weight = 100;
             }
+            $serviceRates = $region->rates()->first();
+            if($serviceRates){
+                $rate = collect($serviceRates->data)->where('weight','<=',$weight)->sortByDesc('weight')->take(1)->first();
+                if(($rate)['leve'] && setting('gde', null, User::ROLE_ADMIN) && setting('gde', null, $order->user_id)){
+                    $type = 'gde_fc_profit';
+                    if($this->service_sub_class == self::GDE_PRIORITY_MAIL){
+                        $type = 'gde_pm_profit';
+                    }
+                    $userProfit = setting($type, null, $order->user_id);
+                    $adminProfit = setting($type, null, User::ROLE_ADMIN);
+                    $profit = $userProfit ? $userProfit : $adminProfit; 
+                    $rate = ($profit / 100) * $rate['leve'] + $rate['leve'];
+                    return number_format($rate,2);
+                }
+            }
+            return 0;
         }
-        return 0;
     }
     
 }
