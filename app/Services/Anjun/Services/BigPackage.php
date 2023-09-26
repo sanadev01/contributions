10672937c2099bcf2b2769f891cbc0e9dd0c54f1
sanadev01 <sanasheraz.dev@gmail.com>
2023-09-26 21:52:36 +0500

    anjun master

--- app/Services/Anjun/Services/BigPackage.php
+++ app/Services/Anjun/Services/BigPackage.php
@@ -1,8 +1,11 @@
 <?php
 namespace App\Services\Anjun\Services;
+
+use App\Models\Warehouse\Container as WarehouseContainer;
 use App\Services\Correios\Contracts\Container;
 
-class BigPackage{
+class BigPackage
+{
 
 
 
@@ -13,11 +16,11 @@ class BigPackage{
     public $cfrom         = "MIA";
     public  $trackingNumbers = [];
     public function __construct(Container $container)
-    { 
-             $this->bigBagId      = "AJ00000".$container->id; 
-            foreach ($container->orders as $order){ 
-             $this->trackingNumbers[] = $order->corrios_tracking_code;
-            }
+    {
+        $this->bigBagId      = "AJ00000" . $container->id;
+        $this->serviceType   = ($container->services_subclass_code == WarehouseContainer::CONTAINER_ANJUNC_IX) ? "EXPRESS" : "STANDARD";
+        foreach ($container->orders as $order) {
+            $this->trackingNumbers[] = $order->corrios_tracking_code;
+        }
     }
- 
 }
