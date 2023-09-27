f1c53d168dab0df8ab2e771a5f1ee43ffd0b1618
sanadev01 <sanasheraz.dev@gmail.com>
2023-09-27 21:38:15 +0500

    GSS Services Check on Prod

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -640,7 +640,7 @@ class OrderRepository
                 });
             }
 
-            if (!setting('gss', null, auth()->user()->id) || !setting('gss', null, User::ROLE_ADMIN)) {
+            if (!setting('gss', null, User::ROLE_ADMIN) || !setting('gss', null, auth()->user()->id)) {
                 $this->shippingServiceError = 'GSS is not enabled for this user';
                 $shippingServices = $shippingServices->filter(function ($shippingService, $key) {
                     return !$shippingService->isGSSService();
