24f8b77377513ed284902e48d7daa9c6e05b4feb
sanadev01 <sanasheraz.dev@gmail.com>
2023-10-19 01:11:45 +0500

    anjun

--- app/Services/Correios/Services/Brazil/Client.php
+++ app/Services/Correios/Services/Brazil/Client.php
@@ -67,17 +67,6 @@ class Client{
 
     public function getAnjunToken()
     {
-        dd([
-            
-            'auth' => [
-                $this->anjun_username,
-                $this->anjun_password
-            ],
-            'json' => [
-                'numero' => $this->anjun_numero
-            ]
-        ]
-            );
         return Cache::remember('anjun_token',Carbon::now()->addHours(24),function (){
             $response = $this->client->post('/token/v1/autentica/cartaopostagem',[
                 'auth' => [
