d13060cbe43cab91a12f33d05ef1f506e8803097
sanadev01 <sanasheraz.dev@gmail.com>
2023-10-19 01:49:17 +0500

    bcn

--- app/Services/Correios/Services/Brazil/Client.php
+++ app/Services/Correios/Services/Brazil/Client.php
@@ -43,7 +43,7 @@ class Client{
         if(setting('bcn_api', null, \App\Models\User::ROLE_ADMIN)){ 
             $this->anjun_username = '37148594000192';
             $this->anjun_password = '9wdkSYsvk2FkqNbojC1CLlUhN1RY3HqqmmADFBPa';
-            $this->anjun_numero = '9912520230';
+            $this->anjun_numero = '0076204456';
         }
     }
 
@@ -67,17 +67,6 @@ class Client{
 
     public function getAnjunToken()
     {
-        // dd([
-            
-        //     'auth' => [
-        //         $this->anjun_username,
-        //         $this->anjun_password
-        //     ],
-        //     'json' => [
-        //         'numero' => $this->anjun_numero
-        //     ]
-        // ]
-        //     );
         return Cache::remember('anjun_token',Carbon::now()->addHours(24),function (){
             $response = $this->client->post('/token/v1/autentica/cartaopostagem',[
                 'auth' => [
