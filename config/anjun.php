a99a3f8e347face4299140106c2ae6a83d144649
sanadev01 <sanasheraz.dev@gmail.com>
2023-11-08 18:26:30 +0500

    bcn setting

--- config/anjun.php
+++ config/anjun.php
@@ -3,8 +3,8 @@
 return [
     'production' => [
         'baseUri'           => 'http://aj.bailidaming.com',
-        'username'          => 'ajtest',
-        'password'          => 'e536dc6236e42b8c6e720e6dfba82d5f',
+        'username'          => 'HERCO',
+        'password'          => 'e10adc3949ba59abbe56e057f20f883e',
         'bigPackageBaseURL' => 'http://api.bailidaming.com'
     ],
     'testing' => [
