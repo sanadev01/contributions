0cb1c76fe516888df77b0ac45619b0e4631e1318
sanadev01 <sanasheraz.dev@gmail.com>
2023-11-13 22:22:49 +0500

    gss report

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -469,7 +469,7 @@ class OrderRepository
         }
 
         if ($request->type == 'domestic') {
-            $orders->where(function ($query) {
+                        $orders->where(function ($query) {
                 $query->whereHas('shippingService', function ($query) {
                     $query->whereIn('service_sub_class', [
                         ShippingService::USPS_PRIORITY,
@@ -477,6 +477,18 @@ class OrderRepository
                         ShippingService::UPS_GROUND,
                         ShippingService::FEDEX_GROUND,
                         ShippingService::USPS_GROUND
+                    ]);
+                })->orWhereNotNull('us_api_tracking_code');
+            });
+        } elseif ($request->type == 'gss') {
+            $orders->where(function ($query) {
+                $query->whereHas('shippingService', function ($query) {
+                    $query->whereIn('service_sub_class', [
+                        ShippingService::GSS_PMI, 
+                        ShippingService::GSS_EPMEI,
+                        ShippingService::GSS_EPMI,
+                        ShippingService::GSS_FCM, 
+                        ShippingService::GSS_EMS
                     ]);
                 })->orWhereNotNull('us_api_tracking_code');
             });
