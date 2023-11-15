88a8ddeb6e32e18b7206489e6e87dccc62e86657
sanadev01 <sanasheraz.dev@gmail.com>
2023-11-16 00:28:48 +0500

    hound

--- app/Http/Controllers/Api/PublicApi/ParcelController.php
+++ app/Http/Controllers/Api/PublicApi/ParcelController.php
@@ -66,7 +66,7 @@ class ParcelController extends Controller
         if (!setting('bcn_api', null, \App\Models\User::ROLE_ADMIN) && $shippingService->is_bcn_service) {
             return apiResponse(false, $shippingService->name . ' is currently not available.');
         }
-        if (Auth::id() != "1233"  && $shippingService->is_anjun_china_service_sub_class) {
+        if (Auth::id() != "1137"  && $shippingService->is_anjun_china_service_sub_class) {
             return apiResponse(false, $shippingService->name . ' is currently not available.');
         }
 
