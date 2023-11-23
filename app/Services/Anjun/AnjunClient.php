2f2c8ac05363d28e047c9d2544e8b99cd806f664
sanadev01 <sanasheraz.dev@gmail.com>
2023-11-23 23:09:44 +0500

    anjun request

--- app/Services/Anjun/AnjunClient.php
+++ app/Services/Anjun/AnjunClient.php
@@ -10,14 +10,14 @@ use App\Services\Anjun\Services\AnjunError;
 use App\Services\Anjun\Services\BigPackage;
 use App\Services\Correios\Models\PackageError;
 use App\Services\Correios\Contracts\Container;
+use Illuminate\Support\Facades\Log;
 
 class AnjunClient
 {
 
     protected $client;
     private   $baseUri;
-    private   $username;
-    private   $password;
+    private   $token;
     private   $bigPackageBaseURL;
 
     public function __construct()
@@ -25,14 +25,12 @@ class AnjunClient
         if (app()->isProduction()) {
             // Anjun Api Production Environment Credentials
             $this->baseUri           = config('anjun.production.baseUri');
-            $this->username          = config('anjun.production.username');
-            $this->password          = config('anjun.production.password');
+            $this->token             = config('anjun.production.token');
             $this->bigPackageBaseURL = config('anjun.production.bigPackageBaseURL');
         } else {
             // Anjun Api Testing Environemtn Credentials 
             $this->baseUri           = config('anjun.testing.baseUri');
-            $this->username          = config('anjun.testing.username');
-            $this->password          = config('anjun.testing.password');
+            $this->token             = config('anjun.testing.token');
             $this->bigPackageBaseURL = config('anjun.testing.bigPackageBaseURL');
         }
         $this->client  = new GuzzleClient([
@@ -43,25 +41,19 @@ class AnjunClient
 
     public function createPackage(Order $order)
     {
-        $username = $this->username;
-        $password = $this->password;
-        $orderBody = [
-            'username'   => $username,
-            'password'   => $password,
-            'dp'         => '',        // store id
-        ] + (new Package($order))->convertToChinese();
+
+        $orderBody = (new Package($order))->requestBody(); 
         try {
-            $response = $this->client->post('/napic.asp', [
+            $response = $this->client->post('/logistics/order/api/create', [
                 'json'    =>  $orderBody,
                 'headers' => [
-                    'Content-Type' => 'application/json',
+                    'Authorization' => $this->token,
                 ]
             ]);
-
-            $responseContents = json_decode($response->getBody()->getContents());
-
-            if ($responseContents->statusCode) {
-                $trackingNumber = $responseContents->guahao;
+            $responseContents = json_decode($response->getBody()->getContents()); 
+            
+            if ($responseContents->code == 200) {
+                $trackingNumber = $responseContents->data->trackNum;
                 if ($trackingNumber) {
                     $order->update([
                         'corrios_tracking_code' => $trackingNumber,
@@ -70,11 +62,12 @@ class AnjunClient
                             "stamp_url"     => route('warehouse.cn23.download', $order->id),
                             'leve'          => false
                         ],
-                    ]);
+                        'api_response' => json_encode($responseContents->data),
+                    ]);  
                     return $this->addOrderTracking($order);
                 }
             } else {
-                return responseUnprocessable((new AnjunError($responseContents))->getErrors());
+                return responseUnprocessable($responseContents->msg);
             }
             return responseSuccessful(null, 'Label Created');
         } catch (\GuzzleHttp\Exception\ClientException $e) {
@@ -89,19 +82,20 @@ class AnjunClient
         $this->client  = new GuzzleClient([
             'base_uri' => $this->bigPackageBaseURL
         ]);
-        try {
-            $response = $this->client->put('/api/channel/apiBaxiPostXbag/createXBag', [
+        try {  
+            $response = $this->client->put("/api/channel/apiBaxiPostXbag/createXBag", [
                 'headers'      => [
                     'Content-Type' => 'application/json',
+                    'Authorization' => $this->token,
                 ],
                 'json' =>  (array) new BigPackage($container),
             ]);
 
-            $responseContents = json_decode($response->getBody()->getContents());
-            if ($responseContents->status == 200) {
+            $responseContents = json_decode($response->getBody()->getContents());  
+            if ($responseContents->status == 200) { 
                 return $this->getCN35BarCode($responseContents->data);
             } else {
-                return responseUnprocessable((new AnjunError($responseContents))->getErrors());
+                return responseUnprocessable($responseContents->msg);
             }
         } catch (\GuzzleHttp\Exception\ClientException $e) {
             return responseUnprocessable($e->getResponse()->getBody()->getContents());
@@ -117,19 +111,17 @@ class AnjunClient
             'base_uri' => $this->bigPackageBaseURL
         ]);
         try {
-            $response = $this->client->get('/api/channel/apiBaxiPostXbag/yuBaoCN35?id=' . $id, [
+            $response = $this->client->get('/api/channel/apiBaxiPostXbag/yuBaoCN35?id='.$id, [
                 'headers'      => [
                     'Content-Type' => 'application/json',
+                    'Authorization' => $this->token,
                 ],
             ]);
-
-            $responseContents = json_decode($response->getBody()->getContents());
-
+            $responseContents = json_decode($response->getBody()->getContents()); 
             if ($responseContents->status == 200) {
-                 
-                return responseSuccessful($responseContents->data, 'Label Printer Success');
-            }
-            else
+
+                return responseSuccessful($responseContents, 'Label Printer Success');
+            } else
                 return responseUnprocessable((new AnjunError($responseContents))->getErrors());
         } catch (\GuzzleHttp\Exception\ClientException $e) {
 
