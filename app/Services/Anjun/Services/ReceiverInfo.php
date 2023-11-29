ce1fe6a9a0763c658814c341f86047cefc817bc7
sanadev01 <sanasheraz.dev@gmail.com>
2023-11-29 22:44:55 +0500

    anjun

--- app/Services/Anjun/Services/ReceiverInfo.php
+++ app/Services/Anjun/Services/ReceiverInfo.php
@@ -2,10 +2,14 @@
 
 namespace App\Services\Anjun\Services;
 
+use App\Models\Order;
 use  App\Models\Recipient;
+use App\Models\ShippingService;
+
 class ReceiverInfo
 {
 
+    public $order;
     public $fullName;
     public $tax_id;               //Recipient tax or cpf // Brazilian personal tax number.
     public $companyName;          //company name
@@ -18,8 +22,9 @@ class ReceiverInfo
     public $email;
     public $address;
 
-    public function __construct(Recipient $recipient)
+    public function __construct(Recipient $recipient, Order $order)
     {
+        $this->order = $order;
         $this->fullName        = $recipient->first_name . ' ' . $recipient->last_name;
         $this->tax_id          = $recipient->tax_id;
         $this->companyName     = '';
@@ -36,10 +41,14 @@ class ReceiverInfo
 
     public function requestBody()
     {
+        $phone = $this->phone;
+        if (strlen($phone) > 12 && $this->order->shippingService->service_sub_class == ShippingService::AJ_Express_CN) {
+            $phone = substr($this->phone, 0, 3) . substr($this->phone, -9);
+        }
         return [
             'receiveName'       => $this->fullName,
-            'receivePhone'      => $this->phone,
-            'receiveMobile'     => $this->phone,
+            'receivePhone'      => $phone,
+            'receiveMobile'     => $phone,
             "receiveMail" => $this->email,
             "receiveCountry" => $this->countryCode,
             "receiveProvince" => $this->stateCode,
@@ -48,7 +57,7 @@ class ReceiverInfo
             "receiveStreet" => "",
             "receiveHouseNumber" => "",
             "receiveAddress" => $this->address,
-            "receiveZipcode" =>$this->zipcode,
+            "receiveZipcode" => $this->zipcode,
             "receiveCompany" => $this->companyName,
             "receiveTax" => $this->tax_id,
             "receiveCertificateType" => "",
