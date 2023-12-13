d6b2d4f79a60428ffeb52a0e2971ab80b53ff959
sanadev01 <sanasheraz.dev@gmail.com>
2023-12-13 19:06:42 +0500

    console remove

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,36 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('brazil:trackings')->everyMinute();
-        $schedule->command('email:order-arrived')->dailyAt('22:00');        
-        $this->scheduleDeletions($schedule);
-        $this->scheduleOrders($schedule);
+        $schedule->command('email:order-arrived')->dailyAt('22:00'); 
     }
-    protected function scheduleDeletions(Schedule $schedule) {
-        $schedule->command('app:del_job_monitors')->dailyAt('01:00');
-        $schedule->command('app:del_bug_reports')->dailyAt('01:30');
-        $schedule->command('app:del_sp_token_responses')->dailyAt('02:00');
-    }
-
-    protected function scheduleOrders(Schedule $schedule) {
-        $schedule->call(function () {
-            $this->delayDispatchJob(User::getActiveCallables(), 1800, function ($user, $delay) {
-                dispatch(new \App\Jobs\AmazonOrders\CreateOrdersHistoryJob($user))->delay($delay);
-            });
-        })->everyThirtyMinutes();
-
-        $schedule->call(function () {
-            $this->delayDispatchJob(User::getActiveCallables(), 900, function (User $user, $delay) {
-                dispatch(new \App\Jobs\AmazonOrders\GetLatestOrdersJob($user))->delay($delay);
-            });
-        })->everyFifteenMinutes();
-
-        $schedule->call(function () {
-            $this->delayDispatchJob(User::getActiveCallables(), 900, function ($user, $delay) {
-                dispatch(new \App\Jobs\AmazonOrders\GetHistoricOrdersJob($user))->delay($delay);
-            });
-        })->cron('*/15 20-22 * * *');
-    }
-    
     protected function delayDispatchJob($instances, $time, $callback) {
         if (!($total_inst = count($instances))) {
             return;
