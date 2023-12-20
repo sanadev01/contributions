6dd1429dd4052221ff08e170cdeb47d810c4a3c2
sanadev01 <sanasheraz.dev@gmail.com>
2023-12-20 23:21:10 +0500

    shcode

--- app/Http/Livewire/ShCodeTable.php
+++ app/Http/Livewire/ShCodeTable.php
@@ -4,9 +4,13 @@ namespace App\Http\Livewire;
 
 use App\Models\ShCode;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShCodeTable extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+  
     public $search = '';
 
     public function render()
