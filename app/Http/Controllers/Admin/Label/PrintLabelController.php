ed3d5a4c1f266c2dcefaf34008d1aff8461c54af
sanadev01 <sanasheraz.dev@gmail.com>
2023-12-27 23:15:12 +0500

    download excel

--- app/Http/Controllers/Admin/Label/PrintLabelController.php
+++ app/Http/Controllers/Admin/Label/PrintLabelController.php
@@ -45,6 +45,7 @@ class PrintLabelController extends Controller
      */
     public function store(Request $request, CorrieosBrazilLabelRepository $labelRepository)
     {
+        ob_end_clean();
         if($request->order){
             if($request->excel){
                 if($request->start_date != null && $request->end_date != null)
