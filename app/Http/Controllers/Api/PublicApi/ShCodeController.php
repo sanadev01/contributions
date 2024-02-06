46feae373e8a2bbbb343021b43d50c3efc3dd707
sanadev01 <sanasheraz.dev@gmail.com>
2024-02-06 23:44:41 +0500

    sh code

--- app/Http/Controllers/Api/PublicApi/ShCodeController.php
+++ app/Http/Controllers/Api/PublicApi/ShCodeController.php
@@ -11,29 +11,24 @@ class ShCodeController extends Controller
     public function __invoke($search = null)
     {
         $type = strtolower(request()->type) == "courier"  ? 'total' : null; 
+        $shCode = ShCode::where('type', $type);
         if ($search) {
-            $shCode = ShCode::where('type', $type)
-                ->where('code', "LIKE", "%{$search}%")
-                ->orWhere('description', 'LIKE', "%{$search}%")
-                ->orderBy('description', 'ASC')
-                ->get(['code', 'description']);
-            if ($shCode->isEmpty()) {
-                return apiResponse(false, 'No SH Code Found');
-            }
+            $shCode = $shCode->where('code', "LIKE", "%{$search}%")->orWhere('description', 'LIKE', "%{$search}%");
         }
-        else{
-            $shCode = ShCode::where('type', $type)->get(['code', 'description']);
-        }
+        $shCode = $shCode->orderBy('description', 'ASC')->get(['code', 'description']); 
         if (!$shCode->isEmpty()) {
             $shCodes = array();
             foreach ($shCode as $sh) {
                 array_push($shCodes, [
                     'code' => $sh->code,
-                    'type' =>  request()->type  ?? 'Corroies',
+                    'type' =>  request()->type == 'courier' ?'Courier':'Postal (Correios)',
                     'description' => $sh->description,
                 ]);
             }
             return $shCodes;
+        }
+        else{
+            return apiResponse(false, 'No SH Code Found');
         }
     }
 }
