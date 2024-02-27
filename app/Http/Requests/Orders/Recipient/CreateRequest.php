ec93f73ce019982b7c3dbf3bd7d99d7d663825f3
sanadev01 <sanasheraz.dev@gmail.com>
2024-02-27 23:14:52 +0500

    state uk

--- app/Http/Requests/Orders/Recipient/CreateRequest.php
+++ app/Http/Requests/Orders/Recipient/CreateRequest.php
@@ -27,35 +27,27 @@ class CreateRequest extends FormRequest
      */
     public function rules()
     {
-
         $rules = [
             'first_name' => ($this->country_id == Country::Chile) ? 'required|max:28' : 'required|max:50',
             'address' => 'required',
             'address2' => 'nullable|max:50',
             'street_no' => 'sometimes',
             'country_id' => 'required|exists:countries,id',
-            'city' => ($this->country_id == \App\Models\Order::Guatemala)?'nullable': 'required_if:service,==,postal_service',
+            'city' => ($this->country_id == \App\Models\Order::Guatemala) ? 'nullable' : 'required_if:service,==,postal_service',
             'commune_id' => 'required_if:service,==,courier_express',
             'phone' => [
-                (($this->country_id == \App\Models\Order::Guatemala)?'nullable':'required'),'max:15','min:11', new PhoneNumberValidator($this->country_id)
+                (($this->country_id == \App\Models\Order::Guatemala) ? 'nullable' : 'required'), 'max:15', 'min:11', new PhoneNumberValidator($this->country_id)
             ],
-            'state_id' => ['sometimes', 'exists:states,id', ($this->country_id == Country::UK)?'nullable':'required'],
-            'region' => ['sometimes', ($this->country_id == \App\Models\Order::Guatemala || $this->country_id == Country::UK|| $this->country_id == Country::Brazil)?'nullable':'required'],
+            'state_id' => ['sometimes', 'exists:states,id', ($this->country_id == Country::UK) ? 'nullable' : 'required'],
+            'region' => ['sometimes', ($this->country_id ==  Country::Chile || $this->country_id ==  Country::UK) ? 'required' : 'nullable'],
             'zipcode' => [
-                ($this->country_id == \App\Models\Order::Guatemala)?'nullable': 'required'
-                // 'required',  new CorreosAddresstValidator($this->country_id,$this->address), new ZipCodeValidator($this->country_id,$this->state_id)
+                ($this->country_id == \App\Models\Order::Guatemala) ? 'nullable' : 'required'
             ]
         ];
 
-        // if (Country::where('code', 'BR')->first()->id == $this->country_id) {
-        //     $rules['cpf'] = 'sometimes|cpf|required_if:country_id,'.Country::where('code', 'BR')->first()->id;
-        //     $rules['cnpj'] = 'sometimes|cnpj|required_if:country_id,'.Country::where('code', 'BR')->first()->id;
-        //     $rules['zipcode'] = ['required', new ZipCodeValidator($this->country_id,$this->state_id)];
-        // }
-        if (Country::where('code', 'BR')->first()->id == $this->country_id && $this->account_type =='individual') {
-            $rules['tax_id'] = 'sometimes|cpf_cnpj|required_if:country_id,'.Country::where('code', 'BR')->first()->id;
-            // $rules['tax_id'] = 'sometimes|cnpj|required_if:country_id,'.Country::where('code', 'BR')->first()->id;
-            $rules['zipcode'] = [ ($this->country_id == \App\Models\Order::Guatemala ?'nullable':'required'), new ZipCodeValidator($this->country_id,$this->state_id)];
+        if (Country::where('code', 'BR')->first()->id == $this->country_id && $this->account_type == 'individual') {
+            $rules['tax_id'] = 'sometimes|cpf_cnpj|required_if:country_id,' . Country::where('code', 'BR')->first()->id;
+            $rules['zipcode'] = [($this->country_id == \App\Models\Order::Guatemala ? 'nullable' : 'required'), new ZipCodeValidator($this->country_id, $this->state_id)];
             $rules['street_no'] = 'sometimes|numeric';
         }
 
@@ -65,6 +57,7 @@ class CreateRequest extends FormRequest
     public function messages()
     {
         return [
+            'region.required' => $this->country_id == Country::UK?'The State field is required':"The Region field is required",
             'cpf.cpf' => 'Invalid CPF',
             'cpf.*' => 'CPF Required For Brazil',
             'cnpj.cnpj' => 'Invalid CNPJ',
