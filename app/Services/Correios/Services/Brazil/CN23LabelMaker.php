b0d83fd1e3d1f71922b67e1c767a7eb4fbc5ff49
sanadev01 <sanasheraz.dev@gmail.com>
2024-03-14 23:38:15 +0500

    development

--- app/Services/Correios/Services/Brazil/CN23LabelMaker.php
+++ app/Services/Correios/Services/Brazil/CN23LabelMaker.php
@@ -66,7 +66,7 @@ class CN23LabelMaker implements HasLableExport
             $this->contractNumber = 'B Contract: 0076204456';
             $this->packageSign = 'B';
         }
-        if($this->order->shippingService->isAnjunService()) {
+        if($this->order->shippingService->is_anjun_service) {
             $this->contractNumber = 'A Contract: 9912501700';
             $this->packageSign = 'A';
 
