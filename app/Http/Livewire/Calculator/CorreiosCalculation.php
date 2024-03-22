e1c7808f7adbf1a316e947c270aa59552351404a
sanadev01 <sanasheraz.dev@gmail.com>
2024-03-22 17:09:46 +0500

    calculator

---
+++ app/Http/Livewire/Calculator/CorreiosCalculation.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Http\Livewire\Calculator;
+
+use App\Models\Order;
+use App\Models\Country;
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use App\Services\Converters\UnitsConverter;
+use App\Services\Calculators\WeightCalculator;
+
+class CorreiosCalculation extends Component
+{   
+    protected $orderId;
+
+    public $weight;
+    public $weightOther;
+    public $length;
+    public $lengthOther;
+    public $width;
+    public $widthOther;
+    public $height;
+    public $heightOther;
+    public $unit;
+    public $cc;
+    public $unitOther;
+    public $volumeWeight;
+    public $volumeWeightOther;
+    public $currentWeightUnit;
+
+    private $userId;
+    public $discountPercentage;
+    public $totalDiscountedWeight;
+
+    public function mount($cc = null, Order $order = null)
+    {
+        $this->cc = $cc;
+        $this->order = optional($order)->toArray();
+        $this->fillData();
+        $this->checkUser();
+
+        if ($this->userId && $this->cc != 'US') {
+            $this->setVolumetricDiscount();
+        }
+        
+    }
+
+    public function render() 
+    {
+        return view('livewire.calculator.correios-calculation');
+    }
+
+    public function updatedUnit()
+    {
+        $this->calculateOtherUnits();
+    }
+
+    public function updatedWeight()
+    {
+        $this->calculateOtherUnits();
+    }
+
+    public function updatedLength()
+    {
+        $this->calculateOtherUnits();
+    }
+
+    public function updatedWidth()
+    {
+        $this->calculateOtherUnits();
+    }
+
+    public function updatedHeight()
+    {
+        $this->calculateOtherUnits();
+    }
+
+    private function fillData()
+    {
+        $this->weight = old('weight', isset($this->order['weight']) ? $this->order['weight'] : 0 );
+        $this->length = old('length',isset($this->order['length']) ? $this->order['length'] : 0);
+        $this->width = old('width',isset($this->order['width']) ? $this->order['width'] : 0);
+        $this->height = old('height',isset($this->order['height']) ? $this->order['height'] : 0);
+        $this->unit = old('unit',isset($this->order['measurement_unit']) ? $this->order['measurement_unit'] : 'kg/cm');
+        $this->calculateOtherUnits();
+    }
+
+
+    public function calculateOtherUnits()
+    {
+        $this->weight = $this->weight ? $this->weight : 0;
+        $this->length = $this->length ? $this->length : 0;
+        $this->width = $this->width ? $this->width : 0;
+        $this->height = $this->height ? $this->height : 0;
+
+        if ( $this->unit == 'kg/cm' ){
+            $this->weightOther = UnitsConverter::kgToPound($this->weight);
+            $this->lengthOther = UnitsConverter::cmToIn($this->length);
+            $this->widthOther = UnitsConverter::cmToIn($this->width);
+            $this->heightOther = UnitsConverter::cmToIn($this->height);
+            $this->currentWeightUnit = 'kg';
+            $this->unitOther = 'lbs';
+            $volumetricWeight = WeightCalculator::getVolumnWeight($this->length,$this->width,$this->height,'cm');
+            $this->volumeWeight = round($volumetricWeight > $this->weight ? $volumetricWeight : $this->weight,2);
+            $this->volumeWeightOther = UnitsConverter::kgToPound($this->volumeWeight);
+
+            if ($this->discountPercentage && $this->discountPercentage > 0) {
+
+                if ($this->discountPercentage == 1) {
+                    return $this->volumeWeight = $this->weight;
+                }
+
+                if ($this->volumeWeight > $this->weight) {
+                    $this->calculateDiscountedWeight();
+                }
+            }
+
+        }else{
+            $this->weightOther = UnitsConverter::poundToKg($this->weight);
+            $this->lengthOther = UnitsConverter::inToCm($this->length);
+            $this->widthOther = UnitsConverter::inToCm($this->width);
+            $this->heightOther = UnitsConverter::inToCm($this->height);
+            $this->currentWeightUnit = 'lbs';
+            $this->unitOther = 'kg';
+            $volumetricWeight = WeightCalculator::getVolumnWeight($this->length,$this->width,$this->height,'in');;
+            $this->volumeWeight = round($volumetricWeight > $this->weight ? $volumetricWeight : $this->weight,2);
+            $this->volumeWeightOther = UnitsConverter::poundToKg($this->volumeWeight);
+
+            if ($this->discountPercentage && $this->discountPercentage > 0) {
+
+                if ($this->discountPercentage == 1) {
+                    return $this->volumeWeight = $this->weight;
+                }
+                
+                if ($this->volumeWeight > $this->weight) {
+                    $this->calculateDiscountedWeight();
+                }
+            }
+        }
+    }
+
+    public function checkUser()
+    {
+        if (Auth::check()) {
+            
+            $this->userId = Auth::user()->id;
+            $country_id = Auth::user()->country_id;
+            if($country_id)
+            {
+                $country = Country::find($country_id);
+                if($country->id == Country::US)
+                {
+                   return $this->unit = 'lbs/in';
+                }
+            }
+        }
+
+        return;
+    }
+
+    private function setVolumetricDiscount()
+    {
+        $volumetricDiscount = setting('volumetric_discount', null, $this->userId);
+        $discountPercentage = setting('discount_percentage', null, $this->userId);
+
+        if ($volumetricDiscount && $discountPercentage) {
+            $this->discountPercentage = ($discountPercentage) ? $discountPercentage/100 : 0;
+        }
+
+        return true;
+    }
+
+    private function calculateDiscountedWeight()
+    {
+        $consideredWeight = $this->volumeWeight - $this->weight;
+        
+        $this->volumeWeight = round($consideredWeight - ($consideredWeight * $this->discountPercentage), 2);
+        $this->totalDiscountedWeight = $consideredWeight - $this->volumeWeight;
+        $this->volumeWeight = round($this->volumeWeight + $this->weight, 2);
+        $this->volumeWeightOther = ($this->unit == 'kg/cm') ? UnitsConverter::kgToPound($this->volumeWeight) : UnitsConverter::poundToKg($this->volumeWeight);
+    }
+}
--- resources/views/calculator/index.blade.php
+++ resources/views/calculator/index.blade.php
@@ -1,98 +1,86 @@
-@extends('layouts.app')
+@extends('layouts.master')
 @section('custom-css')
 <link rel="stylesheet" href="{{ asset('app-assets/select/css/bootstrap-select.min.css') }}">
-    <style>
-        .number-input{
-            margin-top: 3px;
-            padding: 15px;
-            font-size: 16px;
-            width: 100%;
-            border-radius: 3px;
-            border: 1px solid #dcdcdc;
-        }
-    </style>
+<style>
+    .number-input {
+        margin-top: 3px;
+        padding: 15px;
+        font-size: 16px;
+        width: 100%;
+        border-radius: 3px;
+        border: 1px solid #dcdcdc;
+    }
+
+    body {
+        background-color: #f0f4f7;
+    }
+
+
+    .card-bg {
+        color: #373C3F;
+        border: 1px solid #ffffff;
+        background-color: #ffffff;
+    }
+
+    .btn-blue {
+        background-color: #1174b7;
+        color: white;
+    } 
+</style>
 @endsection
-@section('content')
+@section('page')
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
 
-    <!-- Dashboard Analytics Start -->
-    <section id="vue-calculator">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-8">
-                    <div class="card p-2">
-                        <div class="card-header pb-0">
-                            <h2 class="mb-2 text-center w-100">
-                                Calculator
-                            </h2>
-                        </div>
-                        
-                    <form action="{{action('CalculatorController@store')}}" method="POST">
-
-                        @csrf
-                        <div class="card-body">
-                            <div class="row mb-1">
-                                <div class="controls col-12"> 
-                                    <label>Destination Country</label>
-                                    <select id="country"  name="country_id" class="form-control selectpicker show-tick" data-live-search="true">
-                                        <option value="">Select @lang('address.Country')</option>
-                                        @foreach (countries() as $country)
-                                            <option {{ old('country_id') == $country->id ? 'selected' : '' }} value="{{ $country->id }}">{{ $country->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="row mb-1">
-                                <div class="controls col-12">
-                                    <label>Destination State</label>
-                                    <option value="" selected disabled hidden>Select State</option>
-                                    <select name="state_id" id="state" class="form-control selectpicker show-tick" data-live-search="true">
-                                        <option value="">Select @lang('address.State')</option>
-                                    </select>
-                                </div>
-                            </div>
-
-
-                            <livewire:calculator.calculation>
-
-                            <div class="row">
-                                <div class="col-md-12 d-flex justify-content-center">
-                                    <button type="submit" class="btn btn-primary btn-lg">
-                                        Get Rates
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-
-                        </form>
-                        {{-- <div>
-                            <div class="row justify-content-center">
-                                <div class="col-md-8 text-center">
-                                    <button class="btn btn-lg btn-primary">
-                                        Re-Calculate
-                                    </button>
-                                </div>
-                            </div>
-                        </div> --}}
-                    </div>
-                </div>
+<section>
+    <div class="row mt-4">
+        <div class="col-12 mx-2">
+            <div class="ml-3">
+                <dl>
+                    <dt class="font-weight-bold dt">Calculator </dt>
+                </dl>
             </div>
         </div>
-    </section>
+    </div>
+</section>
+<!-- Dashboard Analytics Start -->
+<section id="vue-calculator">
+    <div class="mx-3">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <form action="{{action('CalculatorController@store')}}" method="POST">
+
+                    <div class="card-bg rounded p-4">
+                        @csrf
+
+                        <livewire:calculator.correios-calculation>
 
 
-    <!-- Dashboard Analytics end -->
+                    </div>
+
+                    <div class="row ml-1 mt-3">
+                        <button type="submit" class="btn btn-blue btn-md rounded px-5 my-3">
+                            Get Rates
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Dashboard Analytics end -->
 @endsection
-@section('jquery')
+@section('js')
 <script src="{{ asset('app-assets/select/js/bootstrap-select.min.js') }}"></script>
 @include('layouts.states-ajax')
 @endsection
