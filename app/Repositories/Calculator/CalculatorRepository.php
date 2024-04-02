45e8fd051e4b91fe642445b3d716134577829a21
sanadev01 <sanasheraz.dev@gmail.com>
2024-04-02 17:24:27 +0500

    correis

--- app/Repositories/Calculator/CalculatorRepository.php
+++ app/Repositories/Calculator/CalculatorRepository.php
@@ -95,6 +95,7 @@ class CalculatorRepository {
 
     public function getShippingService()
     {
+        $message="Shipping Service not found";
         $anjunSelected = setting('anjun_api', null, User::ROLE_ADMIN) ||setting('bcn_api', null, User::ROLE_ADMIN);  
         $shippingServices = collect();
         foreach (ShippingService::query()->active()->get() as $shippingService) {
