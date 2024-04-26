393bf183fb3170fa14d7d2f50a3ee6eaffa98a91
sanadev01 <sanasheraz.dev@gmail.com>
2024-04-27 00:55:47 +0500

    GSS Commercial E-Packet Dimension, Weight and Rate Limit

--- config/gss.php
+++ config/gss.php
@@ -1,11 +1,11 @@
 <?php
 return [
     'test' => [
-        'userId' => 'HERFuser',
-        'password' => 'MFLA#831gss',
-        'locationId' => 'MIAMIFLOHERCOFRGTUSM',
+        'userId' => 'HFTSdemo',
+        'password' => 'FLOR23test',
+        'locationId' => 'TESTSITEHERCOFRGTUSM',
         'workStationId' => 'HERCOFRGTUSM',
-        'baseUrl' => 'https://gss.usps.com/usps-cpas/gssapiv2/api',
+        'baseUrl' => 'https://gss.usps.com/usps-cpas/Testgssapiv2/api',
     ],
     'production' => [
         'userId' => 'HERFuser',
