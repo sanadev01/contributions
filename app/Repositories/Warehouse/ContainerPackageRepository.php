42373b35fa68fe43c15449158e0322b6d3a4ccab
sanadev01 <sanasheraz.dev@gmail.com>
2024-05-16 23:45:45 +0500

    execution

--- app/Repositories/Warehouse/ContainerPackageRepository.php
+++ app/Repositories/Warehouse/ContainerPackageRepository.php
@@ -69,7 +69,7 @@ class ContainerPackageRepository extends AbstractRepository{
         if ($order->status < Order::STATUS_PAYMENT_DONE) {
             return $this->validationError404($barcode, 'Please check the Order Status, either the order has been canceled, refunded or not yet paid');
         }  
-        if (!$container->hasAnjunService() && $order->shippingService->isAnjunService()) {
+        if (!$container->hasAnjunService() || !$order->shippingService->isAnjunService()) {
             return $this->validationError404($barcode, 'Order does not belongs to this container Service. Please Check Packet Service');
         }
 
