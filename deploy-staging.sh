c9f87ef3e48f8ab4e1c5708d987e7583ae7f2f43
sanadev01 <sanasheraz.dev@gmail.com>
2024-06-14 21:06:16 +0500

    conflict

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,24 +1,10 @@
-#!/bin/bash
-
-# Define PHP version path
-PHP_PATH="/usr/local/bin/ea-php73"
-
-# Navigate to repository and update code
 cd /home/homeqdba/repositories/hd-v2/
 git checkout development
-git reset --hard
 git pull
-
-# Copy updated code to deployment directory
 cd /home/homeqdba/dev.homedeliverybr.com
 cp -R /home/homeqdba/repositories/hd-v2/* /home/homeqdba/dev.homedeliverybr.com/
-
-# Install dependencies using Composer
-${PHP_PATH} /opt/cpanel/composer/bin/composer install --no-interaction
-
-# Run Laravel Artisan commands
-${PHP_PATH} artisan migrate --no-interaction
-${PHP_PATH} artisan cache:clear --no-interaction
-${PHP_PATH} artisan view:clear --no-interaction
-${PHP_PATH} artisan config:clear --no-interaction
-
+/usr/local/bin/ea-php73 /opt/cpanel/composer/bin/composer install --no-interaction
+php artisan migrate
+php artisan cache:clear --no-interaction
+php artisan view:clear --no-interaction
+php artisan config:clear --no-interaction
