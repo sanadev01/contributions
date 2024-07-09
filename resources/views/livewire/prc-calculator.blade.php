057d1a5c8c7ea0219bf6f7368d37098da45412c7
sanadev01 <sanasheraz.dev@gmail.com>
2024-07-10 00:02:14 +0500

    prc

--- resources/views/livewire/prc-calculator.blade.php
+++ resources/views/livewire/prc-calculator.blade.php
@@ -1,57 +1,127 @@
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-12 text-center">
-            <h1>Tax and Duty Calculator</h1>
-        </div>
-        <div class="col-md-6">
+<div class="mt-5">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb" style="background-color: #f7fbfe;">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Calculator</a></li>
+            <li class="breadcrumb-item active" aria-current="page">PRC</li>
+        </ol>
+    </nav>
+    <div class="row">
+
+        <h4 class="col-12 my-4 font-weight-bold font-black"> Tax and Duty Calculator</h4>
+        <div class="col-md-6 mb-4">
             <div class="card">
                 <div class="card-header">
-                    <h3>PRC Calculator</h3>
+
+                    <div class="d-flex align-items-center">
+                        <div class="vertical-rectangle mb-0"> &nbsp; &nbsp;</div>
+                        <h4 class="col-12 font-weight-bold font-black mb-0">PCR Calculator</h4>
+                    </div>
+
                 </div>
-                <div class="card-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="prcCostOfProduct">Cost of Product:</label>
-                            <input type="number" step="0.01"  id="prcCostOfProduct" class="form-control" wire:model="prcCostOfProduct">
+                <div class="card-body m-3">
+                    <form class="row">
+                        <div class="col-md-6"> 
+                            <div class="form-group">
+                                <label for="prcCostOfProduct">Cost of Product</label>
+                                <input type="number" step="0.01" id="prcCostOfProduct" class="form-control pl-3" wire:model="prcCostOfProduct">
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="prcShippingCost">Shipping Cost (Freight):</label>
-                            <input type="number" step="0.01"  id="prcShippingCost" class="form-control" wire:model="prcShippingCost">
+                        <div class="col-md-6">
+                            <div class="form-group">
+
+                                <label for="prcShippingCost">
+                                    Shiping Cost 
+                                </label>
+                                <input type="number" step="0.01" id="prcShippingCost" class="form-control pl-3" wire:model="prcShippingCost">
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="prcInsurance">Insurance:</label>
-                            <input type="number" step="0.01"  id="prcInsurance" class="form-control" wire:model="prcInsurance">
+                        <div class="col-md-12">
+
+                            <div class="form-group">
+                                <label for="prcInsurance"> Insurance
+                                </label>
+                                <input type="number" step="0.01" id="prcInsurance" class="form-control pl-3" wire:model="prcInsurance">
+                            </div>
                         </div>
                     </form>
                 </div>
-                <div class="card-footer">
-                    <h4>Total Tax and Duty: {{ $prcTotalTaxAndDuty }}</h4>
+            </div>
+            <div class="card">
+                <div class="m-2">
+                    <div class="container my-2">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex align-items-center">
+                                <div class="vertical-rectangle mr-2 mb-0"></div>
+                                <h4 class="font-weight-bold mb-0">PRC Result</h4>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <label class="mr-2 mb-1" for="">Total Tax & Duty</label>
+                                <h4 class="font-weight-bold mb-0">${{ $prcTotalTaxAndDuty }}</h4>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 mb-4">
             <div class="card">
                 <div class="card-header">
-                    <h3>Non-PRC Calculator</h3>
+                    <div class="d-flex align-items-center">
+                        <div class="vertical-rectangle mb-0"> &nbsp; &nbsp;</div>
+                        <h4 class="col-12 font-weight-bold font-black mb-0">Non-PCR Calculator</h4>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="nonPrcCostOfProduct">Cost of Product:</label>
-                            <input type="number" step="0.01"  id="nonPrcCostOfProduct" class="form-control" wire:model="nonPrcCostOfProduct">
+
+                <div class="card-body m-3">
+
+                    <form class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+
+                                <label for="nonPrcCostOfProduct">
+                                    Cost of Product
+                                </label>
+                                <input type="number" step="0.01" id="nonPrcCostOfProduct" class="form-control pl-3" wire:model="nonPrcCostOfProduct">
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="nonPrcShippingCost">Shipping Cost (Freight):</label>
-                            <input type="number" step="0.01"  id="nonPrcShippingCost" class="form-control" wire:model="nonPrcShippingCost">
+                        <div class="col-md-6">
+
+                            <div class="form-group">
+
+                                <label for="nonPrcShippingCost">
+
+                                    Shipping Cost
+                                </label>
+                                <input type="number" step="0.01" id="nonPrcShippingCost" class="form-control pl-3" wire:model="nonPrcShippingCost">
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="nonPrcInsurance">Insurance:</label>
-                            <input type="number" step="0.01"  id="nonPrcInsurance" class="form-control" wire:model="nonPrcInsurance">
+                        <div class="col-md-12">
+
+                            <div class="form-group">
+                                <label for="nonPrcInsurance">
+                                    Insurance
+                                </label>
+                                <input type="number" step="0.01" id="nonPrcInsurance" class="form-control pl-3" wire:model="nonPrcInsurance">
+                            </div>
                         </div>
                     </form>
                 </div>
-                <div class="card-footer">
-                    <h4>Total Tax and Duty: {{ $nonPrcTotalTaxAndDuty }}</h4>
+            </div>
+            <div class="card">
+                <div class="m-2">
+                    <div class="container my-2">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex align-items-center">
+                                <div class="vertical-rectangle mr-2 mb-0"></div>
+                                <h4 class="font-weight-bold mb-0">PRC Result</h4>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <label class="mr-2 mb-1" for="">Total Tax & Duty</label>
+                                <h4 class="font-weight-bold mb-0">${{ $nonPrcTotalTaxAndDuty }}</h4>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/prc-calculator/index.blade.php
+++ resources/views/prc-calculator/index.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.master')
+@section('css')
+<link rel="stylesheet" type="text/css" href="{{ asset('app-assets/css/pages/kpi.css') }}">
+<style>
+    .vertical-rectangle {
+        background-color: #1074B6;
+        width: 3.1px;
+        height: 16.8px;
+        gap: 0px;
+        opacity: 0px;
+    }
+    .form-group:hover input {
+      border-left: 5px solid #1074B6;; 
+      background-color: #F2FAFF;
+    }
+    .form-control{
+        background-color: #F8F8F8;
 
+    }
+    .form-group:hover label {
+      color: #1074B6;
+    }
+</style>
+@endsection
 @section('page')
-    <livewire:prc-calculator />
+<livewire:prc-calculator />
 @endsection
