ecf99f93f3369975c2dd637c2af918161458c7e8
sanadev01 <sanasheraz.dev@gmail.com>
2024-07-11 18:18:44 +0500

    Homedelviery Loader Update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
-    <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; {{ date('Y') }} All rights Reserved</span><span class="float-md-right d-none d-md-block">Developed by Ikonic  <img src="{{ asset('images/ikonic-logo.png') }}" width="18" alt=""></span>
+    <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; {{ date('Y') }} All rights Reserved</span><span class="float-md-right d-none d-md-block">Developed by  <img src="{{ asset('images/ikonic-logo.png') }}" width="80" alt=""></span>
     </p>
 </footer>
 <!-- END: Footer-->
