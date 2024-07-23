7ee4dc8a2d6468dddb2518821380acdb6a2f8af5
sanadev01 <sanasheraz.dev@gmail.com>
2024-07-23 21:49:13 +0500

    Anjun Contract on cn23 label Dev

--- config/anjun.php
+++ config/anjun.php
@@ -12,6 +12,6 @@ return [
         'username'          => 'HERCO',
         'password'          => 'e10adc3949ba59abbe56e057f20f883e',
         'bigPackageBaseURL' => 'http://api.bailidaming.com',
-        'token' => "oZ2ETMjDJznOfJDTg2VULj7/MOC7wxQ3+eVhzaURniQt2VjSFU3GfE9CwyE8XdRZkQ/bDX+jRDYfQASnCA0M7TEqcQCrrqHhK7LU9qfdyftqST6F8K0OKHTANLJeaVGyzQiTFtxJgC5lTpcLIZ+QTJKCRHK0h6o6uMPpMGdVXIivuR4p8beXMVVeqQ//qGfVb9kUzkTuwOat/OhrLaPL0Z0Oe/JfHOSBmAoPjZ8lhNHO40CmeSa6GmprjXQ4/uJTaZ/JWTWosROH1QvY1DClwkseuDumRrppb4Uta5ko9WVmPvVE8G/+VZETdukifW5hVOqsPi6kDDLXC17RerPFmQ==,POdWyBrBOuRUa65rr5ot5qVLJYkgzR7UtH2uDVAsF0/wuiIBFvsSSPeFc+uM9mNmij8yxMwqyjmkNM32+vZc2gZekCLVZqmzDyoWxWvFDscA860IYVPuTaDLgSkqh7SdMCnkz9XByQqZbD6Lzj6kl8BNxdyVWrwAjscHJrisVtc=,"
+        'token' => "BeHR+brPqLiAxdyrF4g6S2knHPyfb/gdZgz/cXwWUSwSfCr01eWu+BvZ+Iz4fhr4W19lWTUV3Lnnq2TUyFivGaEis0oB1TC0thMH/h+SXEZubO5xTva7rC7s88aUauNm7CeEVYC6Cezj1sSyb2kMqQnL4GdAaCwWoQwRnxbDbD8yaHSoqrWg9GZ4j85H3isywk4gyC6+xj/E9bBLdcijL4At9pqKckbnO69yOgzV4I5Fi2L4GlvBb3DXQlkW+5/UAF+dEXctms3HJFp2Y7fGxXeRFCPHnyJoH8IDbguCBqpQQeJQAeBhz26jsfbYCKB9MpBpkfIwCI50BHy9HSEb5Q==,FcX9vyl7bEsZZNMDH9FnhD60GXRYLHjZ1NSf8CzzTqYWLD+gomCJ5SYd6v30kr11YI+bAJBJkYsbVSw0aovH2vbziMT59jEstwwhxC3AsWpDjqzaEl4lNJuhV9NyoEsTNPypQ854yoW8MDHLRJZ+BHLbOdsDI/eerBgWA7RUzvY=,"
     ],
 ];
