48743a1668d432e8278792f204218e2aa56c2a39
sanadev01 <sanasheraz.dev@gmail.com>
2024-07-30 18:44:41 +0500

    anjun china

--- app/Http/Controllers/Admin/Order/OrderExportController.php
+++ app/Http/Controllers/Admin/Order/OrderExportController.php
@@ -17,18 +17,14 @@ class OrderExportController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $todayDate = Carbon::now()->format('Y-m-d');
-        $startDate = $request->start_date;
-        $endDate = $request->end_date;
-        $startDate = $startDate ? $startDate : "2020-01-01";
-        $endDate = $endDate ? $endDate : $todayDate;
-
+        $request['start_date']  = $request->start_date?? Carbon::now()->startOfMonth()->toDateString();
+        $request['end_date']  = $request->end_date?? Carbon::now()->format('Y-m-d'); 
         $report = Reports::create([
             'user_id' => Auth::id(),
             'name' => $request->type == "anjun" ? "Anjun Report" : ( $request->type == "bcn" ? 'BCN Export':( $request->type == "correios" ? 'Correios Export':( $request->type == "anjun_china" ? 'Anjun China Export':"Orders Export"))),
-            'start_date' => $startDate,
-            'end_date' => $endDate,
-        ]); 
+            'start_date' => $request['start_date'],
+            'end_date' => $request['end_date'],
+        ]);
         $request->merge(['report' => $report->id]);
         if(in_array($request->type ,["anjun","anjun_china",'bcn','correios'])){
             ExportAnjunReport::dispatch($request->all(), Auth::user());
