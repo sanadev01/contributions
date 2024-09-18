5e6d4dc35a6e0f2d82c07068377b4c5f21202b03
sanadev01 <sanasheraz.dev@gmail.com>
2024-09-18 23:32:49 +0500

    cainiao

---
+++ app/Services/Cainiao/CN35LabelMaker.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Services\Cainiao;
+
+use App\Models\Warehouse\Container;
+use App\Services\Correios\Contracts\HasLableExport;
+use Carbon\Carbon;
+
+class CN35LabelMaker implements HasLableExport
+{
+
+    private $companyName;
+    private $packetType;
+    private $dispatchNumber;
+    private $officeAddress;
+    private $serialNumber;
+    private $flightNumber;
+    private $dispatchDate;
+    private $originAirpot;
+    private $destinationAirport;
+    private $itemsCount;
+    private $weight;
+    private $service;
+    private $unitCode;
+    private $OrderWeight;
+    private $colombiaContainer = false;
+
+    public function __construct(Container $container)
+    {
+        $this->companyName = 'Cainiao';
+        $this->packetType = 'PACKET STANDARD';
+        $this->officeAddress = '';
+        $this->serialNumber = 1;
+        $this->flightNumber = $container->flight_number;
+        $this->dispatchDate = Carbon::now()->format('Y-m-d');
+        $order = $container->orders->first();
+        if ($order) {
+            $this->setType($order->getOriginalWeight('kg'));
+        }
+
+        $this->weight =  $container->getWeight();
+        $this->dispatchNumber = $container->dispatch_number;
+        $this->setService($container->getServiceCode());
+        $this->destinationAirport = $container->destination_operator_name;
+        $this->itemsCount = $container->getPiecesCount();
+        $this->unitCode = $container->getUnitCode();
+        $this->originAirpot = 'HKG'; 
+        $this->destinationAirport="GRU";
+        $this->officeAddress = "Empresa Brasileira de Correios e Telégrafos<br>
+                                Centro Internacional de Curitiba - SE/PR<br>
+                                Rua Salgado Filho, 476, Jardim Amélia<br>
+                                83330-972 - Pinhais/PR<br>
+                                CNPJ: 34.028.316/9148-22";
+    }
+
+    public function setCompanyName($companyName)
+    {
+        $this->companyName = $companyName;
+        return $this;
+    }
+
+    public function setService($service)
+    {
+        $this->service = $service;
+
+        return $this;
+    }
+
+    public function setDispatchNumber(string $dispatchNumber)
+    {
+        $this->dispatchNumber = $dispatchNumber;
+        return $this;
+    }
+
+    public function setOfficeAddress(string $address)
+    {
+        $this->officeAddress = $address;
+        return $this;
+    }
+
+    public function setSerialNumber(int $serialNumber)
+    {
+        $this->serialNumber = $serialNumber;
+        return $this;
+    }
+
+    public function setFlightNumber(string $flightNumber)
+    {
+        $this->flightNumber = $flightNumber;
+        return $this;
+    }
+
+    public function setDispatchDate(string $date)
+    {
+        $this->dispatchDate = $date;
+        return $this;
+    }
+
+    public function setOriginAirport(string $airport)
+    {
+        $this->originAirpot = $airport;
+        return $this;
+    }
+
+    public function setType(string $weight)
+    {
+        $this->OrderWeight = $weight;
+        $this->officeAddress = 'cainiao Colombia';
+        return $this;
+    }
+    public function setDestinationAirport(string $airport)
+    {
+        $this->destinationAirport = $airport;
+        return $this;
+    }
+
+    public function setItemsCount(string $itemsCount)
+    {
+        $this->itemsCount = $itemsCount;
+        return $this;
+    }
+
+    public function setWeight(string $weight)
+    {
+        $this->weight = $weight;
+        return $this;
+    }
+
+    public function setUnitCode(string $unitCode)
+    {
+        $this->unitCode = $unitCode;
+        return $this;
+    }
+
+    public function render()
+    {
+        return view('labels.cainiao.cn35.index', $this->getViewData());
+    }
+
+    public function download()
+    {
+        return \PDF::loadView('labels.cainiao.cn35.index', $this->getViewData())->stream();
+    }
+
+    public function saveAs($path)
+    {
+        return \PDF::loadView('labels.cainiao.cn35.index', $this->getViewData())->save($path);
+    }
+
+    private function getViewData()
+    {
+        return [
+            'companyName' => $this->companyName,
+            'packetType' => $this->packetType,
+            'dispatchNumber' => $this->dispatchNumber,
+            'officeAddress' => $this->officeAddress,
+            'serialNumber' => $this->serialNumber,
+            'flightNumber' => $this->flightNumber,
+            'dispatchDate' => $this->dispatchDate,
+            'originAirpot' => $this->originAirpot,
+            'destinationAirport' => $this->destinationAirport,
+            'itemsCount' => $this->itemsCount,
+            'weight' => $this->weight,
+            'service' => $this->service,
+            'unitCode' => $this->unitCode,
+            'OrderWeight' => $this->OrderWeight,
+            'colombiaContainer' => $this->colombiaContainer,
+        ];
+    }
+}
