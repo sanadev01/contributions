2a35330814f04aaf93d7f2e1f406f539e75c5fa2
sanadev01 <sanasheraz.dev@gmail.com>
2024-09-23 23:50:09 +0500

    FOX-PHX APi

--- app/Http/Controllers/Api/PublicApi/OrderLabelController.php
+++ app/Http/Controllers/Api/PublicApi/OrderLabelController.php
@@ -27,6 +27,7 @@ use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Events\AutoChargeAmountEvent;
+use App\Repositories\FoxCourierLabelRepository;
 use App\Repositories\HoundExpressLabelRepository;
 use App\Repositories\PasarExLabelRepository;
 use App\Repositories\SenegalLabelRepository;
@@ -178,6 +179,15 @@ class OrderLabelController extends Controller
                     }
                     if ($corrieosBrazilLabelRepository->getError()) {
                         return $this->rollback($corrieosBrazilLabelRepository->getError());
+                    }
+                }
+
+                if ($order->shippingService->is_fox_courier) {
+                    $foxCourierLabelRepository = new FoxCourierLabelRepository();
+                    $foxCourierLabelRepository->get($order);
+                    $error = $foxCourierLabelRepository->getError();
+                    if ($error) {
+                        return $this->rollback((string)$error);
                     }
                 }
             }
