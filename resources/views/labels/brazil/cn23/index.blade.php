b952b197c042a89bff4844164b970d2f5522db91
sanadev01 <sanasheraz.dev@gmail.com>
2024-09-25 19:01:27 +0500

    DDP Condition on Logo Label

--- resources/views/labels/brazil/cn23/index.blade.php
+++ resources/views/labels/brazil/cn23/index.blade.php
@@ -399,7 +399,7 @@
     <img class="partner-logo" src="{{ $partnerLogo }}">
     <img class="corrioes-lable" src="{{ $corriosLogo }}" alt="">
     
-    @if(setting('prc_label', null, $order->user_id) && $order->tax())
+    @if(setting('prc_label', null, $order->user_id) && $order->tax() && $order->tax_modality == 'DDP')
         <img class="customs-logo" src="{{ $customsLogo }}" alt="">
     @else
         <p class="screening-code">CJA01</p>
